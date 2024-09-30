a4996d4b5a14f0d6148d165f5e712262b4065b7a
groninge <groninge@gmail.com>
2024-09-30 19:51:22 +0200

    fix most integration tests

--- packages/lib/vitest.config.integration.ts
+++ packages/lib/vitest.config.integration.ts
@@ -1,5 +1,4 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-import { InlineConfig } from 'vitest'
+import { InlineConfig } from 'vitest/node'
 import vitestUnitConfig from './vitest.config'
 
 function setupFilesWithoutMswSetup() {
