a31e250c3c47334affa3a0918a0b752c833674d4
groninge <groninge@gmail.com>
2024-10-02 07:31:27 +0200

    fix unit tests

--- packages/lib/vitest.config.ts
+++ packages/lib/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, '.'),
+      '@repo/lib': resolve(__dirname, '.'),
     },
   },
   envPrefix: ['VITE', 'NEXT'],
