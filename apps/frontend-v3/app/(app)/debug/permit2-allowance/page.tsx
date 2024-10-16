d0e5c8b3a3e0dcd904d6fd5be4b4016a95cc5ad8
Gareth Fuller <gareth.a.f@gmail.com>
2024-10-16 09:39:48 +0100

    chore: Remove unused deps

--- apps/frontend-v3/app/(app)/debug/permit2-allowance/page.tsx
+++ apps/frontend-v3/app/(app)/debug/permit2-allowance/page.tsx
@@ -1,15 +1,12 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 'use client'
 
-import { getGqlChain, getNetworkConfig } from '@repo/lib/config/app.config'
 import { BPT_DECIMALS } from '@repo/lib/modules/pool/pool.constants'
 import { useUserAccount } from '@repo/lib/modules/web3/UserAccountProvider'
-import { permit2Abi } from '@balancer/sdk'
 import { Center, Input, Text, VStack } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Address, formatUnits } from 'viem'
 import { sepolia } from 'viem/chains'
-import { useReadContract } from 'wagmi'
+import { useDebugPermit2Allowance } from '@repo/lib/shared/hooks/debug.hooks'
 
 export default function Page() {
   const [tokenAddress, setTokenAddress] = useState<Address>('' as Address)
@@ -18,7 +15,7 @@ export default function Page() {
 
   const chainId = chain?.id || sepolia.id
 
-  const { data } = usePermit2Allowance({ chainId, tokenAddress, owner: userAddress })
+  const { data } = useDebugPermit2Allowance({ chainId, tokenAddress, owner: userAddress })
 
   return (
     <Center>
@@ -27,38 +24,16 @@ export default function Page() {
           Enter address of token to check permit2 allowance in the current chain:{' '}
           {chain ? chain.name : 'None'}
         </Text>
-        <Input type="text" onChange={e => setTokenAddress(e.target.value as Address)} />
+        <Input onChange={e => setTokenAddress(e.target.value as Address)} type="text" />
 
-        {data && (
+        {data ? (
           <div>
             <div>Amount: {formatUnits(data[0], BPT_DECIMALS).toString()}</div>
             <div>Expires: {data[1]}</div>
             <div>Nonce: {data[2]}</div>
           </div>
-        )}
+        ) : null}
       </VStack>
     </Center>
   )
-}
-
-type Params = {
-  chainId: number
-  tokenAddress: Address
-  owner: Address
-}
-function usePermit2Allowance({ chainId, tokenAddress, owner }: Params) {
-  const permit2Address = '0x000000000022D473030F116dDEE9F6B43aC78BA3'
-  const balancerRouter = getNetworkConfig(getGqlChain(chainId)).contracts.balancer.router!
-  const spender = balancerRouter
-
-  return useReadContract({
-    chainId,
-    address: permit2Address,
-    abi: permit2Abi,
-    functionName: 'allowance',
-    args: [owner, tokenAddress, spender],
-    query: {
-      enabled: !!tokenAddress && !!owner,
-    },
-  })
 }
