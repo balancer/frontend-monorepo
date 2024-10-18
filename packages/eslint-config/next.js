203f815b735ad43223fb1affa3fa98910f191de5
groninge <groninge@gmail.com>
2024-10-18 14:58:55 +0200

    chore: fix eslint rules (#85)
    * fix eslint rules
    
    * add file
    
    * import prettier rules as json
    
    * add prettier to eslint-config
    
    * change eslint config for lib package

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,16 +1,16 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    require.resolve("@vercel/style-guide/eslint/react"),
-    "turbo",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    require.resolve('@vercel/style-guide/eslint/next'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+    'turbo',
+    'prettier',
   ],
   globals: {
     React: true,
@@ -20,9 +20,9 @@ module.exports = {
     node: true,
     browser: true,
   },
-  plugins: ["@typescript-eslint", "only-warn"],
+  plugins: ['@typescript-eslint', 'only-warn'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
@@ -30,44 +30,43 @@ module.exports = {
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
+    '.*.js',
+    'node_modules/',
   ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
   rules: {
-    curly: ["error", "multi-line"],
-    "no-console": ["off"],
-    "max-len": ["warn", { code: 120, ignoreComments: true, ignoreUrls: true }],
-    "@typescript-eslint/no-explicit-any": ["off"],
-    "no-html-link-for-pages": ["off"],
-    "no-restricted-imports": [
-      "error",
+    curly: ['error', 'multi-line'],
+    'no-console': ['off'],
+    'max-len': ['warn', { code: 120, ignoreComments: true, ignoreUrls: true }],
+    '@typescript-eslint/no-explicit-any': ['off'],
+    'no-html-link-for-pages': ['off'],
+    'no-restricted-imports': [
+      'error',
       {
         patterns: [
           {
-            group: ["wagmi"],
-            importNames: ["useAccount"],
-            message: "Use useUserAccount instead to avoid hydration issues",
+            group: ['wagmi'],
+            importNames: ['useAccount'],
+            message: 'Use useUserAccount instead to avoid hydration issues',
           },
           {
-            group: ["wagmi/dist"],
-            message: "Invalid import: remove dist from import path",
+            group: ['wagmi/dist'],
+            message: 'Invalid import: remove dist from import path',
           },
           {
-            group: ["@apollo/client"],
-            importNames: ["useQuery"],
+            group: ['@apollo/client'],
+            importNames: ['useQuery'],
             message:
               // eslint-disable-next-line max-len
-              "Import useQuery from @apollo/experimental-nextjs-app-support/ssr to avoid u.inFlightLinkObservables errors",
+              'Import useQuery from @apollo/experimental-nextjs-app-support/ssr to avoid u.inFlightLinkObservables errors',
           },
           {
-            group: ["act"],
-            importNames: ["react-dom/test-utils"],
-            message:
-              "Invalid import: import from '@testing-library/react' instead",
+            group: ['act'],
+            importNames: ['react-dom/test-utils'],
+            message: "Invalid import: import from '@testing-library/react' instead",
           },
         ],
       },
     ],
   },
-};
+}
