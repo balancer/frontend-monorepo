a35d48a10b80900d5b7de449f19830855fb81acb
Alberto Gualis <agualis@gmail.com>
2024-10-22 17:01:11 +0200

    chore: format SwapProvider

--- packages/lib/modules/swap/handlers/BaseDefaultSwap.handler.ts
+++ packages/lib/modules/swap/handlers/BaseDefaultSwap.handler.ts
@@ -1,8 +1,6 @@
 import { Path, Slippage, Swap, SwapKind, TokenAmount } from '@balancer/sdk'
 import { getChainId } from '@repo/lib/config/app.config'
-import {
-  GqlSorSwapType
-} from '@repo/lib/shared/services/api/generated/graphql'
+import { GqlSorSwapType } from '@repo/lib/shared/services/api/generated/graphql'
 import { TransactionConfig } from '../../web3/contracts/contract.types'
 import { SdkBuildSwapInputs, SdkSimulateSwapResponse, SimulateSwapInputs } from '../swap.types'
 import { SwapHandler } from './Swap.handler'
@@ -15,7 +13,6 @@ import { ProtocolVersion } from '../../pool/pool.types'
  * Base abstract class that shares common logic shared by Default standard swaps and single pool swaps.
  */
 export abstract class BaseDefaultSwapHandler implements SwapHandler {
-
   public abstract name: string
   public abstract simulate({ ...variables }: SimulateSwapInputs): Promise<SdkSimulateSwapResponse>
 
@@ -47,45 +44,49 @@ export abstract class BaseDefaultSwapHandler implements SwapHandler {
   /**
    * PRIVATE METHODS
    */
-  protected async runSimulation({protocolVersion, swapInputs, paths, hopCount}: {
+  protected async runSimulation({
+    protocolVersion,
+    swapInputs,
+    paths,
+    hopCount,
+  }: {
     protocolVersion: ProtocolVersion
-    swapInputs:  SimulateSwapInputs
-    paths: Path[],
+    swapInputs: SimulateSwapInputs
+    paths: Path[]
     hopCount: number
-  }):Promise<SdkSimulateSwapResponse> {
+  }): Promise<SdkSimulateSwapResponse> {
+    const { chain, swapType, swapAmount } = swapInputs
 
-      const {chain, swapType, swapAmount} = swapInputs
+    // Get accurate return amount with onchain call
+    const rpcUrl = getRpcUrl(getChainId(chain))
 
-      // Get accurate return amount with onchain call
-      const rpcUrl = getRpcUrl(getChainId(chain))
+    const swap = new Swap({
+      chainId: getChainId(chain),
+      paths,
+      swapKind: this.swapTypeToKind(swapType),
+    })
 
-      const swap = new Swap({
-        chainId: getChainId(chain),
-        paths,
-        swapKind: this.swapTypeToKind(swapType),
-      })
+    const queryOutput = await swap.query(rpcUrl)
+    let onchainReturnAmount: TokenAmount
+    if (queryOutput.swapKind === SwapKind.GivenIn) {
+      onchainReturnAmount = queryOutput.expectedAmountOut
+    } else {
+      onchainReturnAmount = queryOutput.expectedAmountIn
+    }
 
-      const queryOutput = await swap.query(rpcUrl)
-      let onchainReturnAmount: TokenAmount
-      if (queryOutput.swapKind === SwapKind.GivenIn) {
-        onchainReturnAmount = queryOutput.expectedAmountOut
-      } else {
-        onchainReturnAmount = queryOutput.expectedAmountIn
-      }
+    // Format return amount to human readable
+    const returnAmount = formatUnits(onchainReturnAmount.amount, onchainReturnAmount.token.decimals)
 
-      // Format return amount to human readable
-      const returnAmount = formatUnits(onchainReturnAmount.amount, onchainReturnAmount.token.decimals)
-
-      return {
-        protocolVersion,
-        hopCount,
-        swapType,
-        returnAmount,
-        swap,
-        queryOutput,
-        effectivePrice: bn(swapAmount).div(returnAmount).toString(),
-        effectivePriceReversed: bn(returnAmount).div(swapAmount).toString(),
-      }
+    return {
+      protocolVersion,
+      hopCount,
+      swapType,
+      returnAmount,
+      swap,
+      queryOutput,
+      effectivePrice: bn(swapAmount).div(returnAmount).toString(),
+      effectivePriceReversed: bn(returnAmount).div(swapAmount).toString(),
+    }
   }
 
   protected swapTypeToKind(swapType: GqlSorSwapType): SwapKind {
