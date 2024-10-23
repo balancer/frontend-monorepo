988c9f4c4736e87b82ff712b178609804859bf5c
groninge <groninge@gmail.com>
2024-10-23 16:40:08 +0200

    revert change

--- packages/lib/modules/price-impact/PriceImpactAccordion.tsx
+++ packages/lib/modules/price-impact/PriceImpactAccordion.tsx
@@ -22,7 +22,6 @@ import { usePriceImpact } from '@repo/lib/modules/price-impact/PriceImpactProvid
 import { fNum } from '@repo/lib/shared/utils/numbers'
 import { ReactNode, useEffect } from 'react'
 import { PriceImpactAcceptModal } from './PriceImpactAcceptModal'
-import { useTokenInputsValidation } from '../tokens/TokenInputsValidationProvider'
 
 interface PriceImpactAccordionProps {
   setNeedsToAcceptPIRisk: (value: boolean) => void
@@ -40,7 +39,6 @@ export function PriceImpactAccordion({
   cannotCalculatePriceImpact = false,
 }: PriceImpactAccordionProps) {
   const acceptHighImpactDisclosure = useDisclosure()
-  const { hasValidationErrors } = useTokenInputsValidation()
 
   const {
     priceImpactLevel,
@@ -91,70 +89,69 @@ export function PriceImpactAccordion({
           <AccordionPanel py="md">{accordionPanelComponent}</AccordionPanel>
         </AccordionItem>
       </Accordion>
-      {!hasValidationErrors &&
-        (priceImpactLevel === 'high' || priceImpactLevel === 'max' || isUnknownPriceImpact) && (
-          <>
-            <VStack align="start" mt="md" spacing="md" w="full">
-              <Alert status="error">
-                <PriceImpactIcon mt="1" priceImpactLevel={priceImpactLevel} size={24} />
-                <Box ml="md">
-                  <AlertTitle>
+      {(priceImpactLevel === 'high' || priceImpactLevel === 'max' || isUnknownPriceImpact) && (
+        <>
+          <VStack align="start" mt="md" spacing="md" w="full">
+            <Alert status="error">
+              <PriceImpactIcon mt="1" priceImpactLevel={priceImpactLevel} size={24} />
+              <Box ml="md">
+                <AlertTitle>
+                  {isUnknownPriceImpact
+                    ? 'Unknown price impact'
+                    : `Price impact is high: Exceeds ${
+                        priceImpactLevel === 'high' ? '1' : '5'
+                      }.00%`}
+                </AlertTitle>
+                <AlertDescription>
+                  <Text color="font.dark" fontSize="sm">
                     {isUnknownPriceImpact
-                      ? 'Unknown price impact'
-                      : `Price impact is high: Exceeds ${
-                          priceImpactLevel === 'high' ? '1' : '5'
-                        }.00%`}
-                  </AlertTitle>
-                  <AlertDescription>
-                    <Text color="font.dark" fontSize="sm">
-                      {isUnknownPriceImpact
-                        ? 'The price impact cannot be calculated. Only proceed if you know exactly what you are doing.'
-                        : 'The higher the price impact, the worse exchange rate you get for this swap.'}
-                    </Text>
-                  </AlertDescription>
-                </Box>
-              </Alert>
-              <Card variant="subSection">
-                <CardBody>
-                  <Text fontWeight="bold" mb="sm">
-                    Price impact acknowledgement
+                      ? 'The price impact cannot be calculated. Only proceed if you know exactly what you are doing.'
+                      : 'The higher the price impact, the worse exchange rate you get for this swap.'}
                   </Text>
-                  {isUnknownPriceImpact ? (
-                    <Text color="grayText" fontSize="sm">
-                      I accept that the price impact of this transaction is unknown. I understand
-                      that proceeding may result in losses if my transaction moves the market price
-                      unfavorably based on the current depth of the market.
-                    </Text>
-                  ) : (
-                    <Text color="grayText" fontSize="sm">
-                      I accept the high price impact of{' '}
-                      {priceImpact && fNum('priceImpact', priceImpact)}. I understand that this may
-                      result in losses, since the size of my swap is likely to move the market price
-                      unfavorably based on the current depth of the market.
-                    </Text>
-                  )}
-                </CardBody>
-                <CardFooter pt="md">
-                  {!acceptPriceImpactRisk ? (
-                    <Button onClick={handleClick} variant="secondary" w="full">
-                      I accept {isUnknownPriceImpact ? 'unknown' : 'high'} price impact
-                    </Button>
-                  ) : (
-                    <Button isDisabled variant="secondary" w="full">
-                      {isUnknownPriceImpact ? 'Unknown' : 'High'} price impact accepted
-                    </Button>
-                  )}
-                </CardFooter>
-              </Card>
-            </VStack>
-            <PriceImpactAcceptModal
-              isOpen={acceptHighImpactDisclosure.isOpen}
-              onClose={acceptHighImpactDisclosure.onClose}
-              onOpen={acceptHighImpactDisclosure.onOpen}
-              setAcceptHighPriceImpact={setAcceptPriceImpactRisk}
-            />
-          </>
-        )}
+                </AlertDescription>
+              </Box>
+            </Alert>
+            <Card variant="subSection">
+              <CardBody>
+                <Text fontWeight="bold" mb="sm">
+                  Price impact acknowledgement
+                </Text>
+                {isUnknownPriceImpact ? (
+                  <Text color="grayText" fontSize="sm">
+                    I accept that the price impact of this transaction is unknown. I understand that
+                    proceeding may result in losses if my transaction moves the market price
+                    unfavorably based on the current depth of the market.
+                  </Text>
+                ) : (
+                  <Text color="grayText" fontSize="sm">
+                    I accept the high price impact of{' '}
+                    {priceImpact && fNum('priceImpact', priceImpact)}. I understand that this may
+                    result in losses, since the size of my swap is likely to move the market price
+                    unfavorably based on the current depth of the market.
+                  </Text>
+                )}
+              </CardBody>
+              <CardFooter pt="md">
+                {!acceptPriceImpactRisk ? (
+                  <Button onClick={handleClick} variant="secondary" w="full">
+                    I accept {isUnknownPriceImpact ? 'unknown' : 'high'} price impact
+                  </Button>
+                ) : (
+                  <Button isDisabled variant="secondary" w="full">
+                    {isUnknownPriceImpact ? 'Unknown' : 'High'} price impact accepted
+                  </Button>
+                )}
+              </CardFooter>
+            </Card>
+          </VStack>
+          <PriceImpactAcceptModal
+            isOpen={acceptHighImpactDisclosure.isOpen}
+            onClose={acceptHighImpactDisclosure.onClose}
+            onOpen={acceptHighImpactDisclosure.onOpen}
+            setAcceptHighPriceImpact={setAcceptPriceImpactRisk}
+          />
+        </>
+      )}
     </Box>
   )
 }
