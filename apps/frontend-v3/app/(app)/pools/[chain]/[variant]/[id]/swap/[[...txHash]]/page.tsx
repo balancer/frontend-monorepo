b1eea73f2ddc3bf2aa616bf8c655970871bf155c
Alberto Gualis <agualis@gmail.com>
2024-10-28 11:32:58 +0100

    feat: nested pool swaps

--- apps/frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/swap/[[...txHash]]/page.tsx
+++ apps/frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/swap/[[...txHash]]/page.tsx
@@ -1,16 +1,15 @@
 'use client'
 
 import { PoolActionsLayout } from '@repo/lib/modules/pool/actions/PoolActionsLayout'
-import { getPoolTokens } from '@repo/lib/modules/pool/pool.helpers'
+import { getPoolActionableTokens } from '@repo/lib/modules/pool/pool.helpers'
 import { usePoolRedirect } from '@repo/lib/modules/pool/pool.hooks'
-import { ProtocolVersion } from '@repo/lib/modules/pool/pool.types'
 import { chainToSlugMap } from '@repo/lib/modules/pool/pool.utils'
 import { usePool } from '@repo/lib/modules/pool/PoolProvider'
 import { SwapForm } from '@repo/lib/modules/swap/SwapForm'
 import SwapLayout from '@repo/lib/modules/swap/SwapLayout'
 import { PathParams, SwapProviderProps } from '@repo/lib/modules/swap/SwapProvider'
 import { useTokens } from '@repo/lib/modules/tokens/TokensProvider'
-import { Hash, Hex } from 'viem'
+import { Hash } from 'viem'
 
 type Props = {
   params: { txHash?: string[] }
@@ -21,21 +20,20 @@ export default function PoolSwapPage({ params: { txHash } }: Props) {
   const { pool, isLoading } = usePool()
   const { redirectToPoolPage } = usePoolRedirect(pool)
 
-  const poolTokens = getPoolTokens(pool, getToken)
+  const poolActionableTokens = getPoolActionableTokens(pool, getToken)
 
   const maybeTxHash = (txHash?.[0] as Hash) || undefined
 
   const pathParams: PathParams = {
     chain: chainToSlugMap[pool.chain],
-    tokenIn: poolTokens[0].address,
-    tokenOut: poolTokens[1].address,
+    tokenIn: poolActionableTokens[0].address,
+    tokenOut: poolActionableTokens[1].address,
     urlTxHash: maybeTxHash,
   }
   const props: SwapProviderProps = {
     pathParams,
-    poolTokens,
-    poolId: pool.id as Hex,
-    poolVersion: pool.protocolVersion as ProtocolVersion,
+    pool,
+    poolActionableTokens: poolActionableTokens || [],
   }
 
   return (
