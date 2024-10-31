4416ede50940f6272fe9c92ee356cd10f4f2a231
Alberto Gualis <agualis@gmail.com>
2024-10-31 12:52:50 +0100

    chore: bump SDK 0.30.1

--- packages/lib/config/networks/mainnet.ts
+++ packages/lib/config/networks/mainnet.ts
@@ -4,6 +4,7 @@ import { convertHexToLowerCase } from '@repo/lib/shared/utils/objects'
 import { NetworkConfig } from '../config.types'
 import { CSP_ISSUE_POOL_IDS } from '../../shared/data/csp-issue'
 import { SupportedWrapHandler } from '@repo/lib/modules/swap/swap.types'
+import { Address } from 'viem'
 
 const networkConfig: NetworkConfig = {
   chainId: 1,
@@ -76,6 +77,7 @@ const networkConfig: NetworkConfig = {
       vaultV2: '0xBA12222222228d8Ba445958a75a0704d566BF2C8',
       relayerV6: '0x35Cea9e57A393ac66Aaa7E25C391D52C74B5648f',
       minter: '0x239e55F427D44C3cc793f49bFB507ebe76638a2b',
+      balancerRouter: '0xNotYetAvailable' as Address,
     },
     feeDistributor: '0xD3cf852898b21fc233251427c2DC93d3d604F3BB',
     veDelegationProxy: '0x6f5a2eE11E7a772AeB5114A20d0D7c0ff61EB8A0',
