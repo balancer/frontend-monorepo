0964bc5d30580d562e5884c53f72e96fa4f0bdd4
Alberto Gualis <agualis@gmail.com>
2024-11-07 16:11:41 +0100

    chore: add test for boostedPoolStateWithBalances

--- packages/lib/modules/pool/actions/LiquidityActionHelpers.integration.spec.ts
+++ packages/lib/modules/pool/actions/LiquidityActionHelpers.integration.spec.ts
@@ -98,13 +98,13 @@ describe('Calculates toInputAmounts from allPoolTokens', () => {
 })
 
 // Unskip when sepolia V3 pools are available in production api
-describe.skip('Liquidity helpers for V3 Boosted pools', async () => {
+describe.only('Liquidity helpers for V3 Boosted pools', async () => {
   const poolId = '0x6dbdd7a36d900083a5b86a55583d90021e9f33e8' // Sepolia stataEthUSDC stataEthUSDT
 
   const usdcSepoliaAddress = '0x94a9d9ac8a22534e3faca9f4e7f2e2cf85d5e4c8'
   const usdtSepoliaAddress = '0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0'
-  // const v3Pool = await getPoolMock(poolId, GqlChain.Sepolia)
-  const v3Pool = {} as GqlPoolElement
+  const v3Pool = await getPoolMock(poolId, GqlChain.Sepolia)
+  // const v3Pool = {} as GqlPoolElement
   const helpers = new LiquidityActionHelpers(v3Pool)
 
   const humanAmountsIn: HumanTokenAmountWithAddress[] = [
@@ -207,6 +207,31 @@ describe.skip('Liquidity helpers for V3 Boosted pools', async () => {
           weight: null,
         },
       ],
+      type: 'Stable',
+    })
+  })
+
+  it('poolStateWithBalances (that calls boostedPoolStateWithBalances underneath)', async () => {
+    const helpers = new LiquidityActionHelpers(v3Pool)
+    expect(helpers.poolStateWithBalances).toEqual({
+      address: '0x6dbdd7a36d900083a5b86a55583d90021e9f33e8',
+      id: '0x6dbdd7a36d900083a5b86a55583d90021e9f33e8',
+      protocolVersion: 3,
+      tokens: [
+        {
+          address: '0x94a9d9ac8a22534e3faca9f4e7f2e2cf85d5e4c8',
+          balance: expect.any(String),
+          decimals: 6,
+          index: 0,
+        },
+        {
+          address: '0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0',
+          balance: expect.any(String),
+          decimals: 6,
+          index: 1,
+        },
+      ],
+      totalShares: expect.any(String),
       type: 'Stable',
     })
   })
