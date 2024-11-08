4b0a1d2f7914957503e712b55066c9913c6e9e5d
groninge <groninge@gmail.com>
2024-11-08 07:47:44 +0100

    update swap

--- apps/beets-frontend-v3/app/(app)/swap/[[...slug]]/layout.tsx
+++ apps/beets-frontend-v3/app/(app)/swap/[[...slug]]/layout.tsx
@@ -1,47 +1,24 @@
 'use client'
 
-import { ChainSlug, slugToChainMap } from '@repo/lib/modules/pool/pool.utils'
-import { SwapProviderProps, SwapProvider } from '@repo/lib/modules/swap/SwapProvider'
-import { TokenBalancesProvider } from '@repo/lib/modules/tokens/TokenBalancesProvider'
-import { TokenInputsValidationProvider } from '@repo/lib/modules/tokens/TokenInputsValidationProvider'
-import { useTokens } from '@repo/lib/modules/tokens/TokensProvider'
-import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
-import { GqlChain } from '@repo/lib/shared/services/api/generated/graphql'
 import { PropsWithChildren } from 'react'
-import { PriceImpactProvider } from '@repo/lib/modules/price-impact/PriceImpactProvider'
-import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
 import { getSwapPathParams } from '@repo/lib/modules/swap/getSwapPathParams'
-import { RelayerSignatureProvider } from '@repo/lib/modules/relayer/RelayerSignatureProvider'
+import SwapLayout from '@repo/lib/modules/swap/SwapLayout'
+import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
+import { SwapProviderProps } from '@repo/lib/modules/swap/SwapProvider'
 
 type Props = PropsWithChildren<{
   params: { slug?: string[] }
 }>
 
-export default function SwapLayout({ params: { slug }, children }: Props) {
+export default function Layout({ params: { slug }, children }: Props) {
   const pathParams = getSwapPathParams(slug)
-
-  const { getTokensByChain } = useTokens()
-  const initChain = pathParams.chain
-    ? slugToChainMap[pathParams.chain as ChainSlug]
-    : GqlChain.Fantom
-  const initTokens = getTokensByChain(initChain)
-  const props: SwapProviderProps = {
+  const swapProps: SwapProviderProps = {
     pathParams,
   }
 
   return (
     <DefaultPageContainer minH="100vh">
-      <TransactionStateProvider>
-        <RelayerSignatureProvider>
-          <TokenInputsValidationProvider>
-            <TokenBalancesProvider initTokens={initTokens}>
-              <PriceImpactProvider>
-                <SwapProvider params={props}>{children}</SwapProvider>
-              </PriceImpactProvider>
-            </TokenBalancesProvider>
-          </TokenInputsValidationProvider>
-        </RelayerSignatureProvider>
-      </TransactionStateProvider>
+      <SwapLayout props={swapProps}> {children} </SwapLayout>
     </DefaultPageContainer>
   )
 }
