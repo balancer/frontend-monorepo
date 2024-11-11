ce0d312a399f699bb11c55d46960f78eebd268dc
groninge <groninge@gmail.com>
2024-11-11 13:11:01 +0100

    fix linting error

--- packages/lib/modules/tokens/TokenRow/TokenRow.tsx
+++ packages/lib/modules/tokens/TokenRow/TokenRow.tsx
@@ -164,6 +164,7 @@ export default function TokenRow({
 
       setAmount(fNum('token', value, { abbreviated }))
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value])
 
   const headingProps = {
