6fa411d76e03f73a7dcc01d3de6d39dece6092d2
groninge <groninge@gmail.com>
2024-11-12 08:41:52 +0100

    revert unknown property fix

--- packages/lib/shared/components/marketing/HomeHero.tsx
+++ packages/lib/shared/components/marketing/HomeHero.tsx
@@ -1,11 +1,11 @@
+/* eslint-disable react/no-unknown-property */
 'use client'
+
 import { Button, Heading, Text, VStack, Flex, Box, useToken } from '@chakra-ui/react'
 import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
 import NextLink from 'next/link'
 import { Link } from '@chakra-ui/react'
-
 import { ArrowUpRight } from 'react-feather'
-
 import { HomeCaseStudies } from '@repo/lib/shared/components/marketing/HomeCaseStudies'
 
 export function HomeHero() {
@@ -137,13 +137,13 @@ export function HomeHero() {
                   </Text>
                   <button
                     className="btn-popover"
-                    data-popover-target="disclose"
-                    data-popover-target-action="toggle"
+                    popovertarget="disclose"
+                    popovertargetaction="toggle"
                     type="button"
                   >
                     Start building
                   </button>
-                  <div className="disclosure" data-popover="auto" id="disclose">
+                  <div className="disclosure" id="disclose" popover="auto">
                     <FadeInOnView>
                       <Box>
                         <header>
