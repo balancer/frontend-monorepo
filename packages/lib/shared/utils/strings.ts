0d637dc1126eda7ddb2140f7afbcdbc1d232c139
groninge <groninge@gmail.com>
2024-11-14 16:27:29 +0100

    add string util

--- packages/lib/shared/utils/strings.ts
+++ packages/lib/shared/utils/strings.ts
@@ -21,3 +21,6 @@ export function arrayToSentence(arr: string[]): string {
   const lastElement = arr.pop()
   return arr.join(', ') + ', and ' + lastElement
 }
+
+export const convertCamelCaseToTitleCase = (s: string): string =>
+  s.replace(/([A-Z])/g, ' $1').trim()
