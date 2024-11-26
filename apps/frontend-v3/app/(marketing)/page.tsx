87d16fb9ce185d2864b03a0cbac0156e7adf2135
Gareth Fuller <gareth.a.f@gmail.com>
2024-11-26 08:50:54 +0000

    chore: prefetch bg images

--- apps/frontend-v3/app/(marketing)/page.tsx
+++ apps/frontend-v3/app/(marketing)/page.tsx
@@ -1,5 +1,16 @@
 import { LandingV3 } from '@repo/lib/modules/marketing/landing-v3/LandingV3'
+import Head from 'next/head'
 
 export default function Home() {
-  return <LandingV3 />
+  return (
+    <>
+      <Head>
+        <link href="/images/bgs/zenbg-1.webp" rel="prefetch" />
+        <link href="/images/bgs/zenbg-2.webp" rel="prefetch" />
+        <link href="/images/bgs/zenbg-3.webp" rel="prefetch" />
+        <link href="/images/bgs/zenbg-4.webp" rel="prefetch" />
+      </Head>
+      <LandingV3 />
+    </>
+  )
 }
