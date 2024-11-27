3b4f9ef31f27a3e8458ea19e955b0ce3133b52bf
groninge <groninge@gmail.com>
2024-11-27 09:55:42 +0100

    refactor

--- packages/lib/modules/pool/PoolProvider.tsx
+++ packages/lib/modules/pool/PoolProvider.tsx
@@ -13,64 +13,51 @@ import { FetchPoolProps } from './pool.types'
 import { useMandatoryContext } from '@repo/lib/shared/utils/contexts'
 import { usePoolHelpers } from './pool.helpers'
 import { useUserAccount } from '@repo/lib/modules/web3/UserAccountProvider'
-import { usePoolEnrichWithOnChainData } from '@repo/lib/modules/pool/queries/usePoolEnrichWithOnChainData'
+import { usePoolEnrichWithOnChainData } from './queries/usePoolEnrichWithOnChainData'
 import { useOnchainUserPoolBalances } from './queries/useOnchainUserPoolBalances'
 import { useInvalidVariantRedirect } from './pool.hooks'
 import { getPoolDisplayTokens } from './pool.utils'
 import { useTokens } from '../tokens/TokensProvider'
 
-export type UsePoolResponse = ReturnType<typeof _usePool> & {
-  chain: GqlChain
-}
-export const PoolContext = createContext<UsePoolResponse | null>(null)
-
 export type Pool = GetPoolQuery['pool']
 export type FeaturedPool = GetFeaturedPoolsQuery['featuredPools'][0]['pool']
+export type UsePoolResponse = ReturnType<typeof usePoolData> & {
+  chain: GqlChain
+}
 
-export function _usePool({
-  id,
-  chain,
-  initialData,
-}: FetchPoolProps & { initialData: GetPoolQuery }) {
-  useInvalidVariantRedirect(initialData.pool)
+export const PoolContext = createContext<UsePoolResponse | null>(null)
+
+function usePoolData({ id, chain, initialData }: FetchPoolProps & { initialData: GetPoolQuery }) {
   const { userAddress } = useUserAccount()
-  const { priceFor } = useTokens()
-
-  const queryVariables = { id, chain, userAddress: userAddress.toLowerCase() }
-
+  const { priceFor, calcTotalUsdValue } = useTokens()
   const myLiquiditySectionRef = useRef<HTMLDivElement | null>(null)
 
+  useInvalidVariantRedirect(initialData.pool)
+
   const { data } = useQuery(GetPoolDocument, {
-    variables: queryVariables,
+    variables: { id, chain, userAddress: userAddress.toLowerCase() },
   })
 
-  const { calcTotalUsdValue } = useTokens()
   const {
     pool: poolWithOnChainData,
     refetch: refetchOnchainData,
     isLoading: isLoadingOnchainData,
   } = usePoolEnrichWithOnChainData(data?.pool || initialData.pool)
 
-  // fallbacks to ensure the pool is always present. We prefer the pool with on chain data
-  let pool: Pool = poolWithOnChainData || data?.pool || initialData.pool
-
   const {
     data: [poolWithOnchainUserBalances],
     refetch: refetchOnchainUserBalances,
     isLoading: isLoadingOnchainUserBalances,
-  } = useOnchainUserPoolBalances([pool])
+  } = useOnchainUserPoolBalances([poolWithOnChainData || data?.pool || initialData.pool])
 
-  pool = poolWithOnchainUserBalances || pool
-
+  const pool = poolWithOnchainUserBalances || poolWithOnChainData || data?.pool || initialData.pool
   const bptPrice = priceFor(pool.address, pool.chain)
-
   const tvl = calcTotalUsdValue(getPoolDisplayTokens(pool), pool.chain)
+  const isLoading = isLoadingOnchainData || isLoadingOnchainUserBalances
 
-  async function refetch() {
+  const refetch = async () => {
     return Promise.all([refetchOnchainData(), refetchOnchainUserBalances()])
   }
-
-  const isLoading = isLoadingOnchainData || isLoadingOnchainUserBalances
 
   return {
     pool,
@@ -94,12 +81,8 @@ export function PoolProvider({
   children,
   data,
 }: PropsWithChildren<FetchPoolProps> & { data: GetPoolQuery }) {
-  const hook = _usePool({ id, chain, variant, initialData: data })
-  const payload = {
-    ...hook,
-    chain,
-  }
-  return <PoolContext.Provider value={payload}>{children}</PoolContext.Provider>
+  const hook = usePoolData({ id, chain, variant, initialData: data })
+  return <PoolContext.Provider value={{ ...hook, chain }}>{children}</PoolContext.Provider>
 }
 
 export const usePool = (): UsePoolResponse => useMandatoryContext(PoolContext, 'Pool')
