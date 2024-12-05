ce7f93b2bff45f565716e1b89c0e4627b6ebc646
Alberto Gualis <agualis@gmail.com>
2024-12-05 23:19:56 +0100

    refactor: nested add generic error text (#240)
    * fix: improve nested adds error handling
    
    * fix: use undefined instead of '' userAddress when user is not connected
    
    * refactor: extract discord url to config
    
    * refactor: generic nested add error

--- packages/lib/shared/components/errors/UnbalancedNestedAddError.tsx
+++ packages/lib/shared/components/errors/UnbalancedNestedAddError.tsx
@@ -11,11 +11,23 @@ type Props = AlertProps & {
 export function UnbalancedNestedAddError({ error }: Props) {
   console.log('Nested pool add error: ', error)
 
+  /*
+    Possible errors include:
+
+    - MaxRatio error due to add liquidity amounts being too unbalanced
+    - MaxRatio error due to add liquidity amounts being too large
+    - MinRatio error due to removeLiquidity amounts being too unbalanced
+    - MinRatio error due to removeLiquidity amounts being too large
+    - Pi calculation error (several possible reasons, but most often happens with near proportional amounts)
+    - Query tx reverted due arithmetic overflow
+
+    We can't always differentiate between these errors, so we'll show a generic message until we have more info/ user feedback.
+  */
   return (
     <ErrorAlert title="Unbalanced amounts">
       <Text color="black" variant="secondary">
-        Your input(s) would fail with the current pool balances. Please try more proportional
-        amounts or report the issue in{' '}
+        Your input(s) are either too large or would excessively unbalance the pool, please try
+        smaller/more proportional amounts or report the issue in{' '}
         <BalAlertLink href={getProjectConfig().externalLinks.discordUrl}>our discord</BalAlertLink>.
       </Text>
     </ErrorAlert>
