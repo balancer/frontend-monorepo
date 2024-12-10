603e3ca7f19e99304b284c1f5fe03db8a59a2174
Daniel <danielBeets@protonmail.com>
2024-12-10 10:38:31 +0800

    First pass on moving the theme towards the new brand

--- apps/beets-frontend-v3/lib/services/chakra/themes/beets/beets.theme.ts
+++ apps/beets-frontend-v3/lib/services/chakra/themes/beets/beets.theme.ts
@@ -1,12 +1,12 @@
 import { ThemeTypings, extendTheme } from '@chakra-ui/react'
 import { colors, primaryTextColor } from './colors'
-import { getTokens } from '@repo/lib/shared/services/chakra/themes/base/tokens'
 import { getComponents } from '@repo/lib/shared/services/chakra/themes/base/components'
 import { config, fonts, styles } from '@repo/lib/shared/services/chakra/themes/base/foundations'
 import { getSemanticTokens } from '@repo/lib/shared/services/chakra/themes/base/semantic-tokens'
 import { proseTheme } from '@repo/lib/shared/services/chakra/themes/base/prose'
+import { getBeetsTokens } from './tokens'
 
-const tokens = getTokens(colors, primaryTextColor)
+const tokens = getBeetsTokens(colors, primaryTextColor)
 const components = getComponents(tokens, primaryTextColor)
 const semanticTokens = getSemanticTokens(tokens, colors)
 
