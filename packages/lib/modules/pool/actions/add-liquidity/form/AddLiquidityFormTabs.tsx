a961ed4131c0f9943128a9d3866c9b38d5123f65
Alberto Gualis <agualis@gmail.com>
2024-12-10 13:47:22 +0100

    fix: avoid min TVL check in non v3 pools2

--- packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormTabs.tsx
+++ packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormTabs.tsx
@@ -13,6 +13,7 @@ import {
 import { useAddLiquidity } from '../AddLiquidityProvider'
 import { TokenInputsMaybeProportional } from './TokenInputsMaybeProportional'
 import { useCurrency } from '@repo/lib/shared/hooks/useCurrency'
+import { isV3Pool } from '../../../pool.helpers'
 const MIN_LIQUIDITY_FOR_BALANCED_ADD = 50000
 
 export function AddLiquidityFormTabs({
