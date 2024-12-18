353bf79da5a9eac363fcbb9d6744a00c923e28ed
Daniel <danielBeets@protonmail.com>
2024-12-18 16:11:00 +0800

    iterating on migration flow

--- apps/beets-frontend-v3/lib/components/navs/MaBeetsNavLink.tsx
+++ apps/beets-frontend-v3/lib/components/navs/MaBeetsNavLink.tsx
@@ -61,8 +61,8 @@ export function MaBeetsRedirectModal({
           </Text>
 
           <Text color="font.secondary" mt="md">
-            It's recommended that you migrate any Fantom maBEETS relics to Sonic. For more
-            information, refrer to the migration guide at{' '}
+            It's recommended that you migrate Fantom maBEETS relics to Sonic. For more information,
+            refrer to the migration guide at{' '}
             <Link href="https://docs.beets.fi" target="_blank">
               https://docs.beets.fi
             </Link>
