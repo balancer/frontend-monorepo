dc375a694012cf90ea53a0569b686c91dc012062
Gareth Fuller <gareth.a.f@gmail.com>
2024-12-18 11:55:15 +0000

    fix: lint-staged config

--- apps/beets-frontend-v3/.lintstagedrc.mjs
+++ apps/beets-frontend-v3/.lintstagedrc.mjs
@@ -1,22 +1,6 @@
-// https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
-const buildEslintCommand = filenames =>
-  `next lint --fix --max-warnings 0 --file ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' --file ')}`
-
-const buildTypecheckCommand = filenames =>
-  `tsc --project tsconfig.json --noEmit --file ${filenames
-    .map(f => path.relative(process.cwd(), f))
-    .join(' --file ')}`
-
-const buildPrettierCommand = filenames =>
-  `prettier --write ${filenames.join(' ')}`
-
-const buildStylelintCommand = filenames =>
-  `stylelint --fix ${filenames.join(' ')}`
-
 export default {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildTypecheckCommand, buildPrettierCommand, buildStylelintCommand],
-  '*.{md,json,yaml,ts,tsx}': buildPrettierCommand,
-  '*.css': buildStylelintCommand,
+  '*.{js,jsx,ts,tsx}': ['bash -c "pnpm lint:fix"'],
+  '**/*.ts?(x)': 'bash -c "pnpm typecheck"',
+  '*.{md,json,yaml,ts,tsx}': 'prettier --write',
+  '*.css': 'stylelint --fix',
 }
