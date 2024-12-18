cdb2ffb9abb8b24b4865a16c78d934086210ed56
Gareth Fuller <gareth.a.f@gmail.com>
2024-12-18 14:08:04 +0000

    chore: Fix pre-lint command

--- apps/beets-frontend-v3/.lintstagedrc.mjs
+++ apps/beets-frontend-v3/.lintstagedrc.mjs
@@ -1,6 +1,7 @@
 export default {
-  '*.{js,jsx,ts,tsx}': ['bash -c "pnpm lint:fix"'],
+  '*.{js,jsx,ts,tsx}': "eslint --fix --cache --max-warnings 0",
   '**/*.ts?(x)': 'bash -c "pnpm typecheck"',
   '*.{md,json,yaml,ts,tsx}': 'prettier --write',
   '*.css': 'stylelint --fix',
 }
+
