78ef40a201320e6fab1dba0214d1987ba8ad64c6
Gareth Fuller <gareth.a.f@gmail.com>
2024-12-18 10:52:08 +0000

    chore: update lint-staged config

--- apps/beets-frontend-v3/.lintstagedrc.mjs
+++ apps/beets-frontend-v3/.lintstagedrc.mjs
@@ -1,13 +1,22 @@
 // https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
-import baseConfig from '../../.lintstagedrc.cjs'
-import path from 'path'
-
 const buildEslintCommand = filenames =>
   `next lint --fix --max-warnings 0 --file ${filenames
     .map(f => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const buildTypecheckCommand = filenames =>
+  `tsc --project tsconfig.json --noEmit --file ${filenames
+    .map(f => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const buildPrettierCommand = filenames =>
+  `prettier --write ${filenames.join(' ')}`
+
+const buildStylelintCommand = filenames =>
+  `stylelint --fix ${filenames.join(' ')}`
+
 export default {
-  ...baseConfig,
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'bash -c "pnpm run typecheck"'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildTypecheckCommand, buildPrettierCommand, buildStylelintCommand],
+  '*.{md,json,yaml,ts,tsx}': buildPrettierCommand,
+  '*.css': buildStylelintCommand,
 }
--- apps/frontend-v3/.lintstagedrc.mjs
+++ apps/frontend-v3/.lintstagedrc.mjs
@@ -1,5 +1,5 @@
 // https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
-import baseConfig from '../../.lintstagedrc.cjs'
+// import baseConfig from '../../.lintstagedrc.cjs'
 import path from 'path'
 
 const buildEslintCommand = filenames =>
@@ -7,7 +7,19 @@ const buildEslintCommand = filenames =>
     .map(f => path.relative(process.cwd(), f))
     .join(' --file ')}`
 
+const buildTypecheckCommand = filenames =>
+  `tsc --project tsconfig.json --noEmit --file ${filenames
+    .map(f => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+const buildPrettierCommand = filenames =>
+  `prettier --write ${filenames.join(' ')}`
+
+const buildStylelintCommand = filenames =>
+  `stylelint --fix ${filenames.join(' ')}`
+
 export default {
-  ...baseConfig,
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand, 'bash -c "pnpm run typecheck"'],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildTypecheckCommand, buildPrettierCommand, buildStylelintCommand],
+  '*.{md,json,yaml,ts,tsx}': buildPrettierCommand,
+  '*.css': buildStylelintCommand,
 }
