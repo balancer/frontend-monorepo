86eb9ea2e88ef1181a920381b6916c422a08b2f9
Daniel <danielBeets@protonmail.com>
2024-12-18 18:08:40 +0800

    set a metadata base

--- apps/beets-frontend-v3/app/(marketing)/layout.tsx
+++ apps/beets-frontend-v3/app/(marketing)/layout.tsx
@@ -3,11 +3,6 @@ import { Box } from '@chakra-ui/react'
 import { Metadata } from 'next'
 import { PropsWithChildren } from 'react'
 
-export const metadata: Metadata = {
-  title: 'Beets',
-  description: `The Flagship LST Hub on Sonic. From seamless staking to earning real yield on LST-focused liquidity pools, beets is the ultimate destination for your liquid-staked tokens.`,
-}
-
 export default function MarketingLayout({ children }: PropsWithChildren) {
   return <Box>{children}</Box>
 }
