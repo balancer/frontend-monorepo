0269bd1967996ae899367e1b42e63a6233dee5e6
groninge <groninge@gmail.com>
2024-12-19 08:48:59 +0100

    add & view sts data

--- packages/lib/modules/beets/lst/Lst.tsx
+++ packages/lib/modules/beets/lst/Lst.tsx
@@ -19,12 +19,16 @@ import {
   Tooltip,
   useDisclosure,
   VStack,
+  Text,
+  Skeleton,
+  Icon,
+  BoxProps,
 } from '@chakra-ui/react'
 import { ConnectWallet } from '@repo/lib/modules/web3/ConnectWallet'
 import { useUserAccount } from '@repo/lib/modules/web3/UserAccountProvider'
 import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
 import { useIsMounted } from '@repo/lib/shared/hooks/useIsMounted'
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useMemo, useRef, useState } from 'react'
 import ButtonGroup, {
   ButtonGroupOption,
 } from '@repo/lib/shared/components/btns/button-group/ButtonGroup'
@@ -39,6 +43,10 @@ import { LstWithdraw } from './components/LstWithdraw'
 import { useGetUserWithdraws, UserWithdraw } from './hooks/useGetUserWithdraws'
 import { useGetUserNumWithdraws } from './hooks/useGetUserNumWithdraws'
 import { useGetStakedSonicData } from './hooks/useGetStakedSonicData'
+import { bn, fNum } from '@repo/lib/shared/utils/numbers'
+import { ZenGarden } from '@repo/lib/shared/components/zen/ZenGarden'
+import { NoisyCard } from '@repo/lib/shared/components/containers/NoisyCard'
+import { ArrowRight } from 'react-feather'
 
 const FAQ_ITEMS = [
   {
@@ -62,6 +70,21 @@ const FAQ_ITEMS = [
       'stS is fully liquid, meaning you can use stS seamlessly across DeFi and access lending markets, liquidity pools, and more without pausing your rewards.',
   },
 ]
+
+const COMMON_NOISY_CARD_PROPS: { contentProps: BoxProps; cardProps: BoxProps } = {
+  contentProps: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomLeftRadius: 'none',
+    borderTopLeftRadius: 'none',
+    borderBottomRightRadius: 'none',
+  },
+  cardProps: {
+    position: 'relative',
+    height: 'full',
+  },
+}
 
 export function Lst() {
   const { isConnected } = useUserAccount()
@@ -87,7 +110,10 @@ export function Lst() {
   const { isBalancesLoading } = useTokenBalances()
   const [disclosure, setDisclosure] = useState(stakeModalDisclosure)
   const { userNumWithdraws, isLoading: isUserNumWithdrawsLoading } = useGetUserNumWithdraws(chain)
-  const { data, isLoading: isWithdrawalsLoading } = useGetUserWithdraws(chain, userNumWithdraws)
+  const { data: UserWithdraws, isLoading: isWithdrawalsLoading } = useGetUserWithdraws(
+    chain,
+    userNumWithdraws
+  )
   const { data: stakedSonicData, loading: isStakedSonicDataLoading } = useGetStakedSonicData()
 
   const isLoading =
@@ -125,6 +151,20 @@ export function Lst() {
       setAmountShares('')
     }
   }, [activeTab])
+
+  const tokenIn = useMemo(() => (isStakeTab ? 'S' : 'stS'), [isStakeTab])
+
+  const tokenOut = useMemo(() => (isStakeTab ? 'stS' : 'S'), [isStakeTab])
+
+  const rate = useMemo(() => {
+    const rate = stakedSonicData?.stsGetGqlStakedSonicData.exchangeRate || '1'
+
+    if (isStakeTab) {
+      return bn(1).div(bn(rate))
+    }
+
+    return rate
+  }, [isStakeTab, stakedSonicData])
 
   function onModalClose() {
     // restart polling for token prices when modal is closed again
@@ -176,7 +216,7 @@ export function Lst() {
               {isWithdrawTab && !isWithdrawalsLoading && (
                 <LstWithdraw
                   isLoading={isWithdrawalsLoading || isUserNumWithdrawsLoading}
-                  withdrawalsData={data as UserWithdraw[]}
+                  withdrawalsData={UserWithdraws as UserWithdraw[]}
                 />
               )}
             </CardBody>
@@ -208,6 +248,54 @@ export function Lst() {
               )}
             </CardFooter>
           </Card>
+          {!isWithdrawTab && (
+            <Card position="relative">
+              <NoisyCard
+                cardProps={COMMON_NOISY_CARD_PROPS.cardProps}
+                contentProps={COMMON_NOISY_CARD_PROPS.contentProps}
+              >
+                <Box bottom={0} left={0} overflow="hidden" position="absolute" right={0} top={0}>
+                  <ZenGarden sizePx="280px" subdued variant="circle" />
+                </Box>
+                <VStack
+                  align="flex-start"
+                  h="full"
+                  justify="flex-start"
+                  m="auto"
+                  p={{ base: 'sm', md: 'md' }}
+                  role="group"
+                  spacing="xl"
+                  w="full"
+                  zIndex={1}
+                >
+                  <HStack justify="space-between" w="full">
+                    <Text>APR:</Text>
+                    {isStakedSonicDataLoading ? (
+                      <Skeleton h="full" w="12" />
+                    ) : (
+                      <Text>
+                        {fNum('apr', stakedSonicData?.stsGetGqlStakedSonicData.stakingApr || '0')}
+                      </Text>
+                    )}
+                  </HStack>
+                  <HStack justify="space-between" w="full">
+                    <Text>Rate:</Text>
+                    <HStack>
+                      {isStakedSonicDataLoading ? (
+                        <Skeleton h="full" w="12" />
+                      ) : (
+                        <>
+                          <Text>{`1 ${tokenIn}`}</Text>
+                          <Icon as={ArrowRight} />
+                          <Text>{`${fNum('token', rate)} ${tokenOut}`}</Text>
+                        </>
+                      )}
+                    </HStack>
+                  </HStack>
+                </VStack>
+              </NoisyCard>
+            </Card>
+          )}
           <Card rounded="xl" w="full">
             <CardHeader as={HStack} justify="space-between" w="full">
               <Heading as="h2" size="lg">
