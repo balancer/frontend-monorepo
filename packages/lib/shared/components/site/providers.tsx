30896c35c1cafb84d403c7752f2f46cfb437e51a
groninge <groninge@gmail.com>
2024-12-30 12:32:16 +0000

    add to providers

--- packages/lib/shared/components/site/providers.tsx
+++ packages/lib/shared/components/site/providers.tsx
@@ -4,25 +4,28 @@ import { ReactNode } from 'react'
 import { RecentTransactionsProvider } from '@repo/lib/modules/transactions/RecentTransactionsProvider'
 import { ApolloGlobalDataProvider } from '@repo/lib/shared/services/api/apollo-global-data.provider'
 import { UserSettingsProvider } from '@repo/lib/modules/user/settings/UserSettingsProvider'
-
 import { wagmiConfig } from '@repo/lib/modules/web3/WagmiConfig'
 import { GlobalAlertsProvider } from '@repo/lib/shared/components/alerts/GlobalAlertsProvider'
 import { VebalLockDataProvider } from '@repo/lib/modules/vebal/lock/VebalLockDataProvider'
+import { ProjectConfig } from '@repo/lib/config/config.types'
+import { ProjectConfigProvider } from '@repo/lib/config/ProjectConfigProvider'
 
-export function Providers({ children }: { children: ReactNode }) {
+export function Providers({ config, children }: { config: ProjectConfig; children: ReactNode }) {
   return (
-    <GlobalAlertsProvider>
-      <Web3Provider wagmiConfig={wagmiConfig}>
-        <ApolloClientProvider>
-          <ApolloGlobalDataProvider>
-            <UserSettingsProvider>
-              <VebalLockDataProvider>
-                <RecentTransactionsProvider>{children}</RecentTransactionsProvider>
-              </VebalLockDataProvider>
-            </UserSettingsProvider>
-          </ApolloGlobalDataProvider>
-        </ApolloClientProvider>
-      </Web3Provider>
-    </GlobalAlertsProvider>
+    <ProjectConfigProvider config={config}>
+      <GlobalAlertsProvider>
+        <Web3Provider wagmiConfig={wagmiConfig}>
+          <ApolloClientProvider>
+            <ApolloGlobalDataProvider>
+              <UserSettingsProvider>
+                <VebalLockDataProvider>
+                  <RecentTransactionsProvider>{children}</RecentTransactionsProvider>
+                </VebalLockDataProvider>
+              </UserSettingsProvider>
+            </ApolloGlobalDataProvider>
+          </ApolloClientProvider>
+        </Web3Provider>
+      </GlobalAlertsProvider>
+    </ProjectConfigProvider>
   )
 }
