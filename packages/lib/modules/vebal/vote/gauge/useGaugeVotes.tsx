08a4318db4947601854b61bafe242dacf902e2f5
Anastasios <anastas3993@gmail.com>
2025-01-05 18:33:00 +0300

    chore: pr fixes

--- packages/lib/modules/vebal/vote/gauge/useGaugeVotes.tsx
+++ packages/lib/modules/vebal/vote/gauge/useGaugeVotes.tsx
@@ -163,7 +163,7 @@ export function useGaugeVotes({ gaugeAddresses }: UseGaugeVotesParams) {
     }
 
     const result: Record<string, VotesData> = Object.fromEntries(
-      Object.keys(gaugeAddresses).map((address, index) => {
+      gaugeAddresses.map((address, index) => {
         const gaugeWeightThisPeriod = (gaugeWeightThisPeriodQuery.data ?? [])[index]
         const gaugeWeightNextPeriod = (gaugeWeightNextPeriodQuery.data ?? [])[index]
         const userVotes = (userVotesQuery.data ?? [])[index]
