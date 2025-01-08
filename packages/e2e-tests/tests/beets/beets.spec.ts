6a4f7aada49af79ca0e8bfa59af016c0cda96322
Alberto Gualis <agualis@gmail.com>
2025-01-08 14:26:11 +0100

    change beets test

--- packages/e2e-tests/tests/beets/beets.spec.ts
+++ packages/e2e-tests/tests/beets/beets.spec.ts
@@ -3,9 +3,7 @@ import { test } from '@playwright/test'
 test('test', async ({ page }) => {
   await page.goto('http://localhost:3001')
   await page.getByRole('button', { name: 'Let me in' }).click()
-  await page.getByRole('link', { name: 'Explore Pools', exact: true }).click()
-  // await page.getByRole('link', { name: 'Sonic wS stS Staked Sonic' }).click();
-  // await page.getByRole('button', { name: 'Add liquidity' }).click();
-  // await page.getByText('ProportionalWhen you enter an').click();
-  // await page.getByPlaceholder('0.00').first().fill('1');
+  await page.getByRole('link', { name: 'Launch app' }).click()
+  await page.getByText('Details').click()
+  await page.getByRole('link', { name: 'Sonic wS stS Staked Sonic' }).click()
 })
