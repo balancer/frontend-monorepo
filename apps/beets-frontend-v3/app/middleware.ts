bdc63d03c5bd75065bd97ed24fa4e171af160a99
groninge <groninge@gmail.com>
2025-01-15 06:45:16 +0000

    add logging for all /api/rpc paths

--- apps/beets-frontend-v3/app/middleware.ts
+++ apps/beets-frontend-v3/app/middleware.ts
@@ -2,12 +2,15 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/rpc')) {
+    // Add logging for /api/rpc paths
+    console.log('Accessed Path:', request.url)
+    console.log('User Agent:', request.headers.get('user-agent'))
+  }
+
   const blockedPaths = ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes']
 
   if (blockedPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
-    // Add some logging
-    console.log('Accessed Path:', request.url)
-    console.log('User Agent:', request.headers.get('user-agent'))
     return new NextResponse('This path is blocked.', { status: 403 })
   }
 
