451da2cdeb02db52efe31fc7bc794fb53ef1632d
groninge <groninge@gmail.com>
2025-01-16 07:00:04 +0000

    keep console.log

--- apps/beets-frontend-v3/middleware.ts
+++ apps/beets-frontend-v3/middleware.ts
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/api/rpc')) {
+    // Add logging for /api/rpc paths
+    console.log('Referer:', request.headers.get('referer'))
+  }
+
   const permanentlyRemovedPaths = ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes']
 
   if (permanentlyRemovedPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
@@ -13,5 +18,5 @@ export function middleware(request: NextRequest) {
 
 // Apply the middleware to /api/rpc/* routes
 export const config = {
-  matcher: ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes'],
+  matcher: ['/api/rpc/:path*'],
 }
