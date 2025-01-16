c6711316443f2473c12a9c18577796a0335ce152
groninge <groninge@gmail.com>
2025-01-16 06:45:57 +0000

    update http status code and remove console.log

--- apps/beets-frontend-v3/middleware.ts
+++ apps/beets-frontend-v3/middleware.ts
@@ -2,15 +2,10 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.startsWith('/api/rpc')) {
-    // Add logging for /api/rpc paths
-    console.log('Referer:', request.headers.get('referer'))
-  }
+  const permanentlyRemovedPaths = ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes']
 
-  const blockedPaths = ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes']
-
-  if (blockedPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
-    return new NextResponse('This path is blocked.', { status: 403 })
+  if (permanentlyRemovedPaths.some(path => request.nextUrl.pathname.startsWith(path))) {
+    return new NextResponse('Gone', { status: 410 })
   }
 
   return NextResponse.next()
@@ -18,5 +13,5 @@ export function middleware(request: NextRequest) {
 
 // Apply the middleware to /api/rpc/* routes
 export const config = {
-  matcher: ['/api/rpc/:path*'],
+  matcher: ['/api/rpc/FANTOM/routes', '/api/rpc/OPTIMISM/routes'],
 }
