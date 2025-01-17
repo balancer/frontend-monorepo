57d68e4f07f6c2812c716431855199dc646bd96a
Alberto Gualis <agualis@gmail.com>
2025-01-17 12:00:05 +0100

    Experimental turbopack dev mode (#335)
    * fix: update dependencies to make apps work with turbopack
    
    * add turbopack instructions and readme
    
    * fix turbopack script
    
    * beets needs date-fns installed
    
    * fix: hydration error in ThemeProvider
    
    * add SENTRY_AUTH_TOKEN to turbo .envs
    
    ---------
    
    Co-authored-by: Gareth Fuller <gareth.a.f@gmail.com>
    Co-authored-by: groninge <groninge@gmail.com>

--- apps/frontend-v3/lib/services/chakra/ThemeProvider.tsx
+++ apps/frontend-v3/lib/services/chakra/ThemeProvider.tsx
@@ -5,9 +5,11 @@ import { ReactNode } from 'react'
 import { theme as balTheme } from './themes/bal/bal.theme'
 import { theme as cowTheme } from './themes/cow/cow.theme'
 import { useCow } from '@repo/lib/modules/cow/useCow'
+import { useIsMounted } from '@repo/lib/shared/hooks/useIsMounted'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const { isCowPath, isCowVariant } = useCow()
+  const isMounted = useIsMounted()
 
   function getTheme(): ThemeTypings {
     if (isCowPath || isCowVariant) return cowTheme
@@ -15,9 +17,13 @@ export function ThemeProvider({ children }: { children: ReactNode }) {
     return balTheme
   }
 
+  // Avoid hydration error in turbopack mode
+  if (!isMounted) return null
+
   return (
     <ChakraProvider
       cssVarsRoot="body"
+      key={isCowPath ? 'cow' : 'bal'}
       theme={getTheme()}
       toastOptions={{ defaultOptions: { position: 'bottom-left' } }}
     >
