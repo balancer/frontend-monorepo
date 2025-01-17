5bc3f409352248c85e5336d7c29c4c48b1992f55
Alberto Gualis <agualis@gmail.com>
2025-01-17 17:20:03 +0100

    chore: remove unused Global Alert provider

--- apps/beets-frontend-v3/app/layouts/base-layout.tsx
+++ apps/beets-frontend-v3/app/layouts/base-layout.tsx
@@ -1,7 +1,6 @@
 import NextTopLoader from 'nextjs-toploader'
 import { PropsWithChildren } from 'react'
 import { NavBarContainer } from '@/lib/components/navs/NavBarContainer'
-import { GlobalAlerts } from '@repo/lib/shared/components/navs/GlobalAlerts'
 import { FooterContainer } from '@/lib/components/footer/FooterContainer'
 import { LzBeetsMigrateModal } from '@/lib/components/modals/LzBeetsMigrateModal'
 
@@ -17,7 +16,6 @@ export function BaseLayout({
       }}
     >
       <NextTopLoader color="#7f6ae8" showSpinner={false} />
-      <GlobalAlerts />
       <NavBarContainer />
       {children}
       {renderLzBeetsModal && <LzBeetsMigrateModal />}
