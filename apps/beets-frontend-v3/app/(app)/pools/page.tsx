7766ce157f7ebca5052fb8db156de7384a4a0b78
Alberto Gualis <agualis@gmail.com>
2025-01-21 11:35:40 +0100

    refactor: extract PoolsPage shared page

--- apps/beets-frontend-v3/app/(app)/pools/page.tsx
+++ apps/beets-frontend-v3/app/(app)/pools/page.tsx
@@ -1,52 +1,11 @@
-import { PoolList } from '@repo/lib/modules/pool/PoolList/PoolList'
-import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
-import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
-import { Box, Skeleton } from '@chakra-ui/react'
-import { Suspense } from 'react'
-// import { getApolloServerClient } from '@repo/lib/shared/services/api/apollo-server.client'
-// import { GetFeaturedPoolsDocument } from '@repo/lib/shared/services/api/generated/graphql'
-// import { FeaturedPools } from '@repo/lib/modules/featured-pools/FeaturedPools'
+import { PoolsPage } from '@repo/lib/shared/pages/PoolsPage'
+
 import { BeetsPromoBanner } from '@/lib/components/promos/BeetsPromoBanner'
 
-export default async function PoolsPage() {
-  // Featured pools set up
-  // const { supportedNetworks } = useProjectConfig()
-
-  // const featuredPoolsQuery = await getApolloServerClient().query({
-  //   query: GetFeaturedPoolsDocument,
-  //   variables: { chains: supportedNetworks },
-  //   context: {
-  //     fetchOptions: {
-  //       next: { revalidate: 300 }, // 5 minutes
-  //     },
-  //   },
-  // })
-
-  // const featuredPools = featuredPoolsQuery.data.featuredPools || []
-
+export default async function PoolsPageWrapper() {
   return (
-    <>
-      <Box>
-        <DefaultPageContainer pb={['xl', '2xl']} pt={['xl', '40px']}>
-          <FadeInOnView animateOnce={false}>
-            <Box>
-              <BeetsPromoBanner />
-            </Box>
-          </FadeInOnView>
-          {/* <FadeInOnView animateOnce={false}>
-            <Box pt="20" pb="4">
-              <FeaturedPools featuredPools={featuredPools} />
-            </Box>
-          </FadeInOnView> */}
-        </DefaultPageContainer>
-      </Box>
-      <DefaultPageContainer noVerticalPadding pb={['xl', '2xl']} pt={['lg', '54px']}>
-        <FadeInOnView animateOnce={false}>
-          <Suspense fallback={<Skeleton h="500px" w="full" />}>
-            <PoolList />
-          </Suspense>
-        </FadeInOnView>
-      </DefaultPageContainer>
-    </>
+    <PoolsPage>
+      <BeetsPromoBanner />
+    </PoolsPage>
   )
 }
