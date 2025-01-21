813a9e2430b723e4fb915406be01f19c83ca3c6d
Alberto Gualis <agualis@gmail.com>
2025-01-21 14:51:54 +0100

    refactor: extract duplicated code into lib (some liquidity pages and layouts) (#461)
    * refactor: extract duplicated code into RemoveLiquidityPage
    
    * refactor: extract AddLiquidityPage
    
    * refactor: extract PoolLayout
    
    * refactor: extract AddLiquidityLayout
    
    * refactor: extract PoolsPage shared page
    
    * show pool symbol or This
    
    ---------
    
    Co-authored-by: groninge <groninge@gmail.com>

--- apps/beets-frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/add-liquidity/[[...txHash]]/layout.tsx
+++ apps/beets-frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/add-liquidity/[[...txHash]]/layout.tsx
@@ -1,56 +1,12 @@
 'use client'
 
-import { isNotSupported, shouldBlockAddLiquidity } from '@repo/lib/modules/pool/pool.helpers'
-import { usePool } from '@repo/lib/modules/pool/PoolProvider'
-import { RelayerSignatureProvider } from '@repo/lib/modules/relayer/RelayerSignatureProvider'
-import { TokenInputsValidationProvider } from '@repo/lib/modules/tokens/TokenInputsValidationProvider'
-import { PriceImpactProvider } from '@repo/lib/modules/price-impact/PriceImpactProvider'
-import { Alert } from '@chakra-ui/react'
-import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
+import { AddLiquidityLayout } from '@repo/lib/shared/layouts/AddLiquidityLayout'
 import { PropsWithChildren } from 'react'
-import { isHash } from 'viem'
-import { usePoolRedirect } from '@repo/lib/modules/pool/pool.hooks'
-import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
-import { AddLiquidityProvider } from '@repo/lib/modules/pool/actions/add-liquidity/AddLiquidityProvider'
-import { Permit2SignatureProvider } from '@repo/lib/modules/tokens/approvals/permit2/Permit2SignatureProvider'
-import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
 
 type Props = PropsWithChildren<{
   params: { txHash?: string[] }
 }>
 
-export default function AddLiquidityLayout({ params: { txHash }, children }: Props) {
-  const { pool } = usePool()
-  const { redirectToPoolPage } = usePoolRedirect(pool)
-
-  const maybeTxHash = txHash?.[0] || ''
-  const urlTxHash = isHash(maybeTxHash) ? maybeTxHash : undefined
-
-  if (shouldBlockAddLiquidity(pool)) {
-    return redirectToPoolPage()
-  }
-
-  if (isNotSupported(pool)) {
-    return (
-      <Alert minW="50%" status="info" w="fit-content">
-        {`This pool type is not currently supported in the ${PROJECT_CONFIG.projectName} UI`}
-      </Alert>
-    )
-  }
-
-  return (
-    <DefaultPageContainer>
-      <TransactionStateProvider>
-        <RelayerSignatureProvider>
-          <Permit2SignatureProvider>
-            <TokenInputsValidationProvider>
-              <AddLiquidityProvider urlTxHash={urlTxHash}>
-                <PriceImpactProvider>{children}</PriceImpactProvider>
-              </AddLiquidityProvider>
-            </TokenInputsValidationProvider>
-          </Permit2SignatureProvider>
-        </RelayerSignatureProvider>
-      </TransactionStateProvider>
-    </DefaultPageContainer>
-  )
+export default function AddLiquidityLayoutWrapper({ params: { txHash }, children }: Props) {
+  return <AddLiquidityLayout txHash={txHash}>{children}</AddLiquidityLayout>
 }
---
+++ packages/lib/shared/pages/RemoveLiquidityPage.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { PoolActionsLayout } from '@repo/lib/modules/pool/actions/PoolActionsLayout'
+import { RemoveLiquidityForm } from '@repo/lib/modules/pool/actions/remove-liquidity/form/RemoveLiquidityForm'
+import { RemoveLiquidityProvider } from '@repo/lib/modules/pool/actions/remove-liquidity/RemoveLiquidityProvider'
+import { RelayerSignatureProvider } from '@repo/lib/modules/relayer/RelayerSignatureProvider'
+import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
+import { PriceImpactProvider } from '@repo/lib/modules/price-impact/PriceImpactProvider'
+import { isHash } from 'viem'
+import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
+import { PermitSignatureProvider } from '@repo/lib/modules/tokens/approvals/permit2/PermitSignatureProvider'
+
+type Props = {
+  txHash?: string[]
+}
+
+export function RemoveLiquidityPage({ txHash }: Props) {
+  const maybeTxHash = txHash?.[0] || ''
+  const urlTxHash = isHash(maybeTxHash) ? maybeTxHash : undefined
+
+  return (
+    <DefaultPageContainer>
+      <TransactionStateProvider>
+        <RelayerSignatureProvider>
+          <PermitSignatureProvider>
+            <RemoveLiquidityProvider urlTxHash={urlTxHash}>
+              <PoolActionsLayout>
+                <PriceImpactProvider>
+                  <RemoveLiquidityForm />
+                </PriceImpactProvider>
+              </PoolActionsLayout>
+            </RemoveLiquidityProvider>
+          </PermitSignatureProvider>
+        </RelayerSignatureProvider>
+      </TransactionStateProvider>
+    </DefaultPageContainer>
+  )
+}
