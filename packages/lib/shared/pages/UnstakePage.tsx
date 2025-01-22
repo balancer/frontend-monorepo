b63e06c20b9687ab5301fadc3a0c495bddd32800
groninge <groninge@gmail.com>
2025-01-22 09:57:47 +0000

    wrap unstake page

---
+++ packages/lib/shared/pages/UnstakePage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { PoolActionsLayout } from '@repo/lib/modules/pool/actions/PoolActionsLayout'
+import { UnstakeForm } from '@repo/lib/modules/pool/actions/unstake/UnstakeForm'
+import { UnstakeProvider } from '@repo/lib/modules/pool/actions/unstake/UnstakeProvider'
+import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
+import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
+
+export function UnstakePage() {
+  return (
+    <DefaultPageContainer>
+      <TransactionStateProvider>
+        <UnstakeProvider>
+          <PoolActionsLayout>
+            <UnstakeForm />
+          </PoolActionsLayout>
+        </UnstakeProvider>
+      </TransactionStateProvider>
+    </DefaultPageContainer>
+  )
+}
