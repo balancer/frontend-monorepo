35ce896bc01fb2205cf02bcb844bcdc632a472cf
groninge <groninge@gmail.com>
2025-01-22 09:53:52 +0000

    wrap stake page

---
+++ packages/lib/shared/pages/StakePage.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { PoolActionsLayout } from '@repo/lib/modules/pool/actions/PoolActionsLayout'
+import { StakeForm } from '@repo/lib/modules/pool/actions/stake/StakeForm'
+import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
+import { StakeProvider } from '@repo/lib/modules/pool/actions/stake/StakeProvider'
+import { DefaultPageContainer } from '@repo/lib/shared/components/containers/DefaultPageContainer'
+
+export default function StakePage() {
+  return (
+    <DefaultPageContainer>
+      <TransactionStateProvider>
+        <StakeProvider>
+          <PoolActionsLayout>
+            <StakeForm />
+          </PoolActionsLayout>
+        </StakeProvider>
+      </TransactionStateProvider>
+    </DefaultPageContainer>
+  )
+}
