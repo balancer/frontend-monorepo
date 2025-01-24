aebab3b81c8435ea93acba8d9b3e2c5af6da33e7
groninge <groninge@gmail.com>
2025-01-24 13:52:58 +0000

    add component for testing

---
+++ apps/beets-frontend-v3/lib/modules/reliquary/Reliquary.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { Box, VStack, CardBody, Card } from '@chakra-ui/react'
+import { useGetRelicPositionsOfOwner } from './hooks/useGetRelicPositionsOfOwner'
+import { GqlChain } from '@repo/lib/shared/services/api/generated/graphql'
+import { useGetPositionForId } from './hooks/useGetPositionForId'
+
+const CHAIN = GqlChain.Sonic
+
+export function Reliquary() {
+  const { positions } = useGetRelicPositionsOfOwner(CHAIN)
+  const { data } = useGetPositionForId(CHAIN, '67')
+
+  console.log({ positions, data })
+
+  return (
+    <Card rounded="xl" w="full">
+      <VStack h="full" w="full">
+        <CardBody align="start" as={VStack} h="full" w="full">
+          <Box h="full" w="full">
+            TEST
+          </Box>
+        </CardBody>
+      </VStack>
+    </Card>
+  )
+}
