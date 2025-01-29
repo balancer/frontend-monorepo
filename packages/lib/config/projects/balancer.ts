a30806453f14b690f1a93d3b8be372c392795b85
0xAnon0602 <utkarshshail005@protonmail.com>
2025-01-29 21:08:54 +0530

    Fixed the broken link for veBAL staking (#527)

--- packages/lib/config/projects/balancer.ts
+++ packages/lib/config/projects/balancer.ts
@@ -112,7 +112,11 @@ export const ProjectConfigBalancer: ProjectConfig = {
           { label: 'Explore pools', href: '/pools' },
           { label: 'Swap tokens', href: '/swap' },
           { label: 'View portfolio', href: '/portfolio' },
-          { label: 'Get veBAL', href: 'https://app.balancer.fi/#/vebal', isExternal: true },
+          {
+            label: 'Get veBAL',
+            href: 'https://app.balancer.fi/#/ethereum/vebal',
+            isExternal: true,
+          },
           {
             label: 'Create an LBP',
             href: 'https://www.fjordfoundry.com/?utm_source=balancer&utm_medium=website',
