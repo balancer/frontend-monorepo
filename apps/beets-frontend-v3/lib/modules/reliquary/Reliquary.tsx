7124bebba8584773e2b5c793bf26647b42ee12f4
groninge <groninge@gmail.com>
2025-01-31 13:15:41 +0000

    remove for now

--- apps/beets-frontend-v3/lib/modules/reliquary/Reliquary.tsx
+++ apps/beets-frontend-v3/lib/modules/reliquary/Reliquary.tsx
@@ -5,29 +5,11 @@ import { useGetRelicPositionsOfOwner } from './hooks/useGetRelicPositionsOfOwner
 import { Relic } from './components/Relic'
 import { useReliquary } from './ReliquaryProvider'
 import { useEffect, useRef } from 'react'
-import { useReliquaryZap } from './hooks/useReliquaryZap'
 
 export function Reliquary() {
   const { chain, setSelectedRelic } = useReliquary()
   const { relics } = useGetRelicPositionsOfOwner(chain)
   const hasSetInitialRelic = useRef(false)
-
-  const { getReliquaryDepositCallData } = useReliquaryZap(chain)
-
-  const handleDeposit = async () => {
-    const callData = await getReliquaryDepositCallData({
-      userAddress: '0x6B808e314d7f758076922297bcD2eCd56aFe7B19', // user's address
-      beetsAmount: '0.09', // amount of BEETS tokens
-      stsAmount: '0.01', // amount of FTM
-      slippage: '0.005', // 1% slippage
-      //relicId: 123, // optional relic ID
-    })
-
-    console.log({ callData })
-    // Use the callData for your transaction
-  }
-
-  handleDeposit()
 
   useEffect(() => {
     if (relics && relics.length > 0 && !hasSetInitialRelic.current) {
