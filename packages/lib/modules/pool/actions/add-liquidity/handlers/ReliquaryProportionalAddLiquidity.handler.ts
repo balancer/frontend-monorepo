325c2c0526774abb7eb5312bc8a9760dcc40c03d
groninge <groninge@gmail.com>
2025-02-01 19:37:53 +0000

    get deposit to new relic working

--- packages/lib/modules/pool/actions/add-liquidity/handlers/ReliquaryProportionalAddLiquidity.handler.ts
+++ packages/lib/modules/pool/actions/add-liquidity/handlers/ReliquaryProportionalAddLiquidity.handler.ts
@@ -1,12 +1,20 @@
 import { TransactionConfig } from '@repo/lib/modules/web3/contracts/contract.types'
-import { AddLiquidity, HumanAmount, Slippage } from '@balancer/sdk'
+import {
+  AddLiquidity,
+  AddLiquidityKind,
+  HumanAmount,
+  Relayer,
+  Slippage,
+  WeightedEncoder,
+} from '@balancer/sdk'
 import { SdkBuildAddLiquidityInput } from '../add-liquidity.types'
 import { BaseProportionalAddLiquidityHandler } from './BaseProportionalAddLiquidity.handler'
 import { BatchRelayerService } from '@repo/lib/shared/services/batch-relayer/batch-relayer.service'
-import { Address, encodeFunctionData } from 'viem'
+import { Address, encodeFunctionData, Hex } from 'viem'
 import networkConfig from '@repo/lib/config/networks/sonic'
 import { Pool } from '../../../pool.types'
 import { balancerV2BalancerRelayerV6Abi } from '@repo/lib/modules/web3/contracts/abi/generated'
+import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
 
 export class ReliquaryProportionalAddLiquidityHandler extends BaseProportionalAddLiquidityHandler {
   constructor(
@@ -19,31 +27,38 @@ export class ReliquaryProportionalAddLiquidityHandler extends BaseProportionalAd
   public async buildCallData({
     account,
     queryOutput,
-    humanAmountsIn,
     slippagePercent,
     relicId,
   }: SdkBuildAddLiquidityInput & { relicId?: number }): Promise<TransactionConfig> {
     const addLiquidity = new AddLiquidity()
 
-    console.log({ queryOutput })
-
-    const { callData: joinCallData, value } = addLiquidity.buildCall({
+    // simulate add liquidity to get maxAmountsIn and minBptOut
+    const call = addLiquidity.buildCall({
       ...queryOutput.sdkQueryOutput,
       slippage: Slippage.fromPercentage(slippagePercent as HumanAmount),
       sender: account,
       recipient: networkConfig.contracts.balancer.relayerV6,
-      wethIsEth: this.helpers.isNativeAssetIn(humanAmountsIn),
+      wethIsEth: false,
+    })
+
+    // Get join call data
+    const joinCallData = this.getReliquaryFbeetsJoinCallData({
+      account,
+      maxAmountsIn: call.maxAmountsIn.map(a => a.amount),
+      minimumBpt: call.minBptOut.amount,
+      outputReference: Relayer.toChainedReference(0n, false),
+      fromInternalBalance: true,
     })
 
     // Get relic deposit data
     const relicDepositData = this.getRelicDepositOrCreateAndDeposit({
       userAddress: account,
       relicId,
-      amount: this.batchRelayerService.toPersistentChainedReference(0),
+      amount: Relayer.toChainedReference(0n, false),
     })
 
-    console.log([joinCallData, relicDepositData])
     // Return multicall data
+    // TODO: create function to encode multicall
     return {
       account,
       chainId: this.helpers.chainId,
@@ -53,8 +68,44 @@ export class ReliquaryProportionalAddLiquidityHandler extends BaseProportionalAd
         args: [[joinCallData, relicDepositData]],
       }),
       to: networkConfig.contracts.balancer.relayerV6,
-      value,
     }
+  }
+
+  private getReliquaryFbeetsJoinCallData({
+    account,
+    maxAmountsIn,
+    outputReference,
+    fromInternalBalance,
+    minimumBpt,
+  }: {
+    account: Address
+    outputReference: bigint
+    maxAmountsIn: bigint[]
+    fromInternalBalance: boolean
+    minimumBpt: bigint
+  }) {
+    return this.batchRelayerService.vaultEncodeJoinPool({
+      poolId: PROJECT_CONFIG.corePoolId as Hex,
+      poolKind: 0,
+      sender: account,
+      recipient: networkConfig.contracts.balancer.relayerV6,
+      joinPoolRequest: {
+        assets: [
+          networkConfig.tokens.addresses.beets || '0x',
+          networkConfig.tokens.stakedAsset?.address || '0x',
+        ],
+        userData: WeightedEncoder.encodeAddLiquidityUserData(AddLiquidityKind.Proportional, {
+          maxAmountsIn,
+          maxAmountsInWithoutBpt: maxAmountsIn,
+          tokenInIndex: undefined,
+          minimumBpt,
+        }),
+        maxAmountsIn,
+        fromInternalBalance,
+      },
+      value: 0n,
+      outputReference,
+    }) as Hex
   }
 
   private getRelicDepositOrCreateAndDeposit({
