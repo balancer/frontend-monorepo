d60ae4f72bebc57cc2f98fb35cf6ff8f655d4d79
Alberto Gualis <agualis@gmail.com>
2025-02-12 12:50:34 +0100

    chore: emtpy remove tokensOut to satisfy TS

--- packages/lib/modules/pool/actions/remove-liquidity/handlers/BoostedProportionalRemoveLiquidityV3.handler.ts
+++ packages/lib/modules/pool/actions/remove-liquidity/handlers/BoostedProportionalRemoveLiquidityV3.handler.ts
@@ -1,4 +1,5 @@
 import {
+  Address,
   RemoveLiquidityBoostedBuildCallInput,
   RemoveLiquidityBoostedV3,
   RemoveLiquidityKind,
@@ -16,16 +17,27 @@ export class BoostedProportionalRemoveLiquidityV3Handler extends BaseProportiona
   public async simulate({
     humanBptIn: bptIn,
     userAddress,
+    // tokensOut,
   }: QueryRemoveLiquidityInput): Promise<SdkQueryRemoveLiquidityOutput> {
+    // DEBUG: in sepolia pool
+    // const tokensOut = [
+    //   '0x7b79995e5f793a07bc00c21412e50ecae098e7f9' as Address, // WETH
+    //   '0xaa8e23fb1079ea71e0a56f48a2aa51851d8433d0' as Address, // usdt Aave
+    // ]
+    const tokensOut: Address[] = []
     const removeLiquidity = new RemoveLiquidityBoostedV3()
-    const removeLiquidityInput = this.constructSdkInput(bptIn, userAddress)
+    const removeLiquidityInput = { ...this.constructSdkInput(bptIn, userAddress), tokensOut }
 
     const sdkQueryOutput = await removeLiquidity.query(
       removeLiquidityInput,
       this.helpers.boostedPoolState
     )
 
-    return { amountsOut: sdkQueryOutput.amountsOut.filter(a => a.amount > 0n), sdkQueryOutput }
+    return {
+      amountsOut: sdkQueryOutput.amountsOut.filter(a => a.amount > 0n),
+      sdkQueryOutput,
+      unwrapWrapped: sdkQueryOutput.unwrapWrapped,
+    }
   }
 
   public async buildCallData({
@@ -33,8 +45,13 @@ export class BoostedProportionalRemoveLiquidityV3Handler extends BaseProportiona
     slippagePercent,
     queryOutput,
     permit,
+    unwrapWrapped,
   }: SdkBuildRemoveLiquidityInput): Promise<TransactionConfig> {
     const removeLiquidity = new RemoveLiquidityBoostedV3()
+
+    if (!unwrapWrapped) {
+      throw new Error('unwrapWrapped is required for boosted remove liquidity')
+    }
 
     const v3BuildCallParams: RemoveLiquidityBoostedBuildCallInput = {
       ...queryOutput.sdkQueryOutput,
@@ -42,6 +59,7 @@ export class BoostedProportionalRemoveLiquidityV3Handler extends BaseProportiona
       protocolVersion: 3,
       userData: '0x',
       removeLiquidityKind: RemoveLiquidityKind.Proportional,
+      unwrapWrapped, // REVIEW: sort by index --> muy importante probar con edge-cases here. En principio viene del SDK
     }
 
     const { callData, to, value } = permit
