be784516e75fa63544df11e715210fd6bc1740b3
Alberto Gualis <agualis@gmail.com>
2025-02-13 10:01:34 +0100

    chore: add v3 non boosted mock

--- packages/lib/modules/pool/__mocks__/api-mocks/allApiMocks.ts
+++ packages/lib/modules/pool/__mocks__/api-mocks/allApiMocks.ts
@@ -4,6 +4,7 @@ import { osETH_wETH_BPTMock } from './osETH_wETH_BPTMock'
 import { b_50sDAI_50wstETHMock } from './b_50sDAI_50wstETHMock'
 import { v2SepoliaStableWithERC4626Mock } from './v2SepoliaStableWithERC4626Mock'
 import { bCoW_50WETH_50GNOMock } from './bCoW_50WETH_50GNOMock'
+import { rsETH_hgETHMock } from './rsETH_hgETHMock'
 import { v3SepoliaNestedBoostedMock } from './v3SepoliaNestedBoostedMock'
 import { aave_USDC_USDTMock } from './aave_USDC_USDTMock'
 import { b_csUSDL_steakUSDCMock } from './b_csUSDL_steakUSDCMock'
@@ -19,6 +20,7 @@ export const allApiMocks: Pool[] = [
   b_50sDAI_50wstETHMock,
   v2SepoliaStableWithERC4626Mock,
   bCoW_50WETH_50GNOMock,
+  rsETH_hgETHMock,
   v3SepoliaNestedBoostedMock,
   aave_USDC_USDTMock,
   b_csUSDL_steakUSDCMock,
