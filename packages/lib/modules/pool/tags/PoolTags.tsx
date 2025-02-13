4a1496123f80232d700c24be72e0c394a50a00b3
groninge <groninge@gmail.com>
2025-02-13 16:27:14 +0000

    refactor to customize entire popover component

--- packages/lib/modules/pool/tags/PoolTags.tsx
+++ packages/lib/modules/pool/tags/PoolTags.tsx
@@ -1,9 +1,9 @@
-import { Badge, HStack, Popover, Text, PopoverTrigger, Image } from '@chakra-ui/react'
+import { Badge, HStack, Text, Image } from '@chakra-ui/react'
 import { PoolTag } from './getPoolTags'
 import { usePool } from '../PoolProvider'
 import { isValidNumber } from '@repo/lib/shared/utils/numbers'
 import { usePoolTags } from './PoolTagsProvider'
-import { PoolHeaderPopoverContent } from '@repo/lib/shared/components/popover/PoolHeaderPopoverContent'
+import { CustomPopover } from '@repo/lib/shared/components/popover/CustomPopover'
 
 function TagValue({ tag }: { tag: PoolTag }) {
   if (tag.value) {
@@ -19,25 +19,14 @@ function PoolTagBadge({ tag }: { tag: PoolTag }) {
   const tagIconSrc = getTagIconSrc(tag)
 
   return (
-    <Popover trigger="hover">
-      <PopoverTrigger>
-        <HStack>
-          {tagIconSrc && (
-            <Badge
-              alignItems="center"
-              bg="background.level2"
-              borderColor="border.base"
-              borderRadius="full"
-              borderWidth={1}
-              display="flex"
-              p="xs"
-              shadow="sm"
-              textTransform="none"
-              zIndex={2}
-            >
-              <Image alt={tag.name} h={6} src={tagIconSrc} w={6} />
-            </Badge>
-          )}
+    <CustomPopover
+      bodyText={tag.description}
+      footerUrl={tag.url}
+      headerText={tag.name}
+      trigger="hover"
+    >
+      <HStack>
+        {tagIconSrc && (
           <Badge
             alignItems="center"
             bg="background.level2"
@@ -45,30 +34,39 @@ function PoolTagBadge({ tag }: { tag: PoolTag }) {
             borderRadius="full"
             borderWidth={1}
             display="flex"
-            minH="34px"
-            ml={tagIconSrc ? -9 : 0}
             p="xs"
-            pl={tagIconSrc ? 8 : undefined}
             shadow="sm"
             textTransform="none"
+            zIndex={2}
           >
-            {tag.iconUrl ? (
-              <Image alt={tag.name} h={6} rounded="full" src={tag.iconUrl} w={6} />
-            ) : (
-              <Text fontSize="xs" fontWeight="bold" px="sm" textTransform="uppercase">
-                {tag.name}
-              </Text>
-            )}
-            <TagValue tag={tag} />
+            <Image alt={tag.name} h={6} src={tagIconSrc} w={6} />
           </Badge>
-        </HStack>
-      </PopoverTrigger>
-      <PoolHeaderPopoverContent
-        bodyTxt={tag.description}
-        footerUrl={tag.url}
-        headerTxt={tag.name}
-      />
-    </Popover>
+        )}
+        <Badge
+          alignItems="center"
+          bg="background.level2"
+          borderColor="border.base"
+          borderRadius="full"
+          borderWidth={1}
+          display="flex"
+          minH="34px"
+          ml={tagIconSrc ? -9 : 0}
+          p="xs"
+          pl={tagIconSrc ? 8 : undefined}
+          shadow="sm"
+          textTransform="none"
+        >
+          {tag.iconUrl ? (
+            <Image alt={tag.name} h={6} rounded="full" src={tag.iconUrl} w={6} />
+          ) : (
+            <Text fontSize="xs" fontWeight="bold" px="sm" textTransform="uppercase">
+              {tag.name}
+            </Text>
+          )}
+          <TagValue tag={tag} />
+        </Badge>
+      </HStack>
+    </CustomPopover>
   )
 }
 
