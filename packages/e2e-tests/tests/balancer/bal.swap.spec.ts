16fa21aa671d2ee075f9c2425fe8d20f22e09255
Alberto Gualis <agualis@gmail.com>
2025-02-14 17:57:15 +0100

    fix: improve swap smoke test6

--- packages/e2e-tests/tests/balancer/bal.swap.spec.ts
+++ packages/e2e-tests/tests/balancer/bal.swap.spec.ts
@@ -1,15 +1,16 @@
 import { test, expect } from '@playwright/test'
 
-test('Open swap page and try eth wrap', async ({ page }) => {
+test('test', async ({ page }) => {
   await page.goto('http://localhost:3000/')
   await page.getByRole('link', { name: 'Swap', exact: true }).click()
 
   // Selects Wrapped Ether token out
-  await page.getByRole('button', { name: 'Select token' }).first().click()
+  await page.getByRole('button', { name: 'Select token' }).click()
   await page.getByPlaceholder('Search by name, symbol or').fill('we')
-  await page.getByText('Wrapped Ether').click()
+  await page.getByRole('img', { name: 'WETH', exact: true }).click()
 
   // Fills 1 ETH token in
+  await page.getByLabel('TokenIn').click()
   await page.getByLabel('TokenIn').fill('1')
 
   await expect(page).toHaveURL('http://localhost:3000/swap/ethereum/ETH/WETH/1')
