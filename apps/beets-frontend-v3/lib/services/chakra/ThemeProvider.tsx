a57bff34567a8b38207e6a251ac929d88f5905f8
groninge <groninge@gmail.com>
2025-02-21 07:41:57 +0000

    also fix next-themes

--- apps/beets-frontend-v3/lib/services/chakra/ThemeProvider.tsx
+++ apps/beets-frontend-v3/lib/services/chakra/ThemeProvider.tsx
@@ -4,14 +4,19 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ReactNode, useEffect } from 'react'
 import { theme } from './themes/beets/beets.theme'
 import { useIsMounted } from '@repo/lib/shared/hooks/useIsMounted'
+import { useTheme } from 'next-themes'
 
 export function ThemeProvider({ children }: { children: ReactNode }) {
   const isMounted = useIsMounted()
+  const { setTheme } = useTheme()
 
+  // this fixes the theme which was set incorrectly but stored in localStorage awhile ago
   useEffect(() => {
     if (typeof window !== 'undefined' && localStorage.getItem('theme')) {
       localStorage.setItem('theme', 'dark')
+      setTheme('dark')
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   // Avoid hydration error in turbopack mode
