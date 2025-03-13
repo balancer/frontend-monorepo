06ee8599a992244a53602cccdc12f43ac4f35cf6
groninge <groninge@gmail.com>
2025-03-13 12:56:02 +0000

    add additional fees for beets

--- apps/beets-frontend-v3/app/(app)/pools/page.tsx
+++ apps/beets-frontend-v3/app/(app)/pools/page.tsx
@@ -1,9 +1,44 @@
 import { PoolsPage } from '@repo/lib/shared/pages/PoolsPage/PoolsPage'
 import { BeetsPromoBanner } from '@/lib/components/promos/BeetsPromoBanner'
+import {
+  GetProtocolStatsDocument,
+  GetStakedSonicDataDocument,
+  GqlChain,
+} from '@repo/lib/shared/services/api/generated/graphql'
+import { getApolloServerClient } from '@repo/lib/shared/services/api/apollo-server.client'
+import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
+import { mins } from '@repo/lib/shared/utils/time'
 
 export default async function PoolsPageWrapper() {
+  const client = getApolloServerClient()
+
+  const variables = {
+    chains: [...PROJECT_CONFIG.supportedNetworks, GqlChain.Fantom], // manually adding Fantom to the list to get the data for the landing page
+  }
+
+  const { data: stakedSonicData } = await client.query({
+    query: GetStakedSonicDataDocument,
+    variables: {},
+  })
+
+  const { data: protocolData } = await client.query({
+    query: GetProtocolStatsDocument,
+    variables,
+    context: {
+      fetchOptions: {
+        next: { revalidate: mins(10).toSecs() },
+      },
+    },
+  })
+
+  // we're on the server here so can't use 'bn'
+  const additionalFees = (
+    parseFloat(stakedSonicData.stsGetGqlStakedSonicData.rewardsClaimed24h) +
+    parseFloat(protocolData.protocolMetricsAggregated.yieldCapture24h)
+  ).toString()
+
   return (
-    <PoolsPage>
+    <PoolsPage additionalFees={additionalFees}>
       <BeetsPromoBanner />
     </PoolsPage>
   )
