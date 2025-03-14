ad3912aad2906146c571b71454271af08781c1bf
Alberto Gualis <agualis@gmail.com>
2025-03-14 10:54:55 +0100

    feat: use openGraph metadata field to display dynamic openGraph images

--- apps/beets-frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/layout.tsx
+++ apps/beets-frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/layout.tsx
@@ -1,12 +1,25 @@
 /* eslint-disable max-len */
 import { FetchPoolProps } from '@repo/lib/modules/pool/pool.types'
 import { ChainSlug } from '@repo/lib/modules/pool/pool.utils'
-import { PoolLayout } from '@repo/lib/shared/layouts/PoolLayout'
+import { generatePoolMetadata, PoolLayout, PoolMetadata } from '@repo/lib/shared/layouts/PoolLayout'
+import { Metadata } from 'next'
 import { PropsWithChildren } from 'react'
 
 export type Props = PropsWithChildren<{
   params: Omit<FetchPoolProps, 'chain'> & { chain: ChainSlug }
 }>
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const poolMetadata: PoolMetadata = await generatePoolMetadata(props.params)
+
+  return {
+    ...poolMetadata.metadata,
+    openGraph: {
+      // We could use a more accurate dynamic image by variant, pool type, chain, etc
+      images: '/images/opengraph/og-beets-pool.png',
+    },
+  }
+}
 
 export default async function PoolLayoutWrapper({
   params: { id, chain, variant },
