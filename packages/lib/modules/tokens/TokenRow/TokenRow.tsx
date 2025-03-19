462954fda137e7569ab3fb98c6ba5a1797ab7034
groninge <groninge@gmail.com>
2025-03-19 13:15:08 +0000

    round square images

--- packages/lib/modules/tokens/TokenRow/TokenRow.tsx
+++ packages/lib/modules/tokens/TokenRow/TokenRow.tsx
@@ -85,6 +85,8 @@ function TokenInfo({
         <TokenIcon
           address={address}
           alt={token?.symbol || address}
+          bg="background.level2"
+          borderRadius="md"
           chain={chain}
           logoURI={logoURI}
           overflow="visible"
