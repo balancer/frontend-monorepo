0620f94364029e1505c36a042b711ee2d3882066
Alberto Gualis <agualis@gmail.com>
2025-03-21 12:30:13 +0100

    fix: improve settings alerts

--- packages/lib/modules/user/settings/SettingsAlert.tsx
+++ packages/lib/modules/user/settings/SettingsAlert.tsx
@@ -1,26 +1,31 @@
 import { useUserSettings } from '@repo/lib/modules/user/settings/UserSettingsProvider'
 import { BalAlert } from '@repo/lib/shared/components/alerts/BalAlert'
+import { useIsSafeApp } from '../../web3/safe.hooks'
 
 export function SettingsAlert() {
   const { shouldUseSignatures, shouldUseTxBundling } = useUserSettings()
+  const isSafeApp = useIsSafeApp()
+  const isTxBundlingDisabled = isSafeApp && !shouldUseTxBundling
 
   function getTitle() {
-    if (!shouldUseTxBundling && !shouldUseSignatures) {
-      return 'Transaction bundling and signatures disabled'
+    if (isTxBundlingDisabled && !shouldUseSignatures) {
+      return 'Safe transaction bundling and signatures disabled'
     }
-    if (!shouldUseTxBundling) return 'Transaction bundling disabled'
+    if (isTxBundlingDisabled) return 'Safe transaction bundling disabled'
     if (!shouldUseSignatures) return 'Signatures disabled'
   }
   function getContent() {
-    if (!shouldUseTxBundling && !shouldUseSignatures) {
+    if (isTxBundlingDisabled && !shouldUseSignatures) {
       return 'You can enable them in your settings.'
     }
-    if (!shouldUseTxBundling) return 'You can enable them in your settings.'
+    if (isTxBundlingDisabled) {
+      return 'For a better user experience, you can enable them in your settings.'
+    }
     if (!shouldUseSignatures) {
       return 'All approvals will require gas transactions. You can enable signatures in your settings.'
     }
   }
-  if (!shouldUseSignatures || !shouldUseTxBundling) {
+  if (!shouldUseSignatures || isTxBundlingDisabled) {
     return <BalAlert content={getContent()} status="warning" title={getTitle()} />
   }
   return null
