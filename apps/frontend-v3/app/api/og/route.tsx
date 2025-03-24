e5d88127eb52cd0a27c621701509e338af41d16c
groninge <groninge@gmail.com>
2025-03-24 10:48:46 +0000

    use tailwind

--- apps/frontend-v3/app/api/og/route.tsx
+++ apps/frontend-v3/app/api/og/route.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/no-unknown-property */
 import { ImageResponse } from '@vercel/og'
 import { NextRequest } from 'next/server'
 
@@ -11,21 +13,41 @@ export async function GET(request: NextRequest) {
     (
       <div
         style={{
-          fontSize: 40,
-          color: 'black',
-          background: 'white',
-          width: '100%',
           height: '100%',
-          padding: '50px 200px',
-          textAlign: 'center',
-          justifyContent: 'center',
-          alignItems: 'center',
+          width: '100%',
           display: 'flex',
           flexDirection: 'column',
-          gap: '20px',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
         }}
       >
-        <h1 style={{ fontSize: 60, margin: 0 }}>{name}</h1>
+        <div tw="bg-gray-50 flex">
+          <div tw="flex flex-col md:flex-row w-full py-12 px-4 md:items-center justify-between p-8">
+            <h2 tw="flex flex-col text-3xl sm:text-4xl font-bold tracking-tight text-gray-900 text-left">
+              <span>Ready to dive in?</span>
+              <span tw="text-indigo-600">Check out this pool!</span>
+            </h2>
+            <div tw="mt-8 flex md:mt-0">
+              <div tw="flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-5 py-3 text-base font-medium text-white"
+                >
+                  {name}
+                </a>
+              </div>
+              <div tw="ml-3 flex rounded-md shadow">
+                <a
+                  href="#"
+                  tw="flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600"
+                >
+                  Learn more
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     ),
     {
