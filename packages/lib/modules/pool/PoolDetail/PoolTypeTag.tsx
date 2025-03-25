38b310859ed262b841aba7e476bc38bd01d9fc37
groninge <groninge@gmail.com>
2025-03-25 11:01:18 +0000

    wip: add quantamm pool type tag

--- packages/lib/modules/pool/PoolDetail/PoolTypeTag.tsx
+++ packages/lib/modules/pool/PoolDetail/PoolTypeTag.tsx
@@ -8,26 +8,30 @@ import Image from 'next/image'
 import { PoolListItem } from '../pool.types'
 import { Erc4626Metadata } from '../metadata/getErc4626Metadata'
 import { usePoolsMetadata } from '../metadata/PoolsMetadataProvider'
+//import { CustomPopover } from '@repo/lib/shared/components/popover/CustomPopover'
 
 type PoolTypeTagProps = {
   pool: Pool | PoolListItem
 }
 
+const tagWrapperProps = {
+  alignItems: 'center',
+  background: 'background.level2',
+  border: '1px solid',
+  borderColor: 'border.base',
+  display: 'flex',
+  fontWeight: 'normal',
+  h: { base: '28px' },
+  px: 'sm',
+  py: 'xs',
+  rounded: 'full',
+  shadow: 'sm',
+  gap: 'xs',
+}
+
 function TagWrapper({ children }: { children: React.ReactNode }) {
   return (
-    <Box
-      alignItems="center"
-      background="background.level2"
-      border="1px solid"
-      borderColor="border.base"
-      display="flex"
-      fontWeight="normal"
-      h={{ base: '28px' }}
-      px="sm"
-      py="xs"
-      rounded="full"
-      shadow="sm"
-    >
+    <Box {...tagWrapperProps}>
       <HStack>{children}</HStack>
     </Box>
   )
@@ -61,60 +65,84 @@ function getPoolTypeLabel(pool: Pool | PoolListItem, erc4626Metadata: Erc4626Met
   switch (type) {
     case GqlPoolType.CowAmm:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.CowAmm} />
           <Text {...textProps}>Weighted</Text>
-        </>
+        </TagWrapper>
       )
 
     case GqlPoolType.Weighted:
-      return <Text {...textProps}>Weighted</Text>
+      return (
+        <TagWrapper>
+          <Text {...textProps}>Weighted</Text>
+        </TagWrapper>
+      )
 
     case GqlPoolType.Stable:
     case GqlPoolType.PhantomStable:
     case GqlPoolType.ComposableStable:
     case GqlPoolType.MetaStable:
-      return <Text {...textProps}>Stable</Text>
+      return (
+        <TagWrapper>
+          <Text {...textProps}>Stable</Text>
+        </TagWrapper>
+      )
 
     case GqlPoolType.Fx:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.Xave} />
           <Text {...textProps}>FX</Text>
-        </>
+        </TagWrapper>
       )
 
     case GqlPoolType.LiquidityBootstrapping:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.Fjord} />
           <Text {...textProps}>LBP</Text>
-        </>
+        </TagWrapper>
       )
 
     case GqlPoolType.Gyro:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.Gyro} />
           <Text {...textProps}>2-CLP</Text>
-        </>
+        </TagWrapper>
       )
 
     case GqlPoolType.Gyro3:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.Gyro} />
           <Text {...textProps}>3-CLP</Text>
-        </>
+        </TagWrapper>
       )
 
     case GqlPoolType.Gyroe:
       return (
-        <>
+        <TagWrapper>
           <ProtocolIcon protocol={Protocol.Gyro} />
           <Text {...textProps}>E-CLP</Text>
-        </>
+        </TagWrapper>
       )
+
+    // TODO: add quantamm type
+    // case GqlPoolType.Gyroe:
+    //   return (
+    //     <CustomPopover
+    //       bodyText="BTFs by QuantAMM dynamically adjust pool weights to capitalize on price movements. For example, a BTF pool can automatically increase its WBTC allocation when its value rises faster than ETH. This allows LPs to earn both trading fees and profits from underlying asset appreciation through continuous, responsive, fully on-chain TradFi-style strategies."
+    //       footerUrl="https://medium.com/@QuantAMM/quantamm-x-balancer-v3-046af77ddc81"
+    //       headerText="Blockchain Traded Funds (BFTs)"
+    //       trigger="hover"
+    //     >
+    //       <Box {...tagWrapperProps}>
+    //         <ProtocolIcon protocol={Protocol.QuantAmm} />
+    //         <Text {...textProps}>BTF</Text>
+    //       </Box>
+    //     </CustomPopover>
+    //   )
 
     default:
       return null
@@ -125,7 +153,5 @@ export function PoolTypeTag({ pool }: PoolTypeTagProps) {
   const { getErc4626Metadata } = usePoolsMetadata()
   const erc4626Metadata = getErc4626Metadata(pool)
 
-  const label = getPoolTypeLabel(pool, erc4626Metadata)
-
-  return <TagWrapper>{label}</TagWrapper>
+  return getPoolTypeLabel(pool, erc4626Metadata)
 }
--- packages/lib/modules/protocols/useProtocols.ts
+++ packages/lib/modules/protocols/useProtocols.ts
@@ -20,6 +20,7 @@ export enum Protocol {
   CowAmm = 'cow-amm',
   Xave = 'xave',
   Fjord = 'fjord',
+  QuantAmm = 'quant-amm',
 }
 
 export const protocolIconPaths: Record<Protocol, string> = {
@@ -30,6 +31,7 @@ export const protocolIconPaths: Record<Protocol, string> = {
   [Protocol.CowAmm]: '/images/protocols/cowamm.png',
   [Protocol.Xave]: '/images/protocols/xave.png',
   [Protocol.Aave]: '/images/protocols/aave.svg',
+  [Protocol.QuantAmm]: '/images/protocols/quantamm.png',
   /* Icons below are yet unused */
   [Protocol.Agave]: '/images/protocols/agave.png',
   [Protocol.Beefy]: '/images/protocols/beefy.svg',
