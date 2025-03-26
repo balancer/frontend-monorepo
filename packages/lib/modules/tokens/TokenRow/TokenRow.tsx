d1f9b06defefeb820c95b809131a215ecdc851be
groninge <groninge@gmail.com>
2025-03-26 10:19:21 +0000

    no longer needed

--- packages/lib/modules/tokens/TokenRow/TokenRow.tsx
+++ packages/lib/modules/tokens/TokenRow/TokenRow.tsx
@@ -87,8 +87,6 @@ function TokenInfo({
         <TokenIcon
           address={address}
           alt={token?.symbol || address}
-          bg="background.level2"
-          borderRadius="md"
           chain={chain}
           logoURI={logoURI}
           overflow="visible"
