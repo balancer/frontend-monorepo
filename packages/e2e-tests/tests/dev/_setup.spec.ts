4c805362be5a8da202a7efc355902135b2a1df30
Alberto Gualis <agualis@gmail.com>
2025-04-01 12:25:52 +0200

    chore: increase E2E setup timeout

--- packages/e2e-tests/tests/dev/_setup.spec.ts
+++ packages/e2e-tests/tests/dev/_setup.spec.ts
@@ -4,7 +4,7 @@ import { balWeth8020 } from '@repo/lib/modules/pool/__mocks__/pool-examples/flat
 // This test must be run before all the others
 test('Wait for dev server to load page', async ({ page }) => {
   // Wait for 1 minute for the dev server to load tested pages before running the real tests
-  test.setTimeout(60000)
+  test.setTimeout(90000)
 
   const poolDetailPageUrl = `http://localhost:3000/pools/ethereum/v2/${balWeth8020.poolId}`
 
@@ -16,6 +16,7 @@ test('Wait for dev server to load page', async ({ page }) => {
   await page.goto(poolDetailPageUrl + '/add-liquidity', {
     waitUntil: 'load',
   })
+
   await page.goto(poolDetailPageUrl + '/remove-liquidity', {
     waitUntil: 'load',
   })
