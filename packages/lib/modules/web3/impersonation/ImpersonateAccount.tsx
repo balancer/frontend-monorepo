7bf49d000f103065868b5d6918be91450d17996c
Alberto Gualis <agualis@gmail.com>
2025-04-02 12:50:31 +0200

    chore: simplify implementation

--- packages/lib/modules/web3/impersonation/ImpersonateAccount.tsx
+++ packages/lib/modules/web3/impersonation/ImpersonateAccount.tsx
@@ -1,71 +1,27 @@
 import { Button, Input, Text, VStack } from '@chakra-ui/react'
-import { shouldUseAnvilFork } from '@repo/lib/config/app.config'
-import {
-  defaultAnvilAccount,
-  getSavedImpersonatedAddressLS,
-} from '@repo/lib/test/utils/wagmi/fork.helpers'
-import { useEffect, useRef, useState } from 'react'
+import { defaultAnvilAccount } from '@repo/lib/test/utils/wagmi/fork.helpers'
+import { useState } from 'react'
 import { Address, isAddress } from 'viem'
-import { useConnect } from 'wagmi'
-import { impersonateWagmiConfig, wagmiConfig } from '../WagmiConfig'
-import { useWagmiConfig } from '../WagmiConfigProvider'
 import { useImpersonateAccount } from './useImpersonateAccount'
 
 export function ImpersonateAccount() {
-  const { setWagmiConfig } = useWagmiConfig()
-  const [impersonatedAddress, setImpersonatedAddress] = useState<string | undefined>(
-    defaultAnvilAccount
-  )
+  const [impersonatedAddress, setImpersonatedAddress] = useState<string>(defaultAnvilAccount)
   const { impersonateAccount } = useImpersonateAccount()
-  const { connectors, connectAsync } = useConnect()
-
-  const storedImpersonatedAddress = useRef<string | undefined>(undefined)
-  const isReconnectingImpersonatedAddress = useRef(false)
-
-  function onAddressChange(address: string) {
-    setImpersonatedAddress(address)
-    if (isAddress(address)) {
-      return setWagmiConfig(impersonateWagmiConfig(address))
-    }
-  }
-
-  useEffect(() => {
-    if (shouldUseAnvilFork) {
-      const impersonatedAddress = getSavedImpersonatedAddressLS()
-      if (impersonatedAddress) {
-        storedImpersonatedAddress.current = impersonatedAddress
-      }
-
-      // Load default account
-      setWagmiConfig(impersonateWagmiConfig(impersonatedAddress || defaultAnvilAccount))
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
-  useEffect(() => {
-    if (storedImpersonatedAddress.current && !isReconnectingImpersonatedAddress.current) {
-      isReconnectingImpersonatedAddress.current = true
-      connectAsync({ connector: connectors[connectors.length - 1], chainId: 1 })
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [storedImpersonatedAddress])
 
   return (
     <VStack>
       <Text>Impersonate Account</Text>
       <Input
         aria-label="Mock address"
-        onChange={e => onAddressChange(e.target.value)}
+        onChange={e => setImpersonatedAddress(e.target.value || '')}
         type="text"
         value={impersonatedAddress ?? ''}
         width="450px"
       />
       <Button
         aria-label="Impersonate"
-        disabled={!isAddress(impersonatedAddress || '')}
-        onClick={() =>
-          impersonateAccount({ impersonatedAddress: impersonatedAddress as Address, wagmiConfig })
-        }
+        disabled={!isAddress(impersonatedAddress)}
+        onClick={() => impersonateAccount({ impersonatedAddress: impersonatedAddress as Address })}
       >
         Connect
       </Button>
--- packages/lib/modules/web3/impersonation/customMock.ts
+++ packages/lib/modules/web3/impersonation/customMock.ts
@@ -2,7 +2,6 @@ import { MockParameters, mock } from 'wagmi/connectors'
 
 import { Address } from 'viem'
 import { CreateConnectorFn } from 'wagmi'
-import { impersonateWagmiConfig } from '../WagmiConfig'
 
 type CustomMockOptions = {
   impersonationAddress?: Address
@@ -19,7 +18,6 @@ export function customMock(
 
   return (config: any) => {
     const connector = originalMock(config)
-    const originalConnect = connector.connect.bind(connector)
 
     // Add impersonated address to the list of accounts to allow non default anvil accounts to approve and run transactions
     if (options.impersonationAddress) {
@@ -28,11 +26,6 @@ export function customMock(
         const originalAccounts = await originalGetAccounts()
         return [options.impersonationAddress, ...originalAccounts] as readonly Address[]
       }
-    }
-
-    connector.connect = params => {
-      impersonateWagmiConfig(options.impersonationAddress)
-      return originalConnect(params)
     }
 
     return connector
