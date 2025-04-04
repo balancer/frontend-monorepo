d83804cbcd64162bc081a0ca30fc4a3fda4a433a
Juanjo Molinero <jjmolinero@gmail.com>
2025-04-04 10:24:27 +0200

    [#782] fix voting page header countdown clock

--- packages/lib/modules/vebal/vote/Votes/VotesContainer.tsx
+++ packages/lib/modules/vebal/vote/Votes/VotesContainer.tsx
@@ -38,13 +38,14 @@ export async function VotesContainer() {
       votingIncentivesLoading={false} /* RSC (SSR) mode, no loading needed */
     >
       <VStack spacing="3xl" w="full">
-        {/* todo: work in progress */}
-        {false && <VotesIntroductionLayout />}
+        <VotesIntroductionLayout />
+
         <TransactionStateProvider>
           <MyVotesProvider>
             <MyVotesLayout />
           </MyVotesProvider>
         </TransactionStateProvider>
+
         <VoteListProvider>
           <VoteListLayout />
         </VoteListProvider>
