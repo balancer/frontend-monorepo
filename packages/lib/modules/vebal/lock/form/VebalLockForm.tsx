5b94d51ffb0176a67cb5dc6efccbb1d68b9857bb
Alberto Gualis <alberto@balancer.finance>
2025-04-08 11:21:34 +0200

    Remove wrong key

--- packages/lib/modules/vebal/lock/form/VebalLockForm.tsx
+++ packages/lib/modules/vebal/lock/form/VebalLockForm.tsx
@@ -158,7 +158,6 @@ export function VebalLockForm() {
                         ]
                       : undefined
                   }
-                  key={vebalBptToken.address}
                   value={lockedAmount ?? ''}
                 />
               </Card>
