0b5cab3f0da20fadd6dc096673d659964b865f9b
uiuxxx <uxuidefi@gmail.com>
2025-04-10 13:33:45 +0100

    WIP

--- apps/frontend-v3/app/(app)/vebal/page.tsx
+++ apps/frontend-v3/app/(app)/vebal/page.tsx
@@ -1,17 +1,34 @@
-import { Button, Stack } from '@chakra-ui/react'
+'use client'
+import { Heading, Button, Stack } from '@chakra-ui/react'
+import Section from '@repo/lib/shared/components/layout/Section'
 
 import NextLink from 'next/link'
 
 export default function VeBALPage() {
   return (
-    <Stack gap="lg" maxW="200px">
-      <Button as={NextLink} href="/vebal/manage" size="lg" variant="primary">
-        Manage veBAL
-      </Button>
+    <>
+      <Section>
+        <Stack gap="lg" maxW="200px">
+          <Heading as="h2" size="lg" variant="special">
+            veBAL xxx
+          </Heading>
+          <Button as={NextLink} href="/vebal/manage" size="lg" variant="primary">
+            Manage veBAL
+          </Button>
 
-      <Button as={NextLink} href="/vebal/vote" size="lg" variant="primary">
-        Vote
-      </Button>
-    </Stack>
+          <Button as={NextLink} href="/vebal/vote" size="lg" variant="primary">
+            Vote
+          </Button>
+        </Stack>
+      </Section>
+      <Section>
+        <Heading as="h2" size="lg" variant="special" />
+      </Section>
+      <Section>
+        <Heading as="h2" size="lg" variant="special">
+          How it works
+        </Heading>
+      </Section>
+    </>
   )
 }
