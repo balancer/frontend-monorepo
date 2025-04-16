269c044170edb0c9c062034b35144005a31b5dd3
groninge <groninge@gmail.com>
2025-04-16 13:59:57 +0000

    rename variable

--- packages/lib/modules/fee-managers/useFeeManager.ts
+++ packages/lib/modules/fee-managers/useFeeManager.ts
@@ -3,12 +3,12 @@ import { getChainId } from '@repo/lib/config/app.config'
 import { Pool } from '../pool/pool.types'
 
 export function useFeeManager(pool: Pool) {
-  const { metadata } = useFeeManagers()
+  const { metadata: feeManagersMetadata } = useFeeManagers()
 
   const hasFeeManager = !!pool.swapFeeManager
   const chainId = getChainId(pool.chain)
 
-  const feeManager = metadata?.find(metadata => {
+  const feeManager = feeManagersMetadata?.find(metadata => {
     const metadataAddresses = metadata.addresses[chainId.toString()]?.map(address =>
       address.toLowerCase()
     )
