ae4a7b41886846c2d1d7294fc742dd4874ee619e
groninge <groninge@gmail.com>
2025-04-25 07:40:13 +0000

    if no snapshots show msg

--- packages/lib/modules/pool/PoolDetail/PoolWeightCharts/PoolWeightShiftsChart.tsx
+++ packages/lib/modules/pool/PoolDetail/PoolWeightCharts/PoolWeightShiftsChart.tsx
@@ -2,9 +2,19 @@
 
 import ReactECharts from 'echarts-for-react'
 import { usePoolWeightShiftsChart } from './usePoolWeightShiftsChart'
+import { Flex, Text } from '@chakra-ui/react'
+import { isEmpty } from 'lodash'
 
 export function PoolWeightShiftsChart() {
   const { option } = usePoolWeightShiftsChart()
 
-  return <ReactECharts option={option} style={{ height: '100%', width: '100%' }} />
+  return isEmpty(option) ? (
+    <Flex align="center" h="full" justify="center" w="full">
+      <Text fontSize="2xl" p="lg" variant="secondary">
+        Not enough data
+      </Text>
+    </Flex>
+  ) : (
+    <ReactECharts option={option} style={{ height: '100%', width: '100%' }} />
+  )
 }
