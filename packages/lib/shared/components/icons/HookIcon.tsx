c6dff53575dbe0b953d221f5244c0d2d4fa98495
groninge <groninge@gmail.com>
2025-05-02 11:37:36 +0000

    put the props spread last again

--- packages/lib/shared/components/icons/HookIcon.tsx
+++ packages/lib/shared/components/icons/HookIcon.tsx
@@ -4,16 +4,16 @@ import { SVGProps } from 'react'
 export function HookIcon({ size = 24, ...props }: { size?: number } & SVGProps<SVGSVGElement>) {
   return (
     <svg
-      height={size}
-      width={size}
-      {...props}
       fill="none"
+      height={size}
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="1.5"
       viewBox="0 0 24 24"
+      width={size}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path d="M16 9v6a5 5 0 0 1-10 0v-4l3 3" />
       <path d="M14 7a2 2 0 1 0 4 0 2 2 0 1 0-4 0M16 5V3" />
