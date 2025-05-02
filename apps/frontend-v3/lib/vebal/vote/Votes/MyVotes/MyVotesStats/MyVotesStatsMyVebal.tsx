3fb7bbe6ae778e23e3de166b44dcdbbec1e066e6
uiuxxx <uxuidefi@gmail.com>
2025-05-02 10:27:52 +0100

    Merged the expired case with the has veBAL case to avoid redundancy.

--- apps/frontend-v3/lib/vebal/vote/Votes/MyVotes/MyVotesStats/MyVotesStatsMyVebal.tsx
+++ apps/frontend-v3/lib/vebal/vote/Votes/MyVotes/MyVotesStats/MyVotesStatsMyVebal.tsx
@@ -45,7 +45,7 @@ export function MyVotesStatsMyVebal({ myVebalBalance, loading }: Props) {
         loading ? (
           <Skeleton height="28px" w="100px" />
         ) : myVebalBalance === 0n ? (
-          <HStack spacing="xs">
+          <HStack spacing="ms">
             <Text color="font.maxContrast">&mdash;</Text>
             {isLockExpired && (
               <Tooltip
@@ -88,16 +88,7 @@ export function MyVotesStatsMyVebal({ myVebalBalance, loading }: Props) {
           <VStack>
             <ConnectWallet size="sm" variant="primary" />
           </VStack>
-        ) : isLockExpired ? (
-          <Button
-            as={NextLink}
-            href={getVeBalManagePath('extend', 'vote')}
-            size="sm"
-            variant="primary"
-          >
-            Manage
-          </Button>
-        ) : myVebalBalance ? (
+        ) : isLockExpired || myVebalBalance ? (
           <Button as={NextLink} href="/vebal/manage" size="sm" variant="tertiary">
             Manage
           </Button>
