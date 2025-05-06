464e22a5c33f77ca15203c3e6eba7dd7a5fac93f
uiuxxx <uxuidefi@gmail.com>
2025-05-06 18:18:55 +0100

    UX improvements to the Pool Selection toaster

--- apps/frontend-v3/lib/vebal/vote/VoteList/SelectedPoolsMenu.tsx
+++ apps/frontend-v3/lib/vebal/vote/VoteList/SelectedPoolsMenu.tsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 import { Box, Button, Divider, HStack, Text, useToken, VStack } from '@chakra-ui/react'
-import { ChevronDown } from 'react-feather'
+import { Maximize2, Minimize2 } from 'react-feather'
 import tinycolor from 'tinycolor2'
+import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
 
-const MIN_VISIBLE_POOLS_COUNT = 1
-const MAX_VISIBLE_POOLS_COUNT = 4
+const MIN_VISIBLE_POOLS_COUNT = 0
+const MAX_VISIBLE_POOLS_COUNT = 5
 
 interface SelectedPoolsToastProps {
   onAddVotesClick?: () => void
@@ -24,45 +25,57 @@ export function SelectedPoolsMenu({ onAddVotesClick, votingPools }: SelectedPool
   const bgColor = tinycolor(_bgColor).setAlpha(0.4).toRgbString()
 
   return (
-    <Box backdropFilter="blur(36px)" bg={bgColor} rounded="xl" w="320px">
+    <Box backdropFilter="blur(12px)" bg={bgColor} rounded="xl" shadow="2xl" w="320px">
       <VStack alignItems="stretch" p="0" spacing="0" w="full">
         <HStack justifyContent="space-between" p="md">
           <Text color="font.maxContrast" flex="1" fontWeight="700">
             Pool selection ({sortedPools.length})
           </Text>
-          <HStack>
+          <HStack gap="ms">
             {onAddVotesClick && (
-              <Button onClick={onAddVotesClick} size="sm" variant="primary">
-                Add votes
+              <Button gap="0" onClick={onAddVotesClick} size="sm" variant="primary">
+                Go to vote
               </Button>
             )}
-            <Box color="font.maxContrast" cursor="pointer" onClick={() => setExpanded(v => !v)}>
-              <ChevronDown
-                size={20}
-                style={{ transition: 'all 0.2s ease-in-out' }}
-                transform={expanded ? undefined : 'rotate(180)'}
-              />
-            </Box>
+            {sortedPools.length > 1 && (
+              <Box
+                _hover={{ color: 'font.highlight' }}
+                color="font.maxContrast"
+                cursor="pointer"
+                onClick={() => setExpanded(v => !v)}
+                transition="color 0.3s var(--ease-out-cubic)"
+              >
+                {expanded ? (
+                  <FadeInOnView>
+                    <Minimize2 size={16} />
+                  </FadeInOnView>
+                ) : (
+                  <FadeInOnView>
+                    <Maximize2 size={16} />
+                  </FadeInOnView>
+                )}
+              </Box>
+            )}
           </HStack>
         </HStack>
-        <Divider />
-        <VStack alignItems="start" p="md" spacing="md" w="full">
-          {visiblePools.map(pool => (
-            <VStack alignItems="start" key={[pool.title, pool.description].join('-')}>
-              <Text fontSize="sm" fontWeight="700">
-                {pool.title}
-              </Text>
-              <Text color="font.secondary" fontSize="sm">
-                {pool.description}
-              </Text>
+
+        {expanded && (
+          <>
+            <Divider />
+            <VStack alignItems="start" p="md" spacing="md" w="full">
+              {visiblePools.map(pool => (
+                <VStack alignItems="start" key={[pool.title, pool.description].join('-')}>
+                  <Text fontSize="sm">{pool.title}</Text>
+                </VStack>
+              ))}
+              {moreCount > 0 && (
+                <Text fontSize="sm" variant="secondary">
+                  Plus {moreCount} more...
+                </Text>
+              )}
             </VStack>
-          ))}
-          {moreCount > 0 && (
-            <Text fontSize="sm" fontWeight="700">
-              Plus {moreCount} more...
-            </Text>
-          )}
-        </VStack>
+          </>
+        )}
       </VStack>
     </Box>
   )
