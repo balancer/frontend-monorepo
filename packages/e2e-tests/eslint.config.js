fd16ccfc549385883987240d701515f56c3ab703
groninge <groninge@gmail.com>
2025-05-07 13:13:18 +0000

    eslint was not configured here

--- packages/e2e-tests/eslint.config.js
+++
@@ -1,9 +0,0 @@
-import baseConfig from '@repo/eslint-config/base'
-
-/** @type {import("eslint").Linter.Config} */
-export default [
-  ...baseConfig,
-  {
-    // E2E tests-specific overrides can be added here
-  },
-]
