aa59c519588ab13806a210852bbefedc0f9ebf5b
groninge <groninge@gmail.com>
2025-05-28 14:06:13 +0000

    only show in dev for now

--- packages/lib/modules/pool/PoolContainer.tsx
+++ packages/lib/modules/pool/PoolContainer.tsx
@@ -4,11 +4,15 @@ import { PoolDetail } from './PoolDetail/PoolDetail'
 import { usePool } from './PoolProvider'
 import { isLBP } from './pool.helpers'
 import { LbpDetail } from './LbpDetail/LbpDetail'
+import { isDev } from '@repo/lib/config/app.config'
 
 export function PoolContainer() {
   const { pool } = usePool()
-  if (isLBP(pool.type)) {
+
+  // TODO: only show in dev for now
+  if (isLBP(pool.type) && isDev) {
     return <LbpDetail />
   }
+
   return <PoolDetail />
 }
