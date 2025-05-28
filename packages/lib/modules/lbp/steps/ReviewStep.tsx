d230aa16d34cf22610766bdfa942e0e59d122745
Matt Pereira <mattpereira.dev@gmail.com>
2025-05-28 15:45:27 -0300

    refactor: persist lbp creation config in local storage (#1106)
    * persist lbp config in local storage
    
    * persist step index for lbp creation
    
    * fix file name

--- packages/lib/modules/lbp/steps/ReviewStep.tsx
+++ packages/lib/modules/lbp/steps/ReviewStep.tsx
@@ -1,11 +1,14 @@
 'use client'
 
 import { Heading, VStack } from '@chakra-ui/react'
+import { LbpFormAction } from '../LbpFormAction'
 
 export function ReviewStep() {
   return (
     <VStack align="start" w="full">
       <Heading size="sm">Review</Heading>
+
+      <LbpFormAction />
     </VStack>
   )
 }
