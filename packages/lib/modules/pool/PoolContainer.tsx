4fe66f85d739f72a872930e8f3551d68a16b17f5
groninge <groninge@gmail.com>
2025-05-28 14:14:56 +0000

    also show in staging

--- packages/lib/modules/pool/PoolContainer.tsx
+++ packages/lib/modules/pool/PoolContainer.tsx
@@ -4,13 +4,13 @@ import { PoolDetail } from './PoolDetail/PoolDetail'
 import { usePool } from './PoolProvider'
 import { isLBP } from './pool.helpers'
 import { LbpDetail } from './LbpDetail/LbpDetail'
-import { isDev } from '@repo/lib/config/app.config'
+import { isDev, isStaging } from '@repo/lib/config/app.config'
 
 export function PoolContainer() {
   const { pool } = usePool()
 
-  // TODO: only show in dev for now
-  if (isLBP(pool.type) && isDev) {
+  // TODO: only show in dev/staging for now
+  if (isLBP(pool.type) && (isDev || isStaging)) {
     return <LbpDetail />
   }
 
