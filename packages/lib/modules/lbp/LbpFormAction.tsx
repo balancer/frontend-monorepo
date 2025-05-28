5aa371322f2193942fbaf821833be26f478dfc0e
Matt Pereira <mattpereira.dev@gmail.com>
2025-05-28 14:26:14 -0700

    lbp creation modal scaffolding

--- packages/lib/modules/lbp/LbpFormAction.tsx
+++ packages/lib/modules/lbp/LbpFormAction.tsx
@@ -3,9 +3,14 @@
 import { Button, HStack, IconButton } from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
 import { useLbpForm } from './LbpFormProvider'
+import { useLbpCreation } from './LbpCreationProvider'
+import { LbpCreationModal } from './modal/LbpCreationModal'
+import { useRef } from 'react'
 
 export function LbpFormAction({ disabled }: { disabled?: boolean }) {
   const { activeStepIndex, setActiveStep, isLastStep, isFirstStep } = useLbpForm()
+  const { previewModalDisclosure } = useLbpCreation()
+  const nextBtn = useRef(null)
 
   return (
     <HStack spacing="md" w="full">
@@ -18,9 +23,23 @@ export function LbpFormAction({ disabled }: { disabled?: boolean }) {
         />
       )}
 
-      <Button disabled={disabled} size="lg" type="submit" variant="primary" w="full">
+      <Button
+        disabled={disabled}
+        onClick={() => isLastStep && previewModalDisclosure.onOpen()}
+        size="lg"
+        type="submit"
+        variant="primary"
+        w="full"
+      >
         {isLastStep ? 'Create LBP' : 'Next'}
       </Button>
+
+      <LbpCreationModal
+        finalFocusRef={nextBtn}
+        isOpen={previewModalDisclosure.isOpen}
+        onClose={previewModalDisclosure.onClose}
+        onOpen={previewModalDisclosure.onOpen}
+      />
     </HStack>
   )
 }
