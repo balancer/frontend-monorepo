ae417f8a7aeaae77e1d7440ca6417dd7ef9d6922
Alberto Gualis <agualis@gmail.com>
2025-05-28 13:32:05 +0200

    spike: enable react compiler in balancer app

--- apps/frontend-v3/next.config.ts
+++ apps/frontend-v3/next.config.ts
@@ -4,6 +4,9 @@ import type { NextConfig } from 'next'
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
   webpack: config => {
     config.resolve.fallback = { fs: false, net: false, tls: false }
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
