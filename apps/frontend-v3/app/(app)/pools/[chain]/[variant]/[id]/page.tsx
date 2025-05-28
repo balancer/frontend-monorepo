a8da5d4c387645b3b3fe31255aed65be516c20ba
groninge <groninge@gmail.com>
2025-05-28 12:06:10 +0000

    render LbpDetail for LBPs

--- apps/frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/page.tsx
+++ apps/frontend-v3/app/(app)/pools/[chain]/[variant]/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { PoolDetail } from '@repo/lib/modules/pool/PoolDetail/PoolDetail'
+import { PoolContainer } from '@repo/lib/modules/pool/PoolContainer'
 import { TransactionStateProvider } from '@repo/lib/modules/transactions/transaction-steps/TransactionStateProvider'
 
 export default function PoolPage() {
   return (
     <TransactionStateProvider>
-      <PoolDetail />
+      <PoolContainer />
     </TransactionStateProvider>
   )
 }
