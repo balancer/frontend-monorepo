f74b140b42d53eeeb2fb64a9221596e4db4907f7
groninge <groninge@gmail.com>
2025-06-02 15:29:05 +0000

    add noPadding variant

--- packages/lib/shared/services/chakra/themes/base/components.ts
+++ packages/lib/shared/services/chakra/themes/base/components.ts
@@ -698,6 +698,11 @@ export function getComponents(tokens: any, primaryTextColor: string) {
         },
       },
       variants: {
+        noPadding: {
+          container: {
+            padding: '0',
+          },
+        },
         subSection: {
           container: {
             background: 'background.level3',
