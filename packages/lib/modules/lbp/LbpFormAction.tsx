57c9735d9e47df31c28b6e3d6d493e04ece6d27f
Juanjo Molinero <jjmolinero@gmail.com>
2025-06-05 16:24:58 +0200

    [#653] add connect button when no wallet connected

--- packages/lib/modules/lbp/LbpFormAction.tsx
+++ packages/lib/modules/lbp/LbpFormAction.tsx
@@ -3,11 +3,14 @@
 import { Button, HStack, IconButton } from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
 import { useLbpForm } from './LbpFormProvider'
+import { useUserAccount } from '../web3/UserAccountProvider'
+import { ConnectWallet } from '../web3/ConnectWallet'
 
 export function LbpFormAction({ disabled }: { disabled?: boolean }) {
+  const { isConnected } = useUserAccount()
   const { activeStepIndex, setActiveStep, isLastStep, isFirstStep } = useLbpForm()
 
-  return (
+  return isConnected ? (
     <HStack spacing="md" w="full">
       {!isFirstStep && (
         <IconButton
@@ -22,5 +25,7 @@ export function LbpFormAction({ disabled }: { disabled?: boolean }) {
         {isLastStep ? 'Create LBP' : 'Next'}
       </Button>
     </HStack>
+  ) : (
+    <ConnectWallet w="full" variant="primary" />
   )
 }
