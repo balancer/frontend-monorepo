c9a76d55ead795da0a8f4900b315d00017c06430
groninge <groninge@gmail.com>
2025-06-05 12:27:25 +0000

    add new visual WIP

--- packages/lib/modules/reclamm/ReclAmmChartProvider.tsx
+++ packages/lib/modules/reclamm/ReclAmmChartProvider.tsx
@@ -4,16 +4,25 @@ import { bn } from '@repo/lib/shared/utils/numbers'
 import { formatUnits } from 'viem'
 import { useGetComputeReclAmmData } from './useGetComputeReclAmmData'
 import { calculateLowerMargin, calculateUpperMargin } from './reclAmmMath'
-import { useCurrency } from '@repo/lib/shared/hooks/useCurrency'
 import { useMandatoryContext } from '@repo/lib/shared/utils/contexts'
 
 type ReclAmmChartContextType = ReturnType<typeof useReclAmmChartLogic>
 
 const ReclAmmChartContext = createContext<ReclAmmChartContextType | null>(null)
 
+function getGradientColor(colorStops: string[]) {
+  return {
+    type: 'linear',
+    x: 0,
+    y: 0,
+    x2: 0,
+    y2: 1,
+    colorStops: colorStops.map((color, index) => ({ offset: index, color })),
+  }
+}
+
 export function useReclAmmChartLogic() {
   const reclAmmData = useGetComputeReclAmmData()
-  const { toCurrency } = useCurrency()
 
   const currentChartData = useMemo(() => {
     if (
@@ -143,236 +152,123 @@ export function useReclAmmChartLogic() {
   }, [reclAmmData])
 
   const option = useMemo(() => {
-    const series = currentChartData.series
-    if (!series) return {}
-
-    const xValues = series.map(point => point[0])
-    const yValues = series.map(point => point[1])
-
-    const maxPricePoint = currentChartData.markPoints?.find(p => p.name === 'upper limit')
-    const minPricePoint = currentChartData.markPoints?.find(p => p.name === 'lower limit')
-
-    const xMin = maxPricePoint?.coord[0] || Math.min(...xValues)
-    const yMax = maxPricePoint?.coord[1] || Math.max(...yValues)
-    const xMax = minPricePoint?.coord[0] || Math.max(...xValues)
-    const yMin = minPricePoint?.coord[1] || Math.min(...yValues)
-
-    const xPadding = (xMax - xMin) * 0.1
-    const yPadding = (yMax - yMin) * 0.1
+    const leftGreyBars = ['AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ']
+    const rightGreyBars = ['EA', 'EB', 'EC', 'ED', 'EE', 'EF', 'EG', 'EH', 'EI', 'EJ']
+    const leftOrangeBars = ['BA', 'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH']
+    const rightOrangeBars = ['DA', 'DB', 'DC', 'DD', 'DE', 'DF', 'DG', 'DH']
+    const greenBars = [
+      'CAA',
+      'CAB',
+      'CAC',
+      'CAD',
+      'CAE',
+      'CAF',
+      'CAG',
+      'CAH',
+      'CAI',
+      'CAJ',
+      'CAK',
+      'CAL',
+      'CAM',
+      'CAN',
+      'CAO',
+      'CAP',
+      'CAQ',
+      'CAR',
+      'CAS',
+      'CAT',
+      'CAU',
+      'CAV',
+      'CAW',
+      'CAX',
+      'CAY',
+      'CAZ',
+      'CBA',
+      'CBB',
+      'CBC',
+      'CBD',
+      'CBE',
+      'CBF',
+      'CBG',
+      'CBH',
+      'CBI',
+      'CBJ',
+      'CBK',
+      'CBL',
+      'CBM',
+      'CBN',
+      'CBO',
+      'CBP',
+    ]
 
     return {
-      grid: {
-        left: '3%',
-        right: '12%',
-        bottom: '5%',
-        top: '15%',
-        containLabel: true,
-      },
-      visualMap: {
+      xAxis: {
         show: false,
-        dimension: 0,
-        min: currentChartData.max,
-        max: currentChartData.min,
-        inRange: {
-          color: ['#FF4560', '#FC7D02', '#93CE07', '#FC7D02', '#FF4560'],
-        },
-        controller: {
-          inRange: {
-            color: ['#FF4560', '#FC7D02', '#93CE07', '#FC7D02', '#FF4560'],
-          },
-        },
-        pieces: [
-          {
-            lte: currentChartData.max,
-            color: '#FF4560',
-          },
-          {
-            gt: currentChartData.max,
-            lte: currentChartData.lowerMargin,
-            color: '#FC7D02',
-          },
-          {
-            gt: currentChartData.lowerMargin,
-            lte: currentChartData.upperMargin,
-            color: '#93CE07',
-          },
-          {
-            gt: currentChartData.upperMargin,
-            lte: currentChartData.min,
-            color: '#FC7D02',
-          },
-          {
-            gt: currentChartData.min,
-            color: '#FF4560',
-          },
+        type: 'category',
+        data: [
+          ...leftGreyBars,
+          ...leftOrangeBars,
+          ...greenBars,
+          ...rightOrangeBars,
+          ...rightGreyBars,
         ],
       },
-      xAxis: {
-        type: 'value',
-        min: xMin - xPadding,
-        max: xMax + xPadding,
-        axisLabel: {
-          show: true,
-          showMinLabel: false,
-          showMaxLabel: false,
-        },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            color: '#666',
-          },
-        },
-        splitLine: {
-          show: true,
-          lineStyle: {
-            color: '#666',
-            opacity: 0.3,
-          },
-        },
-        axisTick: {
-          show: true,
-        },
-        name: reclAmmData.tokenA?.symbol || '',
-        nameLocation: 'end',
-        nameTextStyle: {
-          align: 'right',
-          verticalAlign: 'bottom',
-          padding: [0, 15, -20, 0],
-          fontSize: 12,
-          color: '#999',
-        },
-      },
       yAxis: {
+        show: false,
         type: 'value',
-        min: yMin - yPadding,
-        max: yMax + yPadding,
-        axisLabel: {
-          show: true,
-          // remove min and max labels instead of hiding them
-          formatter: function (value: number) {
-            if (value === yMin - yPadding || value === yMax + yPadding) {
-              return ''
-            }
-            return value
-          },
-        },
-        axisLine: {
-          show: true,
-          lineStyle: {
-            color: '#666',
-          },
-        },
-        splitLine: {
-          show: true,
-          lineStyle: {
-            color: '#666',
-            opacity: 0.3,
-          },
-        },
-        axisTick: {
-          show: true,
-        },
-        name: reclAmmData.tokenB?.symbol || '',
-        nameLocation: 'end',
-        nameTextStyle: {
-          align: 'left',
-          verticalAlign: 'top',
-          padding: [-20, 0, 0, -40],
-          fontSize: 12,
-          color: '#999',
-        },
       },
       series: [
         {
-          data: series,
-          type: 'line',
-          smooth: true,
-          lineStyle: {
-            width: 3,
-          },
-          symbol: 'none',
-          silent: true,
-          tooltip: {
-            show: false,
-          },
-          emphasis: {
-            disabled: true,
-          },
-          markPoint: {
-            symbol: 'circle',
-            symbolSize: 10,
-            label: {
-              show: false,
-            },
-            data: currentChartData.markPoints,
-          },
-          markLine: {
-            silent: true,
-            symbol: 'none',
-            label: {
-              show: true,
-              position: 'end',
-              distance: 10,
-              formatter: function (params: any) {
-                const pointName = params.name
-
-                const point = currentChartData.markPoints?.find(p => p.name === pointName)
-                if (point) {
-                  return [
-                    '{value|' + toCurrency(point.priceValue, { abbreviated: false }) + '}',
-                    '{name|' + `(${pointName})` + '}',
-                  ].join('\n')
-                }
-
-                return ''
-              },
-              rich: {
-                value: {
-                  color: '#333333',
-                  fontWeight: 'bold',
-                  fontSize: 12,
-                  padding: [2, 0, 1, 0],
-                  align: 'center',
-                  width: 70,
-                  textShadow: 'none',
-                },
-                name: {
-                  color: '#333333',
-                  fontSize: 12,
-                  padding: [1, 0, 2, 0],
-                  align: 'center',
-                  width: 70,
-                  textShadow: 'none',
-                },
-              },
-              backgroundColor: function (params: any) {
-                const point = currentChartData.markPoints?.find(p => p.name === params.name)
-                const color = point?.itemStyle?.color || 'rgba(0, 0, 0, 0.75)'
-                return color
-              },
-              borderColor: 'rgba(255, 255, 255, 0.3)',
-              borderWidth: 1,
-              borderRadius: 4,
-              padding: [4, 8],
-              shadowBlur: 3,
-              shadowColor: 'rgba(0, 0, 0, 0.3)',
-              shadowOffsetX: 1,
-              shadowOffsetY: 1,
-            },
-            data: [
-              ...(currentChartData.markPoints || []).map(point => ({
-                name: point.name,
-                yAxis: point.coord[1],
-
-                lineStyle: {
-                  color: point.itemStyle.color,
-                },
-                label: {
-                  backgroundColor: point.itemStyle.color,
+          data: [
+            ...Array(10)
+              .fill(4)
+              .map(value => ({
+                value,
+                itemStyle: {
+                  color: getGradientColor(['rgba(160, 174, 192, 0.5)', 'rgba(160, 174, 192, 0.1)']),
+                  borderRadius: 20,
                 },
               })),
-            ],
-          },
+            ...Array(8)
+              .fill(100)
+              .map(value => ({
+                value,
+                itemStyle: {
+                  color: getGradientColor(['rgb(253, 186, 116)', 'rgba(151, 111, 69, 0.5)']),
+                  borderRadius: 20,
+                },
+              })),
+            ...Array(42)
+              .fill(100)
+              .map(value => ({
+                value,
+                itemStyle: {
+                  color: getGradientColor(['rgb(99, 242, 190)', 'rgba(57, 140, 110, 0.5)']),
+                  borderRadius: 20,
+                },
+              })),
+            ...Array(8)
+              .fill(100)
+              .map(value => ({
+                value,
+                itemStyle: {
+                  color: getGradientColor(['rgb(253, 186, 116)', 'rgba(151, 111, 69, 0.5)']),
+                  borderRadius: 20,
+                },
+              })),
+            ...Array(10)
+              .fill(4)
+              .map(value => ({
+                value,
+                itemStyle: {
+                  color: getGradientColor(['rgba(160, 174, 192, 0.5)', 'rgba(160, 174, 192, 0.1)']),
+                  borderRadius: 20,
+                },
+              })),
+          ],
+          type: 'bar',
+          barWidth: '90%',
+          barCategoryGap: '25%', // Adds space between bars
         },
       ],
     }
