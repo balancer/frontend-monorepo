44e4543f0609d3bce29407a8520283fa9c05432e
groninge <groninge@gmail.com>
2025-06-10 06:47:20 +0000

    use v3 lbp pool helper

--- packages/lib/modules/pool/PoolContainer.tsx
+++ packages/lib/modules/pool/PoolContainer.tsx
@@ -2,16 +2,15 @@
 
 import { PoolDetail } from './PoolDetail/PoolDetail'
 import { usePool } from './PoolProvider'
-import { isLBP } from './pool.helpers'
+import { isV3LBP } from './pool.helpers'
 import { LbpDetail } from './LbpDetail/LbpDetail'
 import { isDev, isStaging } from '@repo/lib/config/app.config'
-import { isV3Pool } from './pool.helpers'
 
 export function PoolContainer() {
   const { pool } = usePool()
 
   // TODO: only show in dev/staging for now
-  if (isV3Pool(pool) && isLBP(pool.type) && (isDev || isStaging)) {
+  if (isV3LBP(pool) && (isDev || isStaging)) {
     return <LbpDetail />
   }
 
