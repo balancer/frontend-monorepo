5f1055c2997f0bf21d7ccf4741845e36d71c9ff0
uiuxxx <uxuidefi@gmail.com>
2025-06-12 11:30:10 +0100

    Add new TextShine component and use it to highlight the terms of use when it appears in Add Liquidity flow

--- packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormCheckbox.tsx
+++ packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormCheckbox.tsx
@@ -14,6 +14,8 @@ import { RisksList } from '../../../PoolDetail/PoolInfo/PoolRisks/PoolRisks'
 import { useAddLiquidity } from '../AddLiquidityProvider'
 import { useTokenBalances } from '@repo/lib/modules/tokens/TokenBalancesProvider'
 import { InfoIcon } from '@repo/lib/shared/components/icons/InfoIcon'
+import { TextShine } from '@repo/lib/shared/components/TextShine/TextShine'
+import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
 
 export function AddLiquidityFormCheckbox() {
   const { tokens, acceptPoolRisks, setAcceptPoolRisks } = useAddLiquidity()
@@ -24,43 +26,47 @@ export function AddLiquidityFormCheckbox() {
   )
 
   return (
-    <HStack spacing="xs">
-      <Checkbox
-        isChecked={acceptPoolRisks}
-        isDisabled={isBalancesLoading || hasNoPoolTokensInWallet}
-        onChange={e => setAcceptPoolRisks(e.target.checked)}
-        size="lg"
-      >
-        <Text as="div" fontSize="md" lineHeight="1" sx={{ textWrap: 'pretty' }}>
-          I accept the risks of interacting with this pool
-          <Box as="span">
-            <Popover placement="top" trigger="hover">
-              <PopoverTrigger>
-                <IconButton
-                  _hover={{ bg: 'background.level2', opacity: '1' }}
-                  aria-label="pool-risks-info"
-                  bg="background.level2"
-                  icon={<InfoIcon />}
-                  size="xs"
-                  position="relative"
-                  top="-1px"
-                  left="1px"
-                  transition="opacity 0.2s var(--ease-out-cubic)"
-                  opacity="0.6"
-                />
-              </PopoverTrigger>
-              <Box shadow="2xl" zIndex="popover">
-                <PopoverContent>
-                  <PopoverArrow bg="background.level3" />
-                  <PopoverBody>
-                    <RisksList textVariant="primary" />
-                  </PopoverBody>
-                </PopoverContent>
-              </Box>
-            </Popover>
-          </Box>
-        </Text>
-      </Checkbox>
-    </HStack>
+    <FadeInOnView scaleUp={false}>
+      <HStack spacing="xs">
+        <Checkbox
+          isChecked={acceptPoolRisks}
+          isDisabled={isBalancesLoading || hasNoPoolTokensInWallet}
+          onChange={e => setAcceptPoolRisks(e.target.checked)}
+          size="lg"
+        >
+          <Text as="div" fontSize="md" lineHeight="1" sx={{ textWrap: 'pretty' }}>
+            <TextShine animationDuration="1.5s">
+              I accept the risks of interacting with this pool
+            </TextShine>
+            <Box as="span">
+              <Popover placement="top" trigger="hover">
+                <PopoverTrigger>
+                  <IconButton
+                    _hover={{ bg: 'background.level2', opacity: '1' }}
+                    aria-label="pool-risks-info"
+                    bg="background.level2"
+                    icon={<InfoIcon />}
+                    size="xs"
+                    position="relative"
+                    top="-1px"
+                    left="1px"
+                    transition="opacity 0.2s var(--ease-out-cubic)"
+                    opacity="0.6"
+                  />
+                </PopoverTrigger>
+                <Box shadow="2xl" zIndex="popover">
+                  <PopoverContent>
+                    <PopoverArrow bg="background.level3" />
+                    <PopoverBody>
+                      <RisksList textVariant="primary" />
+                    </PopoverBody>
+                  </PopoverContent>
+                </Box>
+              </Popover>
+            </Box>
+          </Text>
+        </Checkbox>
+      </HStack>
+    </FadeInOnView>
   )
 }
