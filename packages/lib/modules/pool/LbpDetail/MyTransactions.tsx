8d9b28c8081fef4fad0b0bd5da1205608db99282
Juanjo Molinero <jjmolinero@gmail.com>
2025-06-18 11:06:40 +0200

    [#1118] update My Transactions section when pool events change

--- packages/lib/modules/pool/LbpDetail/MyTransactions.tsx
+++ packages/lib/modules/pool/LbpDetail/MyTransactions.tsx
@@ -11,7 +11,6 @@ import {
   Link,
   Skeleton,
 } from '@chakra-ui/react'
-import { useEffect, useState } from 'react'
 import { useCurrency } from '@repo/lib/shared/hooks/useCurrency'
 import {
   GetPoolEventsQuery,
@@ -41,6 +40,82 @@ type PoolEventRowProps = {
 type EventType = 'Buy' | 'Sell'
 
 const GRID_COLUMNS = '1fr 2fr 1fr 1fr'
+
+export function MyTransactions({
+  userPoolEvents,
+  isLoading,
+}: {
+  userPoolEvents: GetPoolEventsQuery['poolEvents'] | undefined
+  isLoading: boolean
+}) {
+  const { toCurrency } = useCurrency()
+
+  const { chain, pool } = usePool()
+  const lbpPool = pool as GqlPoolLiquidityBootstrappingV3
+
+  const projectToken = pool.poolTokens[lbpPool.projectTokenIndex]
+
+  return (
+    <Card>
+      {isLoading && <Skeleton h="full" w="full" />}
+      {!isLoading && (
+        <VStack alignItems="flex-start" h="full" spacing="md" w="full">
+          <Heading
+            backgroundClip="text"
+            bg="font.special"
+            fontWeight="bold"
+            lineHeight="34px" // to align with 'My liquidity'
+            size="h5"
+          >
+            My transactions
+          </Heading>
+
+          <Divider />
+
+          <Box display={{ base: 'none', md: 'block' }} w="full">
+            <Grid gap="4" templateColumns={{ base: '1fr', md: GRID_COLUMNS }} w="full">
+              {['Action', 'Tokens', 'Value', 'Time'].map((label, index) => (
+                <GridItem
+                  key={label}
+                  mr={index === 3 ? 'md' : 0}
+                  textAlign={index < 2 ? 'left' : 'right'}
+                >
+                  <Text fontSize="0.85rem" fontWeight="medium" variant="secondary">
+                    {label}
+                  </Text>
+                </GridItem>
+              ))}
+            </Grid>
+            <Divider mt="md" />
+          </Box>
+
+          <Box overflowY="auto" w="full">
+            {!userPoolEvents || isEmpty(userPoolEvents) ? (
+              <>
+                <Text variant="secondary">No recent transactions</Text>
+                <Text variant="secondary">
+                  Note: Recent transactions may take a few minutes to display here.
+                </Text>
+              </>
+            ) : (
+              userPoolEvents.map(event => (
+                <PoolEventRow
+                  chain={chain}
+                  key={event.id}
+                  poolEvent={event}
+                  projectTokenAddress={projectToken.address as Address}
+                  projectTokenLogo={projectToken.logoURI || undefined}
+                  txUrl={getBlockExplorerTxUrl(event.tx, event.chain)}
+                  usdValue={toCurrency(event.valueUSD)}
+                />
+              ))
+            )}
+          </Box>
+        </VStack>
+      )}
+    </Card>
+  )
+}
 
 function Action({ poolEventType }: { poolEventType: EventType }) {
   const eventTypeColor = poolEventType === 'Buy' ? 'green.500' : 'red.500'
@@ -165,88 +240,5 @@ function Tokens({
         </Text>
       </HStack>
     </HStack>
-  )
-}
-
-export function MyTransactions({
-  userPoolEvents,
-  isLoading,
-}: {
-  userPoolEvents: GetPoolEventsQuery['poolEvents'] | undefined
-  isLoading: boolean
-}) {
-  const { toCurrency } = useCurrency()
-  const [poolEvents, setPoolEvents] = useState<PoolEventItem[]>([])
-
-  const { chain, pool } = usePool()
-  const lbpPool = pool as GqlPoolLiquidityBootstrappingV3
-
-  const projectToken = pool.poolTokens[lbpPool.projectTokenIndex]
-
-  useEffect(() => {
-    if (!isLoading && userPoolEvents?.length) {
-      setPoolEvents(userPoolEvents)
-    }
-  }, [userPoolEvents, isLoading])
-
-  return (
-    <Card>
-      {isLoading && <Skeleton h="full" w="full" />}
-      {!isLoading && (
-        <VStack alignItems="flex-start" h="full" spacing="md" w="full">
-          <Heading
-            backgroundClip="text"
-            bg="font.special"
-            fontWeight="bold"
-            lineHeight="34px" // to align with 'My liquidity'
-            size="h5"
-          >
-            My transactions
-          </Heading>
-
-          <Divider />
-
-          <Box display={{ base: 'none', md: 'block' }} w="full">
-            <Grid gap="4" templateColumns={{ base: '1fr', md: GRID_COLUMNS }} w="full">
-              {['Action', 'Tokens', 'Value', 'Time'].map((label, index) => (
-                <GridItem
-                  key={label}
-                  mr={index === 3 ? 'md' : 0}
-                  textAlign={index < 2 ? 'left' : 'right'}
-                >
-                  <Text fontSize="0.85rem" fontWeight="medium" variant="secondary">
-                    {label}
-                  </Text>
-                </GridItem>
-              ))}
-            </Grid>
-            <Divider mt="md" />
-          </Box>
-
-          <Box overflowY="auto" w="full">
-            {isEmpty(poolEvents) ? (
-              <>
-                <Text variant="secondary">No recent transactions</Text>
-                <Text variant="secondary">
-                  Note: Recent transactions may take a few minutes to display here.
-                </Text>
-              </>
-            ) : (
-              poolEvents.map(poolEvent => (
-                <PoolEventRow
-                  chain={chain}
-                  key={poolEvent.id}
-                  poolEvent={poolEvent}
-                  projectTokenAddress={projectToken.address as Address}
-                  projectTokenLogo={projectToken.logoURI || undefined}
-                  txUrl={getBlockExplorerTxUrl(poolEvent.tx, poolEvent.chain)}
-                  usdValue={toCurrency(poolEvent.valueUSD)}
-                />
-              ))
-            )}
-          </Box>
-        </VStack>
-      )}
-    </Card>
   )
 }
