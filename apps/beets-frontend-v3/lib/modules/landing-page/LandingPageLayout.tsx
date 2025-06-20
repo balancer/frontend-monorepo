a46755dbe4029820d7d0f9fe9cb0ef0cf75ff9b3
groninge <groninge@gmail.com>
2025-06-20 12:38:32 +0000

    use beets logo particles

--- apps/beets-frontend-v3/lib/modules/landing-page/LandingPageLayout.tsx
+++ apps/beets-frontend-v3/lib/modules/landing-page/LandingPageLayout.tsx
@@ -1,5 +1,5 @@
 import { Box } from '@chakra-ui/react'
-import { BeetsLandingHeroImg } from './components/BeetsLandingHeroImg'
+
 import { LandingBalancerV3Section } from './sections/LandingBalancerV3Section'
 import { LandingBeetsData } from './sections/LandingBeetsData'
 import { LandingBeetsHero } from './sections/LandingBeetsHero'
@@ -12,6 +12,7 @@ import {
   GetProtocolStatsQuery,
   GetStakedSonicDataQuery,
 } from '@repo/lib/shared/services/api/generated/graphql'
+import { BeetsLogoParticles } from '../../components/logo/beetsLogoParticles'
 
 export function LandingPageLayout({
   protocolData,
@@ -23,27 +24,29 @@ export function LandingPageLayout({
   stakedSonicData: GetStakedSonicDataQuery
 }) {
   return (
-    <Box position="relative">
-      <BeetsLandingHeroImg />
-      <LandingBeetsHero />
-      <LandingBeetsData
-        protocolData={protocolData}
-        protocolDataPerChain={protocolDataPerChain}
-        stakedSonicData={stakedSonicData}
-      />
-      <Box mb="2xl" mt="2xl" pb="3xl">
-        <LandingBeetsStakedSonic />
+    <>
+      <BeetsLogoParticles />
+      <Box position="relative" zIndex={1}>
+        <LandingBeetsHero />
+        <LandingBeetsData
+          protocolData={protocolData}
+          protocolDataPerChain={protocolDataPerChain}
+          stakedSonicData={stakedSonicData}
+        />
+        <Box mb="2xl" mt="2xl" pb="3xl">
+          <LandingBeetsStakedSonic />
+        </Box>
+        <Box mb="2xl" pb="3xl">
+          <LandingBalancerV3Section />
+        </Box>
+        <Box mb="2xl" pb="3xl">
+          <LandingMaBeetsSection />
+        </Box>
+        <Box mb="2xl" pb="3xl">
+          <LandingBeetsSecuritySection />
+        </Box>
+        <LandingBeetsSocialClub />
       </Box>
-      <Box mb="2xl" pb="3xl">
-        <LandingBalancerV3Section />
-      </Box>
-      <Box mb="2xl" pb="3xl">
-        <LandingMaBeetsSection />
-      </Box>
-      <Box mb="2xl" pb="3xl">
-        <LandingBeetsSecuritySection />
-      </Box>
-      <LandingBeetsSocialClub />
-    </Box>
+    </>
   )
 }
