9b4a1b9333c436247bc8b45f4c3f1aabf5345303
groninge <groninge@gmail.com>
2025-07-07 12:18:09 +0000

    fix import path

--- packages/lib/modules/eclp/components/EclpChart.tsx
+++ packages/lib/modules/eclp/components/EclpChart.tsx
@@ -2,7 +2,7 @@
 import { Box } from '@chakra-ui/react'
 import ReactECharts from 'echarts-for-react'
 import { useEclpChart } from '../hooks/EclpChartProvider'
-import { ReversedToggleButton } from '../../../shared/components/btns/ReversedToggleButton'
+import { ReversedToggleButton } from '@repo/lib/shared/components/btns/ReversedToggleButton'
 
 export function EclpChart() {
   const { options, toggleIsReversed } = useEclpChart()
