36b935859c74f0080b0c5c927a0c6b33d6d4d19b
uiuxxx <uxuidefi@gmail.com>
2025-07-11 15:45:39 +0100

    Create new component LabelWithTooltip and adjust underlying TooltipWithTouch component

---
+++ packages/lib/shared/components/tooltips/LabelWithTooltip.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { Text, TextProps } from '@chakra-ui/react'
+import { TooltipWithTouch } from './TooltipWithTouch'
+
+interface LabelWithTooltipProps extends TextProps {
+  label: string
+  tooltip: string
+}
+
+export function LabelWithTooltip({ label, tooltip, ...rest }: LabelWithTooltipProps) {
+  return (
+    <TooltipWithTouch label={tooltip}>
+      <Text
+        _after={{
+          content: '""',
+          position: 'absolute',
+          left: 0,
+          bottom: '-1px',
+          width: '100%',
+          borderBottom: '1.5px dotted',
+          borderColor: 'border.base',
+        }}
+        fontSize="sm"
+        fontWeight="semibold"
+        mb="xs"
+        mt="xxs"
+        position="relative"
+        variant="secondary"
+        w="max-content"
+        {...rest}
+      >
+        {label}
+      </Text>
+    </TooltipWithTouch>
+  )
+}
