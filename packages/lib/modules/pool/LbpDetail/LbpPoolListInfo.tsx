9764fd95e8e9b94592b02b65006b9484a9b2c104
Juanjo Molinero <jjmolinero@gmail.com>
2025-07-11 12:00:10 +0200

    [#1183] add time info when LBP has not started

--- packages/lib/modules/pool/LbpDetail/LbpPoolListInfo.tsx
+++ packages/lib/modules/pool/LbpDetail/LbpPoolListInfo.tsx
@@ -1,6 +1,6 @@
-import { Badge, HStack, Text } from '@chakra-ui/react'
+import { HStack, Text } from '@chakra-ui/react'
 import { PoolListItem } from '../pool.types'
-import { differenceInHours, isAfter, secondsToMilliseconds } from 'date-fns'
+import { differenceInDays, differenceInHours, isAfter, secondsToMilliseconds } from 'date-fns'
 import { now } from '@repo/lib/shared/utils/time'
 import { Clock } from 'react-feather'
 
@@ -13,18 +13,39 @@ export function LbpPoolListInfo({ pool }: Props) {
   const endTime = secondsToMilliseconds(pool.lbpParams?.endTime || 0)
   const hasStarted = startTime && isAfter(now(), startTime)
   const hasEnded = endTime && isAfter(now(), endTime)
-  const hoursDiff = differenceInHours(endTime, now())
 
-  return hasStarted && !hasEnded ? (
-    <HStack gap="xs" textColor="font.warning">
-      <Clock size="12" />
-      <Text color="font.warning" fontSize="xs">{`${hoursDiff}h`}</Text>
-    </HStack>
-  ) : hasEnded ? (
-    <Badge background="red.400" color="font.dark" fontSize="xs" size="12" userSelect="none">
-      Finished
-    </Badge>
-  ) : (
-    ''
-  )
+  if (!hasStarted) {
+    const hoursDiff = differenceInHours(startTime, now())
+    const daysDiff = differenceInDays(startTime, now())
+
+    return (
+      <HStack gap="xs" textColor="green.500">
+        <Clock size="12" />
+        <Text color="green.500" fontSize="xs">
+          Starts: {hoursDiff < 24 ? `${hoursDiff}h` : `${daysDiff}d`}
+        </Text>
+      </HStack>
+    )
+  } else if (hasStarted && !hasEnded) {
+    const hoursDiff = differenceInHours(endTime, now())
+    const daysDiff = differenceInDays(endTime, now())
+
+    return (
+      <HStack gap="xs" textColor="font.warning">
+        <Clock size="12" />
+        <Text color="font.warning" fontSize="xs">
+          Ends: {hoursDiff < 24 ? `${hoursDiff}h` : `${daysDiff}d`}
+        </Text>
+      </HStack>
+    )
+  } else {
+    return (
+      <HStack gap="xs" textColor="font.error">
+        <Clock size="12" />
+        <Text color="font.error" fontSize="xs">
+          Ended
+        </Text>
+      </HStack>
+    )
+  }
 }
