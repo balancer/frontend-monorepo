a3c1b7c1540d53ab316df77a71edfc64aa5ba472
groninge <groninge@gmail.com>
2025-07-18 09:37:17 +0000

    add quantamm pool filter for beets

--- packages/lib/config/projects/beets.ts
+++ packages/lib/config/projects/beets.ts
@@ -27,7 +27,6 @@ export const ProjectConfigBeets: ProjectConfig = {
       GqlPoolType.LiquidityBootstrapping,
       GqlPoolType.CowAmm,
       GqlPoolType.Fx,
-      GqlPoolType.QuantAmmWeighted,
       GqlPoolType.Reclamm,
     ],
     hideProtocolVersion: ['cow'],
