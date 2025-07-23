f23b78686f501aa0774e8bad924963380e31a407
groninge <groninge@gmail.com>
2025-07-23 06:18:37 +0000

    add client with cachebusting

--- packages/lib/shared/services/api/apollo-server.client.ts
+++ packages/lib/shared/services/api/apollo-server.client.ts
@@ -2,12 +2,26 @@ import { config } from '@repo/lib/config/app.config'
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
 import { registerApolloClient } from '@apollo/client-integration-nextjs'
 
-const defaultHeaders = {
+type ClientHeaders = {
+  'x-graphql-client-name': string
+  'x-graphql-client-version': string
+  'x-request-id'?: string
+}
+
+const defaultHeaders: ClientHeaders = {
   'x-graphql-client-name': 'FrontendClient',
   'x-graphql-client-version': '1.0.0',
 }
 
-export const { getClient: getApolloServerClient } = registerApolloClient(() => {
+const createApolloClient = (options: { bustCache?: boolean } = {}) => {
+  const { bustCache = false } = options
+
+  const headers = { ...defaultHeaders }
+
+  if (bustCache) {
+    headers['x-request-id'] = `app-${Date.now()}-${Math.random().toString(36).substring(2, 10)}`
+  }
+
   return new ApolloClient({
     cache: new InMemoryCache({
       typePolicies: {
@@ -18,7 +32,15 @@ export const { getClient: getApolloServerClient } = registerApolloClient(() => {
     }),
     link: new HttpLink({
       uri: config.apiUrl,
-      headers: defaultHeaders,
+      headers,
     }),
   })
-})
+}
+
+// Register the default client (without cache busting)
+export const { getClient: getApolloServerClient } = registerApolloClient(() => createApolloClient())
+
+// Export a function for creating clients with optional cache busting
+export function getApolloServerClientWithOptions(options: { bustCache?: boolean } = {}) {
+  return createApolloClient(options)
+}
