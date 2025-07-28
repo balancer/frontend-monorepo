0e35e22fb2a02626e7f488dc2c1acfa4f3aff7a5
Matt Pereira <mattpereira.dev@gmail.com>
2025-07-28 12:07:40 -0700

    add boosted pool risk checkbox

--- packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormCheckbox.tsx
+++ packages/lib/modules/pool/actions/add-liquidity/form/AddLiquidityFormCheckbox.tsx
@@ -1,6 +1,7 @@
 import {
   Checkbox,
   HStack,
+  VStack,
   Popover,
   PopoverTrigger,
   IconButton,
@@ -16,57 +17,107 @@ import { useTokenBalances } from '@repo/lib/modules/tokens/TokenBalancesProvider
 import { InfoIcon } from '@repo/lib/shared/components/icons/InfoIcon'
 import { TextShine } from '@repo/lib/shared/components/TextShine/TextShine'
 import FadeInOnView from '@repo/lib/shared/components/containers/FadeInOnView'
+import { useState, useEffect } from 'react'
+import { usePool } from '../../../PoolProvider'
+import { isBoosted } from '../../../pool.helpers'
+import { usePoolsMetadata } from '../../../metadata/PoolsMetadataProvider'
 
 export function AddLiquidityFormCheckbox() {
-  const { tokens, acceptPoolRisks, setAcceptPoolRisks } = useAddLiquidity()
-  const { balanceFor, isBalancesLoading } = useTokenBalances()
+  const [hasAcceptedPoolRisks, setHasAcceptedPoolRisks] = useState(false)
+  const [hasAcceptedBoostedRisks, setHasAcceptedBoostedRisks] = useState(false)
 
+  const { tokens, setAcceptPoolRisks } = useAddLiquidity()
+  const { balanceFor, isBalancesLoading } = useTokenBalances()
   const hasNoPoolTokensInWallet = tokens.every(
     token => token && balanceFor(token.address)?.formatted === '0'
   )
 
+  const { pool } = usePool()
+  const isBoostedPool = isBoosted(pool)
+  const { getErc4626Metadata } = usePoolsMetadata()
+  const erc4626Metadata = getErc4626Metadata(pool)
+  const protocolNames = erc4626Metadata.map(metadata => metadata.name.split(' ')[0])
+  const numberOfProtocols = protocolNames.length
+
+  let dynamicProtocolString
+  if (numberOfProtocols === 1) {
+    dynamicProtocolString = `${protocolNames[0]}—an unaffiliated third party. If ${protocolNames[0]} is`
+  } else if (numberOfProtocols === 2) {
+    dynamicProtocolString = `${protocolNames[0]} and ${protocolNames[1]}—unaffiliated third parties. If any of these parties are`
+  } else if (numberOfProtocols > 2) {
+    const allButLast = protocolNames.slice(0, -1).join(', ')
+    const last = protocolNames[protocolNames.length - 1]
+    dynamicProtocolString = `${allButLast}, and ${last}—unaffiliated third parties. If any of these are`
+  }
+  const boostedRiskDescription = `I accept that by adding tokens to this Boosted Pool, my tokens are deposited into ${dynamicProtocolString} compromised, I may lose all my tokens in this pool.`
+
+  useEffect(() => {
+    if (isBoostedPool) {
+      setAcceptPoolRisks(hasAcceptedPoolRisks && hasAcceptedBoostedRisks)
+    } else {
+      setAcceptPoolRisks(hasAcceptedPoolRisks)
+    }
+  }, [hasAcceptedPoolRisks, hasAcceptedBoostedRisks, setAcceptPoolRisks, isBoostedPool])
+
   return (
     <FadeInOnView scaleUp={false}>
-      <HStack spacing="xs">
-        <Checkbox
-          isChecked={acceptPoolRisks}
-          isDisabled={isBalancesLoading || hasNoPoolTokensInWallet}
-          onChange={e => setAcceptPoolRisks(e.target.checked)}
-          size="lg"
-        >
-          <Text as="div" fontSize="md" lineHeight="1" sx={{ textWrap: 'pretty' }}>
-            <TextShine animationDuration="1.5s">
-              I accept the risks of interacting with this pool
-            </TextShine>
-            <Box as="span">
-              <Popover placement="top" trigger="hover">
-                <PopoverTrigger>
-                  <IconButton
-                    _hover={{ bg: 'background.level2', opacity: '1' }}
-                    aria-label="pool-risks-info"
-                    bg="background.level2"
-                    icon={<InfoIcon />}
-                    left="1px"
-                    opacity="0.6"
-                    position="relative"
-                    size="xs"
-                    top="-1px"
-                    transition="opacity 0.2s var(--ease-out-cubic)"
-                  />
-                </PopoverTrigger>
-                <Box shadow="2xl" zIndex="popover">
-                  <PopoverContent>
-                    <PopoverArrow bg="background.level3" />
-                    <PopoverBody>
-                      <RisksList textVariant="primary" />
-                    </PopoverBody>
-                  </PopoverContent>
-                </Box>
-              </Popover>
-            </Box>
-          </Text>
-        </Checkbox>
-      </HStack>
+      <VStack align="start" spacing="sm">
+        <HStack spacing="xs">
+          <Checkbox
+            isChecked={hasAcceptedPoolRisks}
+            isDisabled={isBalancesLoading || hasNoPoolTokensInWallet}
+            onChange={e => setHasAcceptedPoolRisks(e.target.checked)}
+            size="lg"
+          >
+            <Text as="div" fontSize="md" lineHeight="1" sx={{ textWrap: 'pretty' }}>
+              <TextShine animationDuration="1.5s">
+                I accept the risks of interacting with this pool
+              </TextShine>
+              <Box as="span">
+                <Popover placement="top" trigger="hover">
+                  <PopoverTrigger>
+                    <IconButton
+                      _hover={{ bg: 'background.level2', opacity: '1' }}
+                      aria-label="pool-risks-info"
+                      bg="background.level2"
+                      icon={<InfoIcon />}
+                      left="1px"
+                      opacity="0.6"
+                      position="relative"
+                      size="xs"
+                      top="-1px"
+                      transition="opacity 0.2s var(--ease-out-cubic)"
+                    />
+                  </PopoverTrigger>
+                  <Box shadow="2xl" zIndex="popover">
+                    <PopoverContent>
+                      <PopoverArrow bg="background.level3" />
+                      <PopoverBody>
+                        <RisksList textVariant="primary" />
+                      </PopoverBody>
+                    </PopoverContent>
+                  </Box>
+                </Popover>
+              </Box>
+            </Text>
+          </Checkbox>
+        </HStack>
+        {isBoostedPool && (
+          <HStack spacing="xs">
+            <Checkbox
+              alignItems="flex-start"
+              isChecked={hasAcceptedBoostedRisks}
+              isDisabled={isBalancesLoading || hasNoPoolTokensInWallet}
+              onChange={e => setHasAcceptedBoostedRisks(e.target.checked)}
+              size="lg"
+            >
+              <Text as="div" fontSize="md" lineHeight="1" sx={{ textWrap: 'pretty' }}>
+                <TextShine animationDuration="1.5s">{boostedRiskDescription}</TextShine>
+              </Text>
+            </Checkbox>
+          </HStack>
+        )}
+      </VStack>
     </FadeInOnView>
   )
 }
