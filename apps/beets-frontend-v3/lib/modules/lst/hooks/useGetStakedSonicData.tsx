cded12fd6897a0df45cadd040731d726d3918c7c
groninge <groninge@gmail.com>
2025-08-13 14:10:47 +0000

    Revert "update import again"
    This reverts commit 53b98291e52bad6ff3be9358c862b152960dc100.

--- apps/beets-frontend-v3/lib/modules/lst/hooks/useGetStakedSonicData.tsx
+++ apps/beets-frontend-v3/lib/modules/lst/hooks/useGetStakedSonicData.tsx
@@ -1,6 +1,6 @@
 import { GetStakedSonicDataDocument } from '@repo/lib/shared/services/api/generated/graphql'
 import { useQuery } from '@apollo/client'
-import { minutesToMilliseconds } from 'date-fns/minutesToMilliseconds'
+import { minutesToMilliseconds } from 'date-fns'
 
 export function useGetStakedSonicData() {
   return useQuery(GetStakedSonicDataDocument, {
