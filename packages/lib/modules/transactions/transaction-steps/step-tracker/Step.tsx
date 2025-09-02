3ac28136107c20b6de3a033dc17e25679c5b6122
groninge <groninge@gmail.com>
2025-09-02 12:49:14 +0000

    update text

--- packages/lib/modules/transactions/transaction-steps/step-tracker/Step.tsx
+++ packages/lib/modules/transactions/transaction-steps/step-tracker/Step.tsx
@@ -98,7 +98,7 @@ function NestedInfo({
     <Box mb="0" mt="0" p="0.5" pl="0">
       {gasCostData ? (
         <Text color={color} fontSize="sm" lineHeight="1">
-          {gasCostData.isActual ? 'Gas cost' : 'Estimated gas'}: {gasCostData.costUsd}
+          {gasCostData.isActual ? 'Final gas' : 'Estimated gas'}: {gasCostData.costUsd}
         </Text>
       ) : (
         <Text color={color} fontSize="sm" lineHeight="1">
