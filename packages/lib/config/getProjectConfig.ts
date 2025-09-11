ce5390373df5e739b2c4e32a4fec4ea242d34f3b
groninge <groninge@gmail.com>
2025-09-11 06:18:02 +0000

    type it

--- packages/lib/config/getProjectConfig.ts
+++ packages/lib/config/getProjectConfig.ts
@@ -1,6 +1,7 @@
 import { ProjectConfigBeets } from './projects/beets'
 import { ProjectConfigBalancer } from './projects/balancer'
 import { ProjectConfigCowAmm } from './projects/cow-amm'
+import { ProjectConfig } from './config.types'
 
 const PROJECT_CONFIGS = {
   [ProjectConfigBalancer.projectId]: ProjectConfigBalancer,
@@ -8,9 +9,9 @@ const PROJECT_CONFIGS = {
   [ProjectConfigCowAmm.projectId]: ProjectConfigCowAmm,
 }
 
-const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID as ProjectConfig['projectId']
 
 export const isBalancer = projectId === ProjectConfigBalancer.projectId
 export const isBeets = projectId === ProjectConfigBeets.projectId
 export const isCowAmm = projectId === ProjectConfigCowAmm.projectId
-export const PROJECT_CONFIG = projectId ? PROJECT_CONFIGS[projectId] : ProjectConfigBalancer
+export const PROJECT_CONFIG = PROJECT_CONFIGS[projectId] ?? ProjectConfigBalancer
