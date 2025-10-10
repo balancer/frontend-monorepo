0fb24e52a4a19fb812ee5c133182663fc77c9562
groninge <groninge@gmail.com>
2025-10-10 17:17:04 +0200

    add slippage & minOut

--- apps/beets-frontend-v3/lib/modules/loops/components/LoopsWithdrawSummary.tsx
+++ apps/beets-frontend-v3/lib/modules/loops/components/LoopsWithdrawSummary.tsx
@@ -1,4 +1,14 @@
-import { Card } from '@chakra-ui/react'
+import {
+  Box,
+  Card,
+  HStack,
+  Popover,
+  //PopoverContent,
+  PopoverTrigger,
+  VStack,
+  Text,
+  Skeleton,
+} from '@chakra-ui/react'
 import { AnimateHeightChange } from '@repo/lib/shared/components/animations/AnimateHeightChange'
 import { useBreakpoints } from '@repo/lib/shared/hooks/useBreakpoints'
 import { MobileStepTracker } from '@repo/lib/modules/transactions/transaction-steps/step-tracker/MobileStepTracker'
@@ -7,12 +17,19 @@ import { BeetsTokenRow } from '../../../components/shared/BeetsTokenRow'
 import { useLoops } from '../LoopsProvider'
 import { formatUnits } from 'viem'
 import { useLoopsGetFlyQuote } from '@/lib/modules/loops/hooks/useLoopsGetFlyQuote'
+import { NumberText } from '@repo/lib/shared/components/typography/NumberText'
+import { bn, fNum } from '@repo/lib/shared/utils/numbers'
+import { InfoIcon } from '@repo/lib/shared/components/icons/InfoIcon'
+import { useTokens } from '@repo/lib/modules/tokens/TokensProvider'
+import { useCurrency } from '@repo/lib/shared/hooks/useCurrency'
 
 export function LoopsWithdrawSummary({
   isLoading: isLoadingReceipt,
   receivedToken,
 }: LoopsWithdrawReceiptResult) {
   const { isMobile } = useBreakpoints()
+  const { usdValueForToken } = useTokens()
+  const { toCurrency } = useCurrency()
 
   const {
     chain,
@@ -23,7 +40,17 @@ export function LoopsWithdrawSummary({
     amountShares,
   } = useLoops()
 
-  const { wethAmountOut, isLoading: isLoadingFlyQuote } = useLoopsGetFlyQuote(amountShares, chain)
+  const {
+    wethAmountOut,
+    isLoading: isLoadingFlyQuote,
+    minWethAmountOut,
+  } = useLoopsGetFlyQuote(amountShares, chain)
+
+  const slippage = bn(bn(wethAmountOut).minus(minWethAmountOut)).div(wethAmountOut).times(100)
+
+  const slippageUsdValue = bn(slippage)
+    .div(100)
+    .times(usdValueForToken(wNativeAsset, formatUnits(wethAmountOut, wNativeAsset?.decimals ?? 18)))
 
   const shouldShowReceipt = !!loopsWithdrawTxHash && !isLoadingReceipt && !!receivedToken
 
@@ -52,6 +79,71 @@ export function LoopsWithdrawSummary({
           }
         />
       </Card>
+      <VStack align="start" fontSize="sm" mt="sm" spacing="sm" w="full">
+        <HStack justify="space-between" w="full">
+          <Text color="grayText" fontSize="sm">
+            Max slippage
+          </Text>
+          <HStack>
+            {isLoadingFlyQuote ? (
+              <Skeleton h="16px" my="1px" w="40px" />
+            ) : (
+              <NumberText color="grayText" fontSize="sm">
+                {`-${toCurrency(slippageUsdValue, { abbreviated: false })} (-${fNum('slippage', slippage)})`}
+              </NumberText>
+            )}
+            <Popover trigger="hover">
+              <PopoverTrigger>
+                <Box
+                  _hover={{ opacity: 1 }}
+                  opacity="0.5"
+                  transition="opacity 0.2s var(--ease-out-cubic)"
+                >
+                  <InfoIcon />
+                </Box>
+              </PopoverTrigger>
+              {/* <PopoverContent p="sm">
+                    <Text fontSize="sm" variant="secondary">
+                      {slippageLabel}
+                    </Text>
+                  </PopoverContent> */}
+            </Popover>
+          </HStack>
+        </HStack>
+        <HStack justify="space-between" w="full">
+          <Text color="grayText" fontSize="sm">
+            You'll get at least
+          </Text>
+          <HStack>
+            {isLoadingFlyQuote ? (
+              <Skeleton h="16px" my="1px" w="40px" />
+            ) : (
+              <NumberText color="grayText" fontSize="sm">
+                {fNum('token', formatUnits(minWethAmountOut, wNativeAsset?.decimals ?? 18), {
+                  abbreviated: false,
+                })}{' '}
+                {wNativeAsset?.symbol}
+              </NumberText>
+            )}
+            <Popover trigger="hover">
+              <PopoverTrigger>
+                <Box
+                  _hover={{ opacity: 1 }}
+                  opacity="0.5"
+                  transition="opacity 0.2s var(--ease-out-cubic)"
+                >
+                  <InfoIcon />
+                </Box>
+              </PopoverTrigger>
+              {/* <PopoverContent p="sm">
+                    <Text fontSize="sm" variant="secondary">
+                      {limitTooltip}
+                    </Text>
+                  </PopoverContent> */}
+            </Popover>
+          </HStack>
+        </HStack>
+      </VStack>
     </AnimateHeightChange>
   )
 }
