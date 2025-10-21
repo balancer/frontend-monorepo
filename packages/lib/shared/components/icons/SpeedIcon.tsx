daad6c8a9eba65181e0264ac73ea383d71155b2f
groninge <groninge@gmail.com>
2025-10-21 14:50:26 +0200

    fix icon

--- packages/lib/shared/components/icons/SpeedIcon.tsx
+++ packages/lib/shared/components/icons/SpeedIcon.tsx
@@ -3,24 +3,24 @@ export function SpeedIcon({ size = 24 }: { size?: number }) {
     <svg
       fill="none"
       height={size}
-      viewBox="0 0 20 19"
+      viewBox="0 0 20 20"
       width={size}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_3425_495)">
+      <g clipPath="url(#clip0_3425_495)">
         <path
           d="M3.51088 13.1957C2.67177 12.3566 2.10033 11.2875 1.86883 10.1236C1.63732 8.95976 1.75614 7.75336 2.21027 6.65701C2.6644 5.56066 3.43344 4.62359 4.42013 3.9643C5.40682 3.30502 6.56686 2.95313 7.75354 2.95312C8.94023 2.95312 10.1003 3.30502 11.087 3.9643C12.0736 4.62359 12.8427 5.56066 13.2968 6.65701C13.7509 7.75336 13.8698 8.95976 13.6383 10.1236C13.4068 11.2875 12.8353 12.3566 11.9962 13.1957"
           stroke="#A0AEC0"
-          stroke-width="1.2"
           strokeLinecap="round"
           strokeLinejoin="round"
+          strokeWidth="1.2"
         />
         <path
           d="M10.4202 6.28125L7.75354 8.94792"
           stroke="#A0AEC0"
-          stroke-width="1.2"
           strokeLinecap="round"
           strokeLinejoin="round"
+          strokeWidth="1.2"
         />
       </g>
       <defs>
