e94af7b5d20f881a184c5192405539ccc0c3ffd2
groninge <groninge@gmail.com>
2025-10-28 11:19:58 +0100

    remove test

--- packages/e2e-tests/tests/build/cow/cow.landing.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test'
-
-test('Cow landing page', async ({ page }) => {
-  await page.goto('http://localhost:3002/')
-  await page.getByRole('group').filter({ hasText: 'WETHWETH50%GNOGNO50%' }).click()
-})
