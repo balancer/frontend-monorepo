e4742672a881480e832f7dece000c5482c5e282c
groninge <groninge@gmail.com>
2025-11-03 14:16:20 +0100

    dynamic project name

--- packages/lib/modules/pool/actions/recovery-mode/RecoveryModeAlert.tsx
+++ packages/lib/modules/pool/actions/recovery-mode/RecoveryModeAlert.tsx
@@ -2,12 +2,14 @@ import { BalAlertButton } from '@repo/lib/shared/components/alerts/BalAlertButto
 import { Pool } from '../../pool.types'
 import { AlertStatus } from '@chakra-ui/react'
 import { usePathname, useRouter } from 'next/navigation'
+import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
 
 export function recoveryModeAlert(pool: Pool) {
   const isV3 = pool.protocolVersion === 3
+
   const content = isV3
     ? 'Liquidity can’t be removed unless recovery mode is enabled. You may trigger recovery or wait for someone else to do it.'
-    : 'Liquidity can’t be removed until Balancer governance authorized recovery mode after assessing the situation.'
+    : `Liquidity can’t be removed until ${PROJECT_CONFIG.projectName} governance authorized recovery mode after assessing the situation.`
 
   return {
     identifier: 'poolIsPaused',
