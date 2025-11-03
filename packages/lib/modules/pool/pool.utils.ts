6fa7dd6a9033b5fd5e66d2fddbd978e414329310
Juanjo Molinero <jjmolinero@gmail.com>
2025-11-03 10:29:40 +0100

    [#1750] add support for XLayer chain

--- packages/lib/modules/pool/pool.utils.ts
+++ packages/lib/modules/pool/pool.utils.ts
@@ -44,6 +44,7 @@ export enum ChainSlug {
   Sonic = 'sonic',
   HyperEVM = 'hyperevm',
   Plasma = 'plasma',
+  Xlayer = 'xlayer',
 }
 
 // Maps GraphQL chain enum to URL slug
@@ -63,6 +64,7 @@ export const chainToSlugMap: Partial<Record<GqlChainValues, ChainSlug>> = {
   [GqlChain.Sonic]: ChainSlug.Sonic,
   [GqlChain.Hyperevm]: ChainSlug.HyperEVM,
   [GqlChain.Plasma]: ChainSlug.Plasma,
+  [GqlChain.Xlayer]: ChainSlug.Xlayer,
 }
 
 export function getChainSlug(chainSlug: ChainSlug): GqlChain {
