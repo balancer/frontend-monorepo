ba4070d10d2ac06cd681b3023825d4ad25c08923
groninge <groninge@gmail.com>
2025-11-27 13:48:08 +0100

    swap wS

--- packages/e2e-tests/tests/dev/beets/swap.spec.ts
+++ packages/e2e-tests/tests/dev/beets/swap.spec.ts
@@ -4,14 +4,14 @@ import { expect, test } from '@playwright/test'
 import { defaultAnvilAccount } from '@repo/lib/test/utils/wagmi/fork.helpers'
 import { setForkBalances } from '@/helpers/e2e.helpers'
 
-test('Swap 1000 BEETS to stS)', async ({ page }) => {
+test('Swap 1000 wS to stS)', async ({ page }) => {
   await setForkBalances(page, {
     chainId: 146,
     forkBalances: {
       146: [
         {
-          tokenAddress: '0x2d0e0814e62d80056181f5cd932274405966e4f0', // BEETS
-          value: '10000',
+          tokenAddress: '0x039e2fb66102314ce7b64ce5ce3e5183bc94ad38', // wS
+          value: '1000',
         },
       ],
     },
@@ -26,19 +26,15 @@ test('Swap 1000 BEETS to stS)', async ({ page }) => {
   await clickLink(page, 'Swap')
 
   await clickButton(page, 'S')
-  await page.getByText('BEETSBEETS', { exact: true }).click()
+  await page.getByText('Wrapped Sonic', { exact: true }).click()
   await clickButton(page, 'Select token')
   await page.getByText('Beets Staked Sonic').click()
   await page.getByRole('spinbutton', { name: 'TokenIn' }).fill('1000')
 
   await clickButton(page, 'Next')
 
-  if (await isButtonVisible(page, 'Approve BEETS to swap')) {
-    await clickButton(page, 'Approve BEETS to swap')
-  }
-
-  if (await isButtonVisible(page, 'Sign permit: BEETS ')) {
-    await clickButton(page, 'Sign permit: BEETS')
+  if (await isButtonVisible(page, 'Approve wS to swap')) {
+    await clickButton(page, 'Approve wS to swap')
   }
 
   await clickButton(page, 'Swap')
