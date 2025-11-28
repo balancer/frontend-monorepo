4afb909d1b5b969ebc8efb9ebafa0c4ae23ad846
groninge <groninge@gmail.com>
2025-11-28 09:50:19 +0100

    fis unstake test

--- packages/e2e-tests/tests/dev/beets/unstake.spec.ts
+++ packages/e2e-tests/tests/dev/beets/unstake.spec.ts
@@ -4,7 +4,7 @@ import { expect, test } from '@playwright/test'
 import { defaultAnvilAccount } from '@repo/lib/test/utils/wagmi/fork.helpers'
 
 test('Unstake stS on /stake', async ({ page }) => {
-  await page.goto('http://localhost:3001/pools')
+  await page.goto('http://localhost:3001/portfolio')
   await impersonate(page, defaultAnvilAccount)
 
   // Wait for dev tools panel to fully close before checking wallet button
@@ -17,10 +17,11 @@ test('Unstake stS on /stake', async ({ page }) => {
   await expect(page.getByRole('button', { name: 'Stake' })).toBeVisible()
 
   // The Unstake tab should be clicked to become active
-  await page.getByRole('button', { name: 'Unstake' }).click()
+  //await page.getByRole('button', { name: 'Unstake' }).click()
+  await page.locator('#button-group-1').click()
 
   // Confirm unstake tab is active
-  await expect(page.getByRole('button', { name: 'stS' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'stS', exact: true })).toBeVisible()
 
   // Fill in the amount to unstake
   await page.getByPlaceholder('0.00').nth(0).fill('1')
