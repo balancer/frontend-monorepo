a884d0e858d94fbf793bf123e05905525f3e5b48
groninge <groninge@gmail.com>
2025-12-01 13:55:37 +0100

    fix linting issue usePoolMetadata

--- packages/lib/modules/pool/metadata/usePoolMetadata.tsx
+++ packages/lib/modules/pool/metadata/usePoolMetadata.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/preserve-manual-memoization */
 import { Pool } from '../pool.types'
 import { PoolListItem } from '@repo/lib/modules/pool/pool.types'
 import { usePoolsMetadata } from './PoolsMetadataProvider'
@@ -7,7 +6,7 @@ import { useMemo } from 'react'
 export function usePoolMetadata(pool: Pick<Pool | PoolListItem, 'chain' | 'address'>) {
   const { getPoolMetadata } = usePoolsMetadata()
 
-  const poolMetadata = useMemo(() => getPoolMetadata(pool), [pool])
+  const poolMetadata = useMemo(() => getPoolMetadata(pool), [pool, getPoolMetadata])
 
   return { ...poolMetadata }
 }
