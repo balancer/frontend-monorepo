20f6f76838b333ff9166320af0da022993d0ecb6
Matt Pereira <mattpereira.dev@gmail.com>
2025-12-03 10:51:21 -0800

    Fix token icon url input validation UX (#1887)
    * fix token icon url input validation
    
    * remove isChecking state for image validation
    
    * use image decode for validation

--- packages/lib/shared/hooks/url.hooks.ts
+++ packages/lib/shared/hooks/url.hooks.ts
@@ -2,42 +2,26 @@ import { useEffect, useState } from 'react'
 import { isValidUrl, normalizeUrl } from '../utils/urls'
 
 export function useCheckImageUrl(url: string) {
-  const [isChecking, setChecking] = useState(true)
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined)
 
   useEffect(() => {
     if (isValidUrl(url) !== true) return
 
     const image = new Image()
+    image.src = normalizeUrl(url)
 
-    image.onload = function () {
-      if (image.width <= 0) {
-        setErrorMessage('Invalid image')
-      }
-
-      setChecking(false)
-    }
-
-    image.onerror = function () {
-      setErrorMessage('Unreachable URL or invalid image')
-      setChecking(false)
-    }
-
-    // We use an async call to avoid linting issues like
-    // https://react.dev/reference/eslint-plugin-react-hooks/lints/set-state-in-effect
-    const updateImageUrl = async () => {
-      setErrorMessage(undefined)
-      setChecking(true)
-      image.src = normalizeUrl(url)
-    }
-    updateImageUrl()
+    image
+      .decode()
+      .then(() => {
+        setErrorMessage(undefined)
+      })
+      .catch(e => {
+        console.error(e)
+        setErrorMessage('Unreachable URL or invalid image')
+      })
   }, [url])
 
-  if (isValidUrl(url) !== true) return { isChecking: false, error: isValidUrl(url) }
-  if (isChecking) return { isChecking: true, error: undefined }
+  if (isValidUrl(url) !== true) return { error: isValidUrl(url) as string }
 
-  return {
-    isChecking: false,
-    error: errorMessage,
-  }
+  return { error: errorMessage }
 }
