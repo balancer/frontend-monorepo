964640a13aad34deb73fa790781c7b1d8764d1bc
groninge <groninge@gmail.com>
2025-12-03 13:02:41 +0100

    fix linting issue useSignRelayerStep

--- packages/lib/modules/transactions/transaction-steps/useSignRelayerStep.tsx
+++ packages/lib/modules/transactions/transaction-steps/useSignRelayerStep.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/preserve-manual-memoization */
 'use client'
 
 import { useSignRelayerApproval } from '@repo/lib/modules/relayer/signRelayerApproval.hooks'
@@ -6,7 +5,7 @@ import { ConnectWallet } from '@repo/lib/modules/web3/ConnectWallet'
 import { useUserAccount } from '@repo/lib/modules/web3/UserAccountProvider'
 import { Alert, Button, VStack } from '@chakra-ui/react'
 import { TransactionStep } from './lib'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 import { NetworkSwitchButton, useChainSwitch } from '../../web3/useChainSwitch'
 import { getChainId } from '@repo/lib/config/app.config'
 import { GqlChain } from '@repo/lib/shared/services/api/generated/graphql'
@@ -15,6 +14,54 @@ import { LabelWithIcon } from '@repo/lib/shared/components/btns/button-group/Lab
 
 export const signRelayerStepTitle = 'Sign relayer'
 
+interface SignRelayerButtonProps {
+  error: string | undefined
+  isConnected: boolean
+  isLoading: boolean
+  shouldChangeNetwork: boolean
+  networkSwitchButtonProps: ReturnType<typeof useChainSwitch>['networkSwitchButtonProps']
+  isDisabled: boolean
+  buttonLabel: string
+  signRelayer: () => void
+}
+
+function SignRelayerButton({
+  error,
+  isConnected,
+  isLoading,
+  shouldChangeNetwork,
+  networkSwitchButtonProps,
+  isDisabled,
+  buttonLabel,
+  signRelayer,
+}: SignRelayerButtonProps) {
+  return (
+    <VStack width="full">
+      {error && (
+        <Alert rounded="md" status="error">
+          {error}
+        </Alert>
+      )}
+      {!isConnected && <ConnectWallet isLoading={isLoading} width="full" />}
+      {shouldChangeNetwork && isConnected && <NetworkSwitchButton {...networkSwitchButtonProps} />}
+      {!shouldChangeNetwork && isConnected && (
+        <Button
+          isDisabled={isDisabled}
+          isLoading={isLoading}
+          loadingText={buttonLabel}
+          onClick={signRelayer}
+          size="lg"
+          variant="primary"
+          w="full"
+          width="full"
+        >
+          <LabelWithIcon icon="sign">{buttonLabel}</LabelWithIcon>
+        </Button>
+      )}
+    </VStack>
+  )
+}
+
 export function useSignRelayerStep(chain: GqlChain): TransactionStep {
   const chainId = getChainId(chain)
   const { isConnected } = useUserAccount()
@@ -22,37 +69,10 @@ export function useSignRelayerStep(chain: GqlChain): TransactionStep {
     useSignRelayerApproval(chainId)
   const { shouldChangeNetwork, networkSwitchButtonProps } = useChainSwitch(chainId)
 
-  function SignRelayerButton() {
-    return (
-      <VStack width="full">
-        {error && (
-          <Alert rounded="md" status="error">
-            {error}
-          </Alert>
-        )}
-        {!isConnected && <ConnectWallet isLoading={isLoading} width="full" />}
-        {shouldChangeNetwork && isConnected && (
-          <NetworkSwitchButton {...networkSwitchButtonProps} />
-        )}
-        {!shouldChangeNetwork && isConnected && (
-          <Button
-            isDisabled={isDisabled}
-            isLoading={isLoading}
-            loadingText={buttonLabel}
-            onClick={signRelayer}
-            size="lg"
-            variant="primary"
-            w="full"
-            width="full"
-          >
-            <LabelWithIcon icon="sign">{buttonLabel}</LabelWithIcon>
-          </Button>
-        )}
-      </VStack>
-    )
-  }
-
-  const isComplete = () => signRelayerState === SignatureState.Completed
+  const isComplete = useCallback(
+    () => signRelayerState === SignatureState.Completed,
+    [signRelayerState]
+  )
 
   return useMemo(
     () => ({
@@ -65,9 +85,30 @@ export function useSignRelayerStep(chain: GqlChain): TransactionStep {
         tooltip: 'Sign relayer',
       },
       isComplete,
-      renderAction: () => <SignRelayerButton />,
+      renderAction: () => (
+        <SignRelayerButton
+          buttonLabel={buttonLabel}
+          error={error}
+          isConnected={isConnected}
+          isDisabled={isDisabled}
+          isLoading={isLoading}
+          networkSwitchButtonProps={networkSwitchButtonProps}
+          shouldChangeNetwork={shouldChangeNetwork}
+          signRelayer={signRelayer}
+        />
+      ),
     }),
 
-    [signRelayerState, isLoading, isConnected]
+    [
+      isComplete,
+      isLoading,
+      isConnected,
+      error,
+      shouldChangeNetwork,
+      networkSwitchButtonProps,
+      isDisabled,
+      buttonLabel,
+      signRelayer,
+    ]
   )
 }
