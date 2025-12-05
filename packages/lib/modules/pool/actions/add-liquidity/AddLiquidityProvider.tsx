d28fe80c18d04e3966924c68744018b4ac836a83
groninge <groninge@gmail.com>
2025-12-05 11:12:20 +0100

    Revert "fix linting issue AddLiquidityProvider"
    This reverts commit 44b8512b484e7a37ed69369ed62f2eec84ee5eb3.

--- packages/lib/modules/pool/actions/add-liquidity/AddLiquidityProvider.tsx
+++ packages/lib/modules/pool/actions/add-liquidity/AddLiquidityProvider.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 'use client'
 
 import { useTokens } from '@repo/lib/modules/tokens/TokensProvider'
 import { useMandatoryContext } from '@repo/lib/shared/utils/contexts'
 import { HumanAmount, isSameAddress } from '@balancer/sdk'
-import { PropsWithChildren, createContext, useMemo, useState } from 'react'
+import { PropsWithChildren, createContext, useEffect, useMemo, useState } from 'react'
 import { Address, Hash } from 'viem'
 import { usePool } from '../../PoolProvider'
 import { useAddLiquiditySimulationQuery } from './queries/useAddLiquiditySimulationQuery'
@@ -36,41 +37,27 @@ import { ApiToken } from '@repo/lib/modules/tokens/token.types'
 import { useIsMinimumDepositMet } from './useIsMinimumDepositMet'
 import { useWrapUnderlying } from '../useWrapUnderlying'
 
-function mapTokensToEmptyHumanAmounts(tokens: ApiToken[]): HumanTokenAmountWithAddress[] {
-  return tokens.map(
-    token =>
-      ({
-        tokenAddress: token.address as Address,
-        humanAmount: '',
-      }) as HumanTokenAmountWithAddress
-  )
-}
-
 export type UseAddLiquidityResponse = ReturnType<typeof useAddLiquidityLogic>
 export const AddLiquidityContext = createContext<UseAddLiquidityResponse | null>(null)
 
 export function useAddLiquidityLogic(urlTxHash?: Hash) {
-  const { pool, refetch: refetchPool, isLoading } = usePool()
-  const { wrapUnderlying, setWrapUnderlyingByIndex } = useWrapUnderlying(pool)
-
-  // Actionable tokens selected in the add form
-  const tokens = getPoolActionableTokens(pool, wrapUnderlying)
-
-  const [humanAmountsIn, setHumanAmountsIn] = useState<HumanTokenAmountWithAddress[]>(() =>
-    mapTokensToEmptyHumanAmounts(tokens)
-  )
+  const [humanAmountsIn, setHumanAmountsIn] = useState<HumanTokenAmountWithAddress[]>([])
   // only used by Proportional handlers that require a referenceAmount
   const [referenceAmountAddress, setReferenceAmountAddress] = useState<Address | undefined>()
   const [needsToAcceptHighPI, setNeedsToAcceptHighPI] = useState(false)
   const [acceptPoolRisks, setAcceptPoolRisks] = useState(false)
   const [wethIsEth, setWethIsEth] = useState(false)
-
+  const [totalUSDValue, setTotalUSDValue] = useState('0')
+  const { pool, refetch: refetchPool, isLoading } = usePool()
+  const { wrapUnderlying, setWrapUnderlyingByIndex } = useWrapUnderlying(pool)
   /* wantsProportional is true when:
     - the pool requires proportional input
     - the user selected the proportional tab
   */
   const [wantsProportional, setWantsProportional] = useState(requiresProportionalInput(pool))
+
   const { getNativeAssetToken, getWrappedNativeAssetToken, isLoadingTokenPrices } = useTokens()
+
   const { isConnected } = useUserAccount()
   const { hasValidationErrors } = useTokenInputsValidation()
   const { slippage } = useUserSettings()
@@ -89,6 +76,9 @@ export function useAddLiquidityLogic(urlTxHash?: Hash) {
   const nativeAsset = getNativeAssetToken(chain)
   const wNativeAsset = getWrappedNativeAssetToken(chain)
 
+  // Actionable tokens selected in the add form
+  const tokens = getPoolActionableTokens(pool, wrapUnderlying)
+
   // All tokens that can be used in the pool form
   // standard tokens + wrapped/native asset (when wrapped native asset is present) + wrapped/underlying tokens (when the token is boosted)
   const validTokens = [
@@ -99,7 +89,14 @@ export function useAddLiquidityLogic(urlTxHash?: Hash) {
   const { usdValueFor } = useTotalUsdValue(validTokens)
 
   function setInitialHumanAmountsIn() {
-    setHumanAmountsIn(mapTokensToEmptyHumanAmounts(tokens))
+    const amountsIn = tokens.map(
+      token =>
+        ({
+          tokenAddress: token.address,
+          humanAmount: '',
+        }) as HumanTokenAmountWithAddress
+    )
+    setHumanAmountsIn(amountsIn)
   }
 
   function setHumanAmountIn(token: ApiToken, humanAmount: HumanAmount | '') {
@@ -130,11 +127,11 @@ export function useAddLiquidityLogic(urlTxHash?: Hash) {
 
   const tokensWithNativeAsset = replaceWrappedWithNativeAsset(tokens, nativeAsset)
 
-  const totalUSDValue = useMemo(() => {
-    if (isLoadingTokenPrices) return '0'
-
-    return usdValueFor(humanAmountsIn)
-  }, [humanAmountsIn, isLoadingTokenPrices, usdValueFor])
+  useEffect(() => {
+    if (!isLoadingTokenPrices) {
+      setTotalUSDValue(usdValueFor(humanAmountsIn))
+    }
+  }, [humanAmountsIn, isLoadingTokenPrices])
 
   const isMinimumDepositMet = useIsMinimumDepositMet({ humanAmountsIn, totalUSDValue })
 
@@ -186,6 +183,11 @@ export function useAddLiquidityLogic(urlTxHash?: Hash) {
     }
     await Promise.all([simulationQuery.refetch(), priceImpactQuery.refetch()])
   }
+
+  // On initial render, set the initial humanAmountsIn
+  useEffect(() => {
+    setInitialHumanAmountsIn()
+  }, [])
 
   const disabledConditions: [boolean, string][] = [
     [!isConnected, LABELS.walletNotConnected],
