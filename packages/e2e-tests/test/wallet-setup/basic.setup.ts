e84420c71857233c384f97843c67afa5035ade57
groninge <groninge@gmail.com>
2025-12-08 15:11:45 +0100

    add account and anvil fork

--- packages/e2e-tests/test/wallet-setup/basic.setup.ts
+++ packages/e2e-tests/test/wallet-setup/basic.setup.ts
@@ -14,6 +14,20 @@ export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   // Import the wallet using the seed phrase
   await metamask.importWallet(SEED_PHRASE)
 
+  // anvil #3 wallet
+  await metamask.importWalletFromPrivateKey(
+    '0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6',
+  )
+
+  await metamask.addNetwork({
+    name: 'Local Anvil Fork',
+    rpcUrl: 'http://127.0.0.1:8545',
+    chainId: 1,
+    symbol: 'ETH',
+  })
+
+  await metamask.switchNetwork('Local Anvil Fork', true)
+
   // Additional setup steps can be added here, such as:
   // - Adding custom networks
   // - Importing tokens
