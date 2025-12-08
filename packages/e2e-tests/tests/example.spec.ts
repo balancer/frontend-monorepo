092cfeb44abb8ca285c135ea58ef86e1d8bbc722
groninge <groninge@gmail.com>
2025-12-08 15:14:40 +0100

    expand example

--- packages/e2e-tests/tests/example.spec.ts
+++ packages/e2e-tests/tests/example.spec.ts
@@ -2,6 +2,8 @@
 import { testWithSynpress } from '@synthetixio/synpress'
 import { MetaMask, metaMaskFixtures } from '@synthetixio/synpress/playwright'
 import basicSetup from '../test/wallet-setup/basic.setup'
+import { clickButton, isButtonVisible } from '@/helpers/user.helpers'
+import { setForkBalances } from '@/helpers/e2e.helpers'
 
 // Create a test instance with Synpress and MetaMask fixtures
 const test = testWithSynpress(metaMaskFixtures(basicSetup))
@@ -16,11 +18,23 @@ test('should connect wallet to the MetaMask Test Dapp', async ({
   metamaskPage,
   extensionId,
 }) => {
+  await setForkBalances(page, {
+    chainId: 1,
+    forkBalances: {
+      1: [
+        {
+          tokenAddress: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
+          value: '100',
+        },
+      ],
+    },
+  })
+
   // Create a new MetaMask instance
   const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId)
 
-  // Navigate to the homepage
-  await page.goto('http://localhost:3000/pools')
+  // Navigate to the swap page
+  await page.goto('http://localhost:3000/swap')
 
   // Click the connect button
   await page.locator('#connectButton').click()
@@ -29,6 +43,7 @@ test('should connect wallet to the MetaMask Test Dapp', async ({
   await page.locator('text=MetaMask').click()
 
   // Connect MetaMask to the dapp
+  await metamask.switchAccount('Account 2') // anvil #3 wallet
   await metamask.connectToDapp()
 
   // Accept policies
@@ -41,7 +56,32 @@ test('should connect wallet to the MetaMask Test Dapp', async ({
   await page.getByRole('button', { name: 'Proceed' }).click()
 
   // Verify the connected account address
-  await expect(page.locator('#connectedButton')).toHaveText('0xf3…2266')
+  await expect(page.locator('#connectedButton')).toHaveText('0x90…b906')
+
+  await clickButton(page, 'ETH')
+  await page.getByText('Wrapped Ether').click()
+
+  await clickButton(page, 'Select token')
+  await page.getByText('USDCUSDC').click()
+  await page.getByRole('spinbutton', { name: 'TokenIn' }).fill('0.1')
+
+  // need to reload the page to get the correct token balance
+  // but it still looks like there is no balance?
+  await page.reload()
+
+  await clickButton(page, 'Next')
+
+  if (await isButtonVisible(page, 'Approve WETH to swap')) {
+    await clickButton(page, 'Approve WETH to swap')
+  }
+
+  await metamask.approveTokenPermission()
+
+  await clickButton(page, 'Swap')
+  await expect(page.getByText('Transaction confirmed')).toBeVisible()
+  await metamask.confirmTransaction()
+
+  await page.getByRole('button', { name: 'Swap again' }).click()
 
   // Additional test steps can be added here, such as:
   // - Sending transactions
