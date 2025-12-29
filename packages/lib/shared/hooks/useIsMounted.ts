cb0463c8dbc4458288b2230de44b7f6bd3254989
Juanjo Molinero <jjmolinero@gmail.com>
2025-12-29 11:02:54 +0100

    [#1932] disable linting on useIsMounted (explanation on comment)

--- packages/lib/shared/hooks/useIsMounted.ts
+++ packages/lib/shared/hooks/useIsMounted.ts
@@ -1,5 +1,3 @@
-// TODO: https://github.com/balancer/frontend-monorepo/issues/1932
-/* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from 'react'
 
 // The usage of mounted helps to overcome nextjs hydration mismatch
@@ -8,7 +6,12 @@ import { useEffect, useState } from 'react'
 export function useIsMounted() {
   const [mounted, setMounted] = useState(false)
   useEffect(() => {
-    setMounted(true)
+    // We disable the linting here because there is no other way
+    // to check if a component is already mounted (and avoid the hydration
+    // mismatch). The new versions of useIsMounted that try to do it
+    // with a ref don't work for this problem (they are designed to prevent
+    // memory leaks) as they will not re-render the component on change.
+    setMounted(true) // eslint-disable-line react-hooks/set-state-in-effect
   }, [])
 
   return mounted
