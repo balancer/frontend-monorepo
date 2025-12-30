2e08523366b07800f662ec73d0be7739493810cc
mattpereira <mattpereira.dev@gmail.com>
2025-12-30 15:59:31 -0800

    add info alert for HH sunset

--- apps/frontend-v3/lib/vebal/vote/Votes/MyVotes/MyVotes.tsx
+++ apps/frontend-v3/lib/vebal/vote/Votes/MyVotes/MyVotes.tsx
@@ -50,6 +50,22 @@ export function MyVotes() {
 
   return (
     <VStack align="start" spacing="md" w="full">
+      <GridItem w="full">
+        <Alert status="info" variant="WideOnDesktop">
+          <AlertIcon />
+          <Stack
+            alignItems="baseline"
+            direction={{ base: 'column', lg: 'row' }}
+            gap={{ base: '0', lg: 'sm' }}
+          >
+            <AlertTitle>Hidden Hand sunset on December 31, 2025</AlertTitle>
+            <AlertDescription>
+              Bribe market data will be available after the migration to StakeDAO is complete
+            </AlertDescription>
+          </Stack>
+        </Alert>
+      </GridItem>
+
       <HStack alignItems="baseline" justifyContent="space-between" w="full">
         <Heading
           as="h2"
