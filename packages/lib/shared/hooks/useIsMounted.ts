40a9c1e4c17f3006014243992c527594961ff7c4
Juanjo Molinero <jjmolinero@gmail.com>
2026-01-05 10:07:32 +0100

    [#1932] use the hack of useSyncExternalStore to check for server code

--- packages/lib/shared/hooks/useIsMounted.ts
+++ packages/lib/shared/hooks/useIsMounted.ts
@@ -1,18 +1,15 @@
-import { useEffect, useState } from 'react'
+import { useDeferredValue, useSyncExternalStore } from 'react'
 
-// The usage of mounted helps to overcome nextjs hydration mismatch
-// errors where the state of the user on the server pass is different
-// than the state on the client side rehydration.
+// https://tkdodo.eu/blog/avoiding-hydration-mismatches-with-use-sync-external-store
+// The new versions of useIsMounted (e.g. usehooks-ts) that try to do it
+// with a ref don't work for this problem (they are designed to prevent
+// memory leaks) as they will not re-render the component on change.
 export function useIsMounted() {
-  const [mounted, setMounted] = useState(false)
-  useEffect(() => {
-    // We disable the linting here because there is no other way
-    // to check if a component is already mounted (and avoid the hydration
-    // mismatch). The new versions of useIsMounted that try to do it
-    // with a ref don't work for this problem (they are designed to prevent
-    // memory leaks) as they will not re-render the component on change.
-    setMounted(true) // eslint-disable-line react-hooks/set-state-in-effect
-  }, [])
+  const isClient = useSyncExternalStore(
+    () => () => {}, // noop
+    () => true, // Client
+    () => false // Server
+  )
 
-  return mounted
+  return useDeferredValue(isClient)
 }
