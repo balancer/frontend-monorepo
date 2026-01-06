caf48ca40b1a587ff0225b086adcd4715299868f
uiuxxx <uxuidefi@gmail.com>
2026-01-06 13:20:24 +0000

    Update warning text for custom pool hooks

--- packages/lib/modules/pool/actions/create/steps/details/PoolSettingsRadioGroup.tsx
+++ packages/lib/modules/pool/actions/create/steps/details/PoolSettingsRadioGroup.tsx
@@ -116,7 +116,7 @@ export function PoolSettingsRadioGroup({
                             />
                             {name === 'poolHooksContract' && (
                               <BalAlert
-                                content="All unapproved hook contracts require review before add liquidity is enabled on the Balancer UI."
+                                content="All new hook contracts must pass a review before 'Add Liquidity' is enabled for users on the Balancer UI."
                                 status="warning"
                                 title="Unrecognized hooks require review"
                                 w="full"
