3b91e2c2b660d8f6eb008f39da1dae65ea47fd3d
mattpereira <mattpereira.dev@gmail.com>
2026-01-09 10:39:57 -0800

    fix lbp review step

--- packages/lib/modules/pool/actions/create/modal/cow-amm-steps/useCreateCowSteps.tsx
+++ packages/lib/modules/pool/actions/create/modal/cow-amm-steps/useCreateCowSteps.tsx
@@ -2,17 +2,40 @@ import { useBindTokenStep } from './useBindTokenStep'
 import { useSetSwapFeeStep } from './useSetSwapFeeStep'
 import { useFinalizeStep } from './useFinalizeStep'
 import { ExtendedInitPoolInput } from '../../types'
+import { GqlChain } from '@repo/lib/shared/services/api/generated/graphql'
+import { PoolType } from '@balancer/sdk'
+import { LS_KEYS } from '@repo/lib/modules/local-storage/local-storage.constants'
+import { useLocalStorage } from 'usehooks-ts'
+import { Address } from 'viem'
 
-export function useCreateCowSteps(initPoolInput: ExtendedInitPoolInput) {
+interface UseCreateCowStepsParams {
+  initPoolInput: ExtendedInitPoolInput
+  network: GqlChain
+  poolType: PoolType
+}
+
+export function useCreateCowSteps({ initPoolInput, network, poolType }: UseCreateCowStepsParams) {
+  const [poolAddress] = useLocalStorage<Address | undefined>(
+    LS_KEYS.PoolCreation.Address,
+    undefined
+  )
+
   const { amountsIn } = initPoolInput
+  const txConfig = { network, poolType, poolAddress }
 
   // cow pool will always be exactly 2 tokens
-  const { step: bindToken0Step, isLoading: isLoadingBindToken0 } = useBindTokenStep(amountsIn[0])
-  const { step: bindToken1Step, isLoading: isLoadingBindToken1 } = useBindTokenStep(amountsIn[1])
+  const { step: bindToken0Step, isLoading: isLoadingBindToken0 } = useBindTokenStep({
+    token: amountsIn[0],
+    ...txConfig,
+  })
+  const { step: bindToken1Step, isLoading: isLoadingBindToken1 } = useBindTokenStep({
+    token: amountsIn[1],
+    ...txConfig,
+  })
 
-  const { setSwapFeeStep, isLoadingSetSwapFeeStep } = useSetSwapFeeStep()
+  const { setSwapFeeStep, isLoadingSetSwapFeeStep } = useSetSwapFeeStep(txConfig)
 
-  const { finalizeStep, isLoadingFinalizeStep } = useFinalizeStep()
+  const { finalizeStep, isLoadingFinalizeStep } = useFinalizeStep(txConfig)
 
   const finishCowSteps = [bindToken0Step, bindToken1Step, setSwapFeeStep, finalizeStep]
 
