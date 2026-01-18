78ea1ec10decf5e63fded03ea1f767e45d13d080
mattpereira <mattpereira.dev@gmail.com>
2026-01-18 15:51:23 -0800

    test build popover

--- packages/e2e-tests/tests/dev/balancer/create-pool.spec.ts
+++ packages/e2e-tests/tests/dev/balancer/create-pool.spec.ts
@@ -46,3 +46,53 @@ test('Create pool form step navigation', async ({ page }) => {
   await expect(createPoolButton).toBeVisible()
   await expect(createPoolButton).toHaveAttribute('disabled', '')
 })
+
+test('Build popover sets form state for protocol', async ({ page }) => {
+  await page.goto('http://localhost:3000/swap')
+
+  await impersonate(page, defaultAnvilAccount)
+
+  const buildLink = page.getByText('Build', { exact: true })
+  await expect(buildLink).toBeVisible()
+  await buildLink.click()
+
+  await page.getByText('CoW AMM', { exact: true }).click()
+
+  await expect(page).toHaveURL(`${BASE_URL}?protocol=cow`)
+
+  await expect(page.getByText('CoW AMM: 50/50')).toBeVisible()
+})
+
+test.describe('Warning modal triggered by build popover', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(`${BASE_URL}`)
+    await expect(page).toHaveURL(`${BASE_URL}/step-1-type`)
+
+    await impersonate(page, defaultAnvilAccount)
+
+    const step1NextButton = page.getByRole('link', { name: 'Next' })
+    await step1NextButton.click()
+
+    await expect(page).toHaveURL(`${BASE_URL}/step-2-tokens`)
+
+    const buildLink = page.getByText('Build', { exact: true })
+    await expect(buildLink).toBeVisible()
+    await buildLink.click()
+
+    await page.getByText('CoW AMM', { exact: true }).click()
+    await expect(page).toHaveURL(`${BASE_URL}?protocol=cow`)
+
+    await expect(page.getByText('Delete progress and start over')).toBeVisible()
+  })
+
+  test('can continue progress', async ({ page }) => {
+    await page.getByRole('button', { name: 'Continue set up' }).click()
+    await expect(page).toHaveURL(`${BASE_URL}/step-2-tokens`)
+  })
+
+  test('can reset progress', async ({ page }) => {
+    await page.getByRole('button', { name: 'Delete and start over' }).click()
+    await expect(page).toHaveURL(`${BASE_URL}/step-1-type`)
+    await expect(page.getByText('Choose protocol')).toBeVisible()
+  })
+})
