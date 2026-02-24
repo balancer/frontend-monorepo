e8fc4dd243ef3547213ac936a7535e8e31861917
Matt Pereira <mattpereira.dev@gmail.com>
2026-02-24 08:39:05 -0800

    DRY schema

--- packages/lib/shared/services/api/codegen.ts
+++ packages/lib/shared/services/api/codegen.ts
@@ -3,20 +3,18 @@ import { CodegenConfig } from '@graphql-codegen/cli'
 const schemaWithHeaders = {
   [process.env.NEXT_PUBLIC_BALANCER_API_URL as string]: {
     headers: {
-      // Prevent gzip-compressed responses that the schema loader can't decompress
-      'Accept-Encoding': 'identity',
+      'Accept-Encoding': 'identity', // Prevent gzip-compressed responses that the schema loader can't decompress
     },
   },
 }
 
 const config: CodegenConfig = {
   generates: {
+    schema: schemaWithHeaders,
     ['./shared/services/api/generated/schema.graphql']: {
-      schema: schemaWithHeaders,
       plugins: ['schema-ast'],
     },
     [`./shared/services/api/generated/`]: {
-      schema: schemaWithHeaders,
       documents: ['./shared/services/api/**/*.graphql'],
       preset: 'client',
       presetConfig: {
