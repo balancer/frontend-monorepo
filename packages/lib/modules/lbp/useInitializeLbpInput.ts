e2abf0f13bebe0a15c1a7bda8446a09e8de7c7b7
groninge <groninge@gmail.com>
2026-02-27 14:12:10 +0100

    sdk requires reserve token and that it's amount is set to zero

--- packages/lib/modules/lbp/useInitializeLbpInput.ts
+++ packages/lib/modules/lbp/useInitializeLbpInput.ts
@@ -8,7 +8,7 @@ import { useWatch } from 'react-hook-form'
 import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
 
 export function useInitializeLbpInput() {
-  const { saleStructureForm, isCollateralNativeAsset, isDynamicSale } = useLbpForm()
+  const { saleStructureForm, isCollateralNativeAsset, isFixedSale } = useLbpForm()
   const [
     selectedChain,
     collateralTokenAddress,
@@ -66,11 +66,12 @@ export function useInitializeLbpInput() {
   const reserveTokenAmountIn: InitPoolInputAmount = {
     address: reserveTokenAddress as Address,
     decimals: reserveTokenDecimals,
-    rawAmount: parseUnits(collateralTokenAmount, reserveTokenDecimals),
+    // sdk requires reserve token amount to be zero for fixed sale
+    rawAmount: isFixedSale ? 0n : parseUnits(collateralTokenAmount, reserveTokenDecimals),
     symbol: reserveTokenSymbol,
   }
 
-  const amountsIn = [...(isDynamicSale ? [reserveTokenAmountIn] : []), launchTokenAmountIn]
+  const amountsIn = [reserveTokenAmountIn, launchTokenAmountIn]
 
   return { amountsIn, minBptAmountOut, chainId, wethIsEth }
 }
