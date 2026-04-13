5b03f054c156d590bd90a9c2139a9a23eab90625
groninge <groninge@gmail.com>
2026-04-13 14:40:55 +0200

    feat: prefetch GetPools in beets-frontend-v3 pools RSC page

--- apps/beets-frontend-v3/app/(app)/pools/page.tsx
+++ apps/beets-frontend-v3/app/(app)/pools/page.tsx
@@ -1,7 +1,51 @@
 import { PoolsPage } from '@repo/lib/shared/pages/PoolsPage/PoolsPage'
 import { BeetsPromoBanner } from '@/lib/components/promos/BeetsPromoBanner'
-import { GetStakedSonicDataDocument } from '@repo/lib/shared/services/api/generated/graphql'
-import { getApolloServerClient } from '@repo/lib/shared/services/api/apollo-server.client'
+import {
+  GetStakedSonicDataDocument,
+  GetPoolsDocument,
+  GqlPoolOrderBy,
+  GqlPoolOrderDirection,
+  GqlPoolType,
+} from '@repo/lib/shared/services/api/generated/graphql'
+import {
+  getApolloServerClient,
+  PreloadQuery,
+} from '@repo/lib/shared/services/api/apollo-server.client'
+import { PROJECT_CONFIG } from '@repo/lib/config/getProjectConfig'
+
+const revalidateContext = {
+  fetchOptions: { next: { revalidate: 60 } },
+}
+
+const defaultPoolListVariables = {
+  first: 20,
+  skip: 0,
+  orderBy: GqlPoolOrderBy.TotalLiquidity,
+  orderDirection: GqlPoolOrderDirection.Desc,
+  where: {
+    poolTypeIn: [
+      GqlPoolType.Weighted,
+      GqlPoolType.Stable,
+      GqlPoolType.ComposableStable,
+      GqlPoolType.MetaStable,
+      GqlPoolType.Gyro,
+      GqlPoolType.Gyro3,
+      GqlPoolType.Gyroe,
+      GqlPoolType.CowAmm,
+      GqlPoolType.Fx,
+      GqlPoolType.QuantAmmWeighted,
+      GqlPoolType.Reclamm,
+    ],
+    poolTypeNotIn: [GqlPoolType.LiquidityBootstrapping],
+    chainIn: PROJECT_CONFIG.supportedNetworks,
+    userAddress: null,
+    minTvl: 0,
+    tagIn: null,
+    tagNotIn: ['BLACK_LISTED'],
+    protocolVersionIn: undefined,
+  },
+  textSearch: null,
+}
 
 export default async function PoolsPageWrapper() {
   const client = getApolloServerClient()
@@ -14,9 +58,15 @@ export default async function PoolsPageWrapper() {
   if (!stakedSonicData) return null
 
   return (
-    <PoolsPage rewardsClaimed24h={stakedSonicData.stsGetGqlStakedSonicData.rewardsClaimed24h}>
-      {/* TODO: add <PromoBanners /> at a later date */}
-      <BeetsPromoBanner />
-    </PoolsPage>
+    <PreloadQuery
+      context={revalidateContext}
+      query={GetPoolsDocument}
+      variables={defaultPoolListVariables}
+    >
+      <PoolsPage rewardsClaimed24h={stakedSonicData.stsGetGqlStakedSonicData.rewardsClaimed24h}>
+        {/* TODO: add <PromoBanners /> at a later date */}
+        <BeetsPromoBanner />
+      </PoolsPage>
+    </PreloadQuery>
   )
 }
