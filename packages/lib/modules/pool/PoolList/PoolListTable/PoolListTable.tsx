8cc006d6d9be2a23645e50d56408f6746732aa6f
uiuxxx <uxuidefi@gmail.com>
2026-04-13 15:53:52 +0100

    Fix bug where table row hover state didn't extend full width on smaller screens when user scrolls rightwards

--- packages/lib/modules/pool/PoolList/PoolListTable/PoolListTable.tsx
+++ packages/lib/modules/pool/PoolList/PoolListTable/PoolListTable.tsx
@@ -5,7 +5,7 @@ import { PoolListTableHeader } from './PoolListTableHeader'
 import { PoolListTableRow } from './PoolListTableRow'
 import { getPaginationProps } from '@repo/lib/shared/components/pagination/getPaginationProps'
 import { PoolListItem } from '../../pool.types'
-import { Card, Skeleton } from '@chakra-ui/react'
+import { Box, Card, Skeleton } from '@chakra-ui/react'
 import { useIsMounted } from '@repo/lib/shared/hooks/useIsMounted'
 import { usePoolList } from '../PoolListProvider'
 import { useCallback, useMemo } from 'react'
@@ -69,16 +69,18 @@ export function PoolListTable({ pools, count, loading }: Props) {
       pr={{ base: 'lg', sm: 'lg', md: 'lg', lg: '0' }}
       w={{ base: '100vw', lg: 'full' }}
     >
-      <PaginatedTable
-        getRowId={item => item.id}
-        items={pools}
-        loading={loading}
-        noItemsFoundLabel="No pools found"
-        paginationProps={paginationProps}
-        renderTableHeader={renderTableHeader}
-        renderTableRow={renderTableRow}
-        showPagination={showPagination}
-      />
+      <Box minW="max-content" w="full">
+        <PaginatedTable
+          getRowId={item => item.id}
+          items={pools}
+          loading={loading}
+          noItemsFoundLabel="No pools found"
+          paginationProps={paginationProps}
+          renderTableHeader={renderTableHeader}
+          renderTableRow={renderTableRow}
+          showPagination={showPagination}
+        />
+      </Box>
     </Card>
   )
 }
