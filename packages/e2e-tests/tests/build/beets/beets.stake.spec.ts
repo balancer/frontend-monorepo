94310e7b54fa2bbeae24d44592350a92825b8dc9
groninge <groninge@gmail.com>
2026-06-03 17:29:50 +0200

    fix flaky test

--- packages/e2e-tests/tests/build/beets/beets.stake.spec.ts
+++ packages/e2e-tests/tests/build/beets/beets.stake.spec.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test'
 
 test('Beets: stake page renders', async ({ page }) => {
   await page.goto('http://localhost:3001/stake')
-  await expect(page.getByText('stS rate')).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Stake' })).toBeVisible()
 })
