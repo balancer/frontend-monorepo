893163e9487b3c8a8e30cfa12a403c9b733e2128
groninge <groninge@gmail.com>
2026-06-08 11:56:35 +0200

    test: fix integration spec token address for waEthWETH pool

--- packages/lib/modules/pool/actions/add-liquidity/handlers/UnbalancedAddLiquidityViaSwapV3.handler.integration.spec.ts
+++ packages/lib/modules/pool/actions/add-liquidity/handlers/UnbalancedAddLiquidityViaSwapV3.handler.integration.spec.ts
@@ -2,18 +2,17 @@ import { HumanTokenAmountWithSymbol } from '@repo/lib/modules/tokens/token.types
 import { GqlChain } from '@repo/lib/shared/services/api/generated/graphql'
 import { defaultTestUserAccount } from '@repo/test/anvil/anvil-setup'
 import { UnbalancedAddLiquidityViaSwapV3Handler } from './UnbalancedAddLiquidityViaSwapV3.handler'
-import { selectAddLiquidityHandler } from './selectAddLiquidityHandler'
 import { fetchPoolMock } from '../../../__mocks__/fetchPoolMock'
 
 describe('When adding unbalanced liquidity via swap for a V3 2-token pool', async () => {
-  const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
+  const waEthWETHAddress = '0x0bfc9d54fc184518a81162f8fb99c2eaca081202'
   const poolId = '0x1ea5870f7c037930ce1d5d8d9317c670e89e13e3' // rETH-waEthWETH
   const v3Pool = await fetchPoolMock({ poolId, chain: GqlChain.Mainnet })
 
-  const handler = selectAddLiquidityHandler(v3Pool) as UnbalancedAddLiquidityViaSwapV3Handler
+  const handler = new UnbalancedAddLiquidityViaSwapV3Handler(v3Pool)
 
   const humanAmountsIn: HumanTokenAmountWithSymbol[] = [
-    { humanAmount: '0.1', tokenAddress: wethAddress, symbol: 'WETH' },
+    { humanAmount: '0.1', tokenAddress: waEthWETHAddress, symbol: 'waEthWETH' },
   ]
 
   it('returns zero price impact (not yet supported by SDK)', async () => {
