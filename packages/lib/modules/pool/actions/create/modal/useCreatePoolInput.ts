631624f628944a6d7d6bf5a101eeb1f2cd785a03
groninge <groninge@gmail.com>
2026-06-25 11:01:10 +0200

    revert: restore original import order in useCreatePoolInput.ts

--- packages/lib/modules/pool/actions/create/modal/useCreatePoolInput.ts
+++ packages/lib/modules/pool/actions/create/modal/useCreatePoolInput.ts
@@ -3,8 +3,8 @@ import { TokenType, CreatePoolV3BaseInput, PoolType } from '@balancer/sdk'
 import { parseUnits, zeroAddress } from 'viem'
 import { PERCENTAGE_DECIMALS, DEFAULT_DECIMALS } from '../constants'
 import { getNetworkConfig, getGqlChain } from '@repo/lib/config/app.config'
-import { CreatePoolInput } from '../types'
 import { invertNumber } from '@repo/lib/shared/utils/numbers'
+import { CreatePoolInput } from '../types'
 
 export function useCreatePoolInput(chainId: number): CreatePoolInput {
   const { poolCreationForm, autoRangeConfigForm, eclpConfigForm } = usePoolCreationForm()
